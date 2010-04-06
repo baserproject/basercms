b48d0e348f542754a1586f84d7b996b263ae51f4
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2010-04-06 06:43:07 +0000

    プラグインのコンテンツID取得を改善
    プレフィックス付きの場合でも取得できるようにした
    
    git-svn-id: http://svn.e-catchup.jp/basercms/trunk@195 074335c2-c415-4d42-95a7-fc8f016e1c3c

--- baser/controllers/plugins_controller.php
+++ baser/controllers/plugins_controller.php
@@ -108,31 +108,42 @@ class PluginsController extends AppController {
  */
     function getContentId(){
 
-        if(!isset($this->PluginContent)){
-            return null;
-        }
-        
+		if(!isset($this->PluginContent)){
+			return null;
+		}
+		
 		if(!isset($this->params['url']['url'])){
 			return null;
 		}
-									   
-        $url = split('/',$this->params['url']['url']);
-        if($url[0]!=Configure::read('Mobile.prefix')){
-            $url = $url[0];
-        }else{
-            $url = $url[1];
-        }
+		$contentName = '';
+		$url = split('/',$this->params['url']['url']);
+		if($url[0]!=Configure::read('Mobile.prefix')){
+			if(!empty($this->params['prefix']) && $url[0] == $this->params['prefix']){
+				if(isset($url[1])){
+					$contentName = $url[1];
+				}
+			}else{
+				$contentName = $url[0];
+			}
+		}else{
+			if(!empty($this->params['prefix']) && $url[0] == $this->params['prefix']){
+				$contentName = $url[2];
+			}else{
+				$contentName = $url[1];
+			}
+		}
+		
 		// プラグインと同じ名前のコンテンツ名の場合に正常に動作しないので
 		// とりあえずコメントアウト
-        /*if( Inflector::camelize($url) == $this->name){
-            return null;
-        }*/
-        $pluginContent = $this->PluginContent->findByName($url);
-        if($pluginContent){
-            return $pluginContent['PluginContent']['content_id'];
-        }else{
-            return null;
-        }
+		/*if( Inflector::camelize($url) == $this->name){
+			return null;
+		}*/
+		$pluginContent = $this->PluginContent->findByName($contentName);
+		if($pluginContent){
+			return $pluginContent['PluginContent']['content_id'];
+		}else{
+			return null;
+		}
         
     }
 /**
