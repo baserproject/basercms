aa884d24a3cbaf32730c96c542946d72bddab51c
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2010-05-18 17:01:51 +0000

    Session Cookie をCAKEPHP から BASERCMSに変更
    git-svn-id: http://svn.e-catchup.jp/basercms/trunk@221 074335c2-c415-4d42-95a7-fc8f016e1c3c

--- app/config/core.php
+++ app/config/core.php
@@ -117,7 +117,7 @@
 /**
  * The name of CakePHP's session cookie.
  */
-	Configure::write('Session.cookie', 'CAKEPHP');
+	Configure::write('Session.cookie', 'BASERCMS');
 /**
  * Session time out time (in seconds).
  * Actual value depends on 'Security.level' setting.
