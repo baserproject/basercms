493232206fb5b954bdb46b7fc7f9c549838aede3
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2010-06-08 09:27:23 +0000

    コード整理。PostgreSQLバックアップの出力ファイル名を変更。
    plugin_baser.sql → baser_plugin.sql
    
    また、過去のバックアップファイルが混入してしまうバグを修正。
    
    fixes #704
    
    git-svn-id: http://svn.e-catchup.jp/basercms/trunk@229 074335c2-c415-4d42-95a7-fc8f016e1c3c

--- baser/controllers/site_configs_controller.php
+++ baser/controllers/site_configs_controller.php
@@ -2,7 +2,7 @@
 /* SVN FILE: $Id$ */
 /**
  * サイト設定コントローラー
- * 
+ *
  * PHP versions 4 and 5
  *
  * BaserCMS :  Based Website Development Project <http://basercms.net>
@@ -34,7 +34,7 @@ class SiteConfigsController extends AppController {
  * @var		string
  * @access 	public
  */
-   	var $name = 'SiteConfigs';
+	var $name = 'SiteConfigs';
 /**
  * モデル
  *
@@ -48,7 +48,7 @@ class SiteConfigsController extends AppController {
  * @var     array
  * @access  public
  */
-   var $components = array('Auth','Cookie','AuthConfigure');
+	var $components = array('Auth','Cookie','AuthConfigure');
 /**
  * サブメニューエレメント
  *
@@ -58,9 +58,9 @@ class SiteConfigsController extends AppController {
 	var $subMenuElements = array();
 /**
  * ヘルパー
- * @var array 
+ * @var array
  */
-    var $helpers = array('FormEx');
+	var $helpers = array('FormEx');
 /**
  * ぱんくずナビ
  *
@@ -76,7 +76,7 @@ class SiteConfigsController extends AppController {
 	var $Folder;
 
 /**
- *
+ * beforeFilter
  */
 	function beforeFilter() {
 
@@ -85,81 +85,83 @@ class SiteConfigsController extends AppController {
 		// init Folder
 		$this->Folder =& new Folder();
 		$this->Folder->mode = 0777;
+		
 	}
-
 /**
  * [ADMIN] サイト基本設定
  *
  * @return	void
  * @access 	public
  */
-	function admin_form(){
+	function admin_form() {
 
-		if(empty($this->data)){
+		if(empty($this->data)) {
 			$this->data = $this->SiteConfig->read(null, 1);
-            $this->data['SiteConfig'] = $this->siteConfigs;
-            $this->data['SiteConfig']['mode'] = $this->readDebug();
-		}else{
-            // テーブル構造が特殊なので強引にバリデーションを行う
-            $this->SiteConfig->data = $this->data;
-            if(!$this->SiteConfig->validates()){
-                $this->Session->setFlash('入力エラーです。内容を修正してください。');
-            }else{
-                // KeyValueへ変換処理
-                $mode = $this->data['SiteConfig']['mode'];
-                unset($this->data['SiteConfig']['mode']);
-                unset($this->data['SiteConfig']['id']);
-                $this->SiteConfig->saveKeyValue($this->data);
+			$this->data['SiteConfig'] = $this->siteConfigs;
+			$this->data['SiteConfig']['mode'] = $this->readDebug();
+		}else {
+			// テーブル構造が特殊なので強引にバリデーションを行う
+			$this->SiteConfig->data = $this->data;
+			if(!$this->SiteConfig->validates()) {
+				$this->Session->setFlash('入力エラーです。内容を修正してください。');
+			}else {
+				// KeyValueへ変換処理
+				$mode = $this->data['SiteConfig']['mode'];
+				unset($this->data['SiteConfig']['mode']);
+				unset($this->data['SiteConfig']['id']);
+				$this->SiteConfig->saveKeyValue($this->data);
 
-                // ビューのキャッシュを削除
-                $this->deleteViewCache();
-                $this->writeDebug($mode);
-                $this->Session->setFlash('システム設定を保存しました。');
-                $this->redirect(array('action'=>'form'));
-            }
+				// ビューのキャッシュを削除
+				$this->deleteViewCache();
+				$this->writeDebug($mode);
+				$this->Session->setFlash('システム設定を保存しました。');
+				$this->redirect(array('action'=>'form'));
+			}
 		}
 
-        // バックアップ機能を実装しているデータベースの場合のみバックアップへのリンクを表示
-        $enableBackupDb = array('sqlite','sqlite3','mysql','csv','postgres');
-        $dbConfigs = new DATABASE_CONFIG();
-        $dbConfig = $dbConfigs->{'baser'};
-        $driver = str_replace('_ex','',$dbConfig['driver']);
-        if(in_array($driver,$enableBackupDb)){
-            $this->set('backupEnabled',true);
-        }
+		// バックアップ機能を実装しているデータベースの場合のみバックアップへのリンクを表示
+		$enableBackupDb = array('sqlite','sqlite3','mysql','csv','postgres');
+		$dbConfigs = new DATABASE_CONFIG();
+		$dbConfig = $dbConfigs->{'baser'};
+		$driver = str_replace('_ex','',$dbConfig['driver']);
+		if(in_array($driver,$enableBackupDb)) {
+			$this->set('backupEnabled',true);
+		}
 
-        // テーマの一覧を取得
-        $this->set('themes',$this->SiteConfig->getThemes());
+		// テーマの一覧を取得
+		$this->set('themes',$this->SiteConfig->getThemes());
 
 		// 表示設定
-        $this->subMenuElements = array('site_configs');
+		$this->subMenuElements = array('site_configs');
 		$this->pageTitle = 'サイト基本設定';
 
 	}
 /**
  * [ADMIN] バックアップデータを作成する
  *
- * mysql/csvdb のバックアップを [app/tmp/backup/database]に保存し、
+ * データベースのバックアップを [app/tmp/backup/database]に保存し、
  * zip形式にアーカイブした状態でダウンロードできる
- *
+ * TODO 現在、PostgreSQLの場合、DB接続設定ごとにファイルが生成されるが、
+ * 他のDBについては、１回で全てのDBのデータを出力する仕様となってしまっている。
+ * 同じDBの場合はただの上書き。違うDBを利用した場合には、正常にバックアップがとれない。
  * @param	boolean	バックアップアーカイブをクリアする場合は、true を設定
  * @return 	void
  * @access	public
  */
-	function admin_backup_data($blnClearOldArchives = false){
+	function admin_backup_data($blnClearOldArchives = false) {
 
 		$backupDir = TMP . 'backup' . DS;
-		$this->Folder->create($backupDir);
-
 		$backupPath = $backupDir . 'database' . DS;
-		$this->Folder->create($backupPath);
+		$this->Folder->delete($backupPath);
+		$this->Folder->create($backupDir,0777);
+		$this->Folder->create($backupPath,0777);
 
 		$dbConfigs = new DATABASE_CONFIG();
 		foreach ($dbConfigs as $key => $dbConfig) {
 
 			$backupMethodName = preg_replace('/Ex$/', '', '_backup' . Inflector::camelize($dbConfig['driver']));
 
-			if($key != 'test' && method_exists($this, $backupMethodName)){
+			if($key != 'test' && method_exists($this, $backupMethodName)) {
 				call_user_func_array(array($this, $backupMethodName), array($dbConfig, $backupPath, $key));
 			}
 
@@ -170,12 +172,10 @@ class SiteConfigsController extends AppController {
 		$createZip->get_files_from_folder($backupPath, '/');
 
 		$archiveDir = $backupDir . 'archives' . DS;
-		$this->Folder->create($archiveDir);
-
-		if($blnClearOldArchives){
+		if($blnClearOldArchives) {
 			$this->Folder->delete($archiveDir);
-			$this->Folder->create($archiveDir);
 		}
+		$this->Folder->create($archiveDir);
 
 		$fileName = $archiveDir . date('Ymd_His') . '_backup.zip';
 		$fd = fopen ($fileName, "wb");
@@ -187,88 +187,17 @@ class SiteConfigsController extends AppController {
 
 	}
 /**
- * CSVのデータをバックアップ
- * TODO DBOに移行する
- *
- * @param	array	データベース設定情報
- * @param	string	保存先パス
- * @return 	boolean
- * @access	protected
- */
-	function _backupCsv($config,$savePath){
-
-		$csvDir = 'csv';
-		$csvPath = APP.'db'.DS.$csvDir;
-		$_savePath = $savePath.$csvDir;
-
-		$this->Folder->create($savePath);
-		$this->Folder->create($_savePath);
-
-		return $this->_backupDir($csvPath,$_savePath);
-
-	}
-/**
- * SQLite3のバックアップ
- * TODO DBOに移行する
- * @param array $config
- * @param string $savePath
- * @return boolean
- */
-    function _backupSqlite3($config,$savePath){
-
-		$dir = 'sqlite';
-		$path = $config['database'];
-        $dbName = basename($config['database']);
-		$_savePath = $savePath.$dir;
-
-		$this->Folder->create($savePath);
-		$this->Folder->create($_savePath);
-
-        return copy($path,$_savePath.DS.$dbName);
-
-    }
-/**
- * ディレクトリごとファイルバックアップする
- *
- * ※ ただし、defaultという名称のディレクトリはコピーしない
- * TODO 除外オプションをつけるべき
- *
- * @param	string	コピー元ディレクトリパス
- * @param	string	コピー先ディレクトリパス
- * @return 	boolean
- * @access	protected
- */
-	function _backupDir($targetPath,$savePath){
-
-		$dir = dir($targetPath);
-		while(($file = $dir->read()) !== false){
-			if($file != '.' && $file != '..' && $file != 'default'){
-				$_targetPath = $targetPath.DS.$file;
-				$_savePath = $savePath.DS.$file;
-				if(is_dir($_targetPath)){
-					$this->Folder->create($_savePath);
-					$this->_backupDir($_targetPath,$_savePath);
-				}else{
-					copy($_targetPath,$_savePath);
-					chmod($_savePath,0666);
-				}
-			}
-		}
-		return true;
-
-	}
-/**
  * MySqlのデータをバックアップ
- * TODO DBOに移行する
+ * TODO SQLDumperに移行する
  * @param	array	データベース設定情報
  * @param	string	保存先パス
  * @return 	boolean
  * @access	protected
  */
-	function _backupMysql($config,$savePath){
+	function _backupMysql($config,$savePath) {
 
 		$_savePath = $savePath.'mysql'.DS;
-		$this->Folder->create($_savePath);
+		$this->Folder->create($_savePath,0777);
 
 		App::import('Vendor','mysqldump');
 		$connection = @mysql_connect($config['host'],$config['login'],$config['password']);
@@ -277,43 +206,99 @@ class SiteConfigsController extends AppController {
 		$filename = $_savePath.$config['database'].'.sql';
 
 		$dump = new MySQLDump($config['database'],$filename);
-                $dump->doDump();
+		$dump->doDump();
 
 	}
 /**
- * MySqlのデータをバックアップ(mysql_logドライバ用）
- *
- * @param	array	データベース設定情報
- * @param	string	保存先パス
- * @return 	boolean
- * @access	protected
+ * SQLite3のバックアップ
+ * TODO SQLDumperに移行する
+ * @param array $config
+ * @param string $savePath
+ * @return boolean
  */
-	function _backupMysqlLog($config,$savePath){
+	function _backupSqlite3($config,$savePath) {
 
-        $this->_backupMysql($config,$savePath);
+		$dir = 'sqlite';
+		$path = $config['database'];
+		$dbName = basename($config['database']);
+		$_savePath = $savePath.$dir;
+
+		$this->Folder->create($savePath,0777);
+		$this->Folder->create($_savePath,0777);
+
+		return copy($path,$_savePath.DS.$dbName);
 
 	}
 /**
  * PostgreSQLのデータをバックアップ
- * TODO DBOに移行する
  * @param	array	データベース設定情報
  * @param	string	保存先パス
+ * @key		string	DB設定名
  * @return 	boolean
  * @access	protected
  */
-	function _backupPostgres($config, $savePath, $key) {
+	function _backupPostgres($config, $savePath, $configName) {
 
 		$_savePath = $savePath . 'postgres' . DS;
-		$this->Folder->create($_savePath);
+		$this->Folder->create($_savePath,0777);
 
 		App::import('Vendor', 'SqlDumper', array('file' => 'sql_dumper' . DS . 'sql_dumper.php'));
 
 		/* @var SqlDumper $sqlDumpr */
 		$sqlDumper =& ClassRegistry::init('SqlDumper', 'Vendor');
 		$sqlDumper->file_prefix = '';
-		$sqlDumper->file_suffix = '_' . $config['database'] . '.sql';
-		$sqlDumper->process($key, null, $_savePath);
+		$sqlDumper->file_suffix = '_' . $configName . '.sql';
+		$sqlDumper->process($config['database'], null, $_savePath);
 
 	}
+/**
+ * CSVのデータをバックアップ
+ * TODO SQLDumperに移行する（ただし、ドライバーも対応させる必要がある）
+ * @param	array	データベース設定情報
+ * @param	string	保存先パス
+ * @return 	boolean
+ * @access	protected
+ */
+	function _backupCsv($config,$savePath) {
+
+		$csvDir = 'csv';
+		$csvPath = APP.'db'.DS.$csvDir;
+		$_savePath = $savePath.$csvDir;
+
+		$this->Folder->create($savePath,0777);
+		$this->Folder->create($_savePath,0777);
+
+		return $this->_backupDir($csvPath,$_savePath);
+
+	}
+/**
+ * ディレクトリごとファイルバックアップする
+ * ※ ただし、defaultという名称のディレクトリはコピーしない
+ * TODO 除外オプションをつけるべき
+ * @param	string	コピー元ディレクトリパス
+ * @param	string	コピー先ディレクトリパス
+ * @return 	boolean
+ * @access	protected
+ */
+	function _backupDir($targetPath,$savePath) {
+
+		$dir = dir($targetPath);
+		while(($file = $dir->read()) !== false) {
+			if($file != '.' && $file != '..' && $file != 'default') {
+				$_targetPath = $targetPath.DS.$file;
+				$_savePath = $savePath.DS.$file;
+				if(is_dir($_targetPath)) {
+					$this->Folder->create($_savePath,0777);
+					$this->_backupDir($_targetPath,$_savePath);
+				}else {
+					copy($_targetPath,$_savePath);
+					chmod($_savePath,0666);
+				}
+			}
+		}
+		return true;
+
+	}
+	
 }
 ?>
