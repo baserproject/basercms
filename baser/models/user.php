8874ae2b560d4bbbe1b93c45cfd7a50b99930f79
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2010-07-20 04:53:20 +0000

    コード整理
    git-svn-id: http://svn.e-catchup.jp/basercms/trunk@371 074335c2-c415-4d42-95a7-fc8f016e1c3c

--- baser/models/user.php
+++ baser/models/user.php
@@ -2,12 +2,12 @@
 /* SVN FILE: $Id$ */
 /**
  * ユーザーモデル
- * 
+ *
  * PHP versions 4 and 5
  *
  * BaserCMS :  Based Website Development Project <http://basercms.net>
  * Copyright 2008 - 2010, Catchup, Inc.
- *								9-5 nagao 3-chome, fukuoka-shi 
+ *								9-5 nagao 3-chome, fukuoka-shi
  *								fukuoka, Japan 814-0123
  *
  * @copyright		Copyright 2008 - 2010, Catchup, Inc.
@@ -34,60 +34,58 @@ class User extends AppModel {
  * @var		string
  * @access 	public
  */
-   	var $name = 'User';
+	var $name = 'User';
 /**
  * データベース接続
  *
  * @var     string
  * @access  public
  */
-    var $useDbConfig = 'baser';
+	var $useDbConfig = 'baser';
 /**
  * belongsTo
  * @var 	array
  * @access	public
  */
- 	var $belongsTo = array('UserGroup' =>   array(  'className'=>'UserGroup',
-                                                        'foreignKey'=>'user_group_id'));
+	var $belongsTo = array('UserGroup' =>   array(  'className'=>'UserGroup',
+							'foreignKey'=>'user_group_id'));
 /**
  * beforeValidate
  *
  * @return	boolean
  * @access	public
  */
-	function beforeValidate(){
-		
-		
-		
+	function beforeValidate() {
+
 		$this->validate['name'] = array(array(	'rule' => VALID_NOT_EMPTY,
-												'message' => ">> アカウント名を入力して下さい"),
-										array(	'rule' => 'halfText',
-												'message' => '>> アカウント名は半角のみで入力して下さい'),
-										array(	'rule' => array('duplicate','name'),
-												'message' => '>> 既に登録のあるアカウント名です'));
-											
+						'message' => ">> アカウント名を入力して下さい"),
+				array(	'rule' => 'halfText',
+						'message' => '>> アカウント名は半角のみで入力して下さい'),
+				array(	'rule' => array('duplicate','name'),
+						'message' => '>> 既に登録のあるアカウント名です'));
+
 		$this->validate['real_name_1'] = array(array(	'rule' => VALID_NOT_EMPTY,
-														'message' => ">> 名前[姓]を入力して下さい"));
-											
+						'message' => ">> 名前[姓]を入力して下さい"));
+
 		/*$this->validate['real_name_2'] = array(array(	'rule' => VALID_NOT_EMPTY,
 														'message' => ">> 名前[名]を入力して下さい"));*/
-	
-        $this->validate['password'] = array('minLength' => array(
-												'rule' => array('minLength',6),
-												'allowEmpty' => false,
-												'message' => '>> パスワードは６文字以上で入力して下さい。'),
-											'alphaNumeric' => array(
-												'rule' => 'alphaNumeric',
-												'message' => '>> パスワードは半角英数字のみで入力して下さい'));
-	
-/*		$this->validate['email'] = array(array(	'rule' => array('email'),
+
+		$this->validate['password'] = array('minLength' => array(
+						'rule' => array('minLength',6),
+						'allowEmpty' => false,
+						'message' => '>> パスワードは６文字以上で入力して下さい。'),
+				'alphaNumeric' => array(
+						'rule' => 'alphaNumeric',
+						'message' => '>> パスワードは半角英数字のみで入力して下さい'));
+
+		/*		$this->validate['email'] = array(array(	'rule' => array('email'),
 												'message' => ">> Eメールの形式が不正です",
 												'required' => true));*/
-											
+
 		$this->validate['user_group_id'] =	array(array('rule' => VALID_NOT_EMPTY,
-														'message' => ">> グループを選択して下さい"));
+						'message' => ">> グループを選択して下さい"));
 		return true;
-		
+
 	}
 /**
  * バリデート処理
@@ -95,90 +93,84 @@ class User extends AppModel {
  * @param	array	$options
  * @return	boolean True if there are no errors
  * @access 	public
+ * TODO beforeValidateに移行する事
  */
-// TODO beforeValidateに移行する事
-	function invalidFields($options = array())
-	{
-
+	function invalidFields($options = array()) {
+	
 		$data = $this->data;
-		
-		// Id重複チェック
-		if(!$this->exists() && isset($data[$this->name]['name']) && $this->checkRepeatedId($data[$this->name]['name'])){
-			$this->invalidate('name','>> 希望されたIDは既に利用されています');
-		}
 
 		/*** パスワードチェック ***/
-		
-		if(isset($data[$this->name]['password_1']) && isset($data[$this->name]['password_2'])){
+
+		if(isset($data['User']['password_1']) && isset($data['User']['password_2'])) {
 
 			// 入力ミスチェック
-			if($data[$this->name]['password_1'] != $data[$this->name]['password_2']){
+			if($data['User']['password_1'] != $data['User']['password_2']) {
 				$this->invalidate('password','>> パスワードが同じものではありません');
-			}else{
-				$this->data[$this->name]['password'] = $this->data[$this->name]['password_1'];
+			}else {
+				$this->data['User']['password'] = $this->data['User']['password_1'];
 			}
-            $this->data[$this->name]['password'] = $this->data[$this->name]['password_1'];
-            
+			$this->data['User']['password'] = $this->data['User']['password_1'];
+
 		}
-		
+
 		$ret = parent::invalidFields($options);
 
 		// テキストボックスにエラークラスを表示させる
 		// TODO invalidFields は、エラーのあるフィールドリストを返す関数の為、処理見直しが必要かも
-		if(isset($this->validationErrors['password'])){
+		if(isset($this->validationErrors['password'])) {
 			$this->invalidate('password_1');
 			$this->invalidate('password_2');
 		}
 
 		return $ret;
-				
+
 	}
 /**
  * アカウント重複チェック
- * 
+ *
  * @param	string	user_name
  * @return	boolean
  * @access	public
  */
-	function checkRepeatedId($id){
-		
+	function checkRepeatedId($id) {
+
 		$ret = $this->find(array('name'=>$id));
-		if($ret){
+		if($ret) {
 			return true;
-		}else{
+		}else {
 			return false;
 		}
-		
+
 	}
 /**
  * コントロールソースを取得する
- * 
+ *
  * @param	string	フィールド名
  * @return	array	コントロールソース
  * @access	public
  */
-	function getControlSource($field = null){
+	function getControlSource($field = null) {
 
 		$controlSources['user_group_id'] = $this->UserGroup->find('list');
-		
-		if(isset($controlSources[$field])){
+
+		if(isset($controlSources[$field])) {
 			return $controlSources[$field];
-		}else{
+		}else {
 			return false;
 		}
-			
+
 	}
 /**
  * ユーザーリストを取得する
  * 認証ユーザーのみのリストを取得する場合は引数を指定する
- * @param array $authUser 
+ * @param array $authUser
  */
-	function getUserList($authUser=null){
+	function getUserList($authUser=null) {
 
-		if($authUser && $authUser['User']['user_group_id']!=1){
-			if($authUser['User']['real_name_2']){
+		if($authUser && $authUser['User']['user_group_id']!=1) {
+			if($authUser['User']['real_name_2']) {
 				$name = $authUser['User']['real_name_1']." ".$authUser['User']['real_name_2'];
-			}else{
+			}else {
 				$name = $authUser['User']['real_name_1'];
 			}
 			return array($authUser['User']['id']=>$name);
@@ -186,13 +178,13 @@ class User extends AppModel {
 
 		$users = $this->find("all",array('fields'=>array('id','real_name_1','real_name_2')));
 		$list = array();
-        if ($users) {
+		if ($users) {
 			// 苗字が同じ場合にわかりにくいので、foreachで生成
 			//$this->set('users',Set::combine($users, '{n}.User.id', '{n}.User.real_name_1'));
-			foreach($users as $key => $user){
-				if($user['User']['real_name_2']){
+			foreach($users as $key => $user) {
+				if($user['User']['real_name_2']) {
 					$name = $user['User']['real_name_1']." ".$user['User']['real_name_2'];
-				}else{
+				}else {
 					$name = $user['User']['real_name_1'];
 				}
 				$list[$user['User']['id']] = $name;
@@ -202,15 +194,15 @@ class User extends AppModel {
 	}
 /**
  * フォームの初期値を設定する
- * 
+ *
  * @return	array	初期値データ
  * @access	public
  */
-	function getDefaultValue(){
-		
-		$data[$this->name]['user_group_id'] = 1;
+	function getDefaultValue() {
+
+		$data['User']['user_group_id'] = 1;
 		return $data;
-		
+
 	}
 /**
  * afterFind
@@ -219,16 +211,16 @@ class User extends AppModel {
  * @param	array	$primary
  * @return	array	結果セット
  * @access	public
- */	
-	function afterFind($results, $primary = false){
-	
-		if(isset($results[0][$this->name][0])){
-			$results[0][$this->name] = $this->convertResults($results[0][$this->name]);
-		}else{
+ */
+	function afterFind($results, $primary = false) {
+
+		if(isset($results[0]['User'][0])) {
+			$results[0]['User'] = $this->convertResults($results[0]['User']);
+		}else {
 			$results = $this->convertResults($results);
 		}
 		return parent::afterFind($results,$primary);
-		
+
 	}
 /**
  * 取得結果を変換する
@@ -238,26 +230,26 @@ class User extends AppModel {
  * @param	array	結果セット
  * @return	array	結果セット
  * @access	public
- */	
-	function convertResults($results){
+ */
+	function convertResults($results) {
 
-		if($results){
-			if(isset($result[$this->name])||isset($results[0][$this->name])){
-				foreach($results as $key => $result){
-					if(isset($result[$this->name])){
-						if($result[$this->name]){
-							$results[$key][$this->name] = $this->convertToView($result[$this->name]);
+		if($results) {
+			if(isset($result['User'])||isset($results[0]['User'])) {
+				foreach($results as $key => $result) {
+					if(isset($result['User'])) {
+						if($result['User']) {
+							$results[$key]['User'] = $this->convertToView($result['User']);
 						}
-					}elseif(!empty($result)){
+					}elseif(!empty($result)) {
 						$results[$key] = $this->convertToView($result);
-					}				
+					}
 				}
-			}else{
+			}else {
 				$results = $this->convertToView($results);
 			}
 		}
 		return $results;
-		
+
 	}
 /**
  * View用のデータを取得する
@@ -266,11 +258,11 @@ class User extends AppModel {
  * @return	array	結果セット
  * @access	public
  */
-	function convertToView($data){
+	function convertToView($data) {
 
 		return $data;
-		
+
 	}
-	
+
 }
 ?>
