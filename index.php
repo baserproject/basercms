0484de93fbd2d0a505034606237c1d95838c74da
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2010-12-26 08:57:42 +0000

    test commit2
    git-svn-id: http://svn.e-catchup.jp/basercms/trunk@838 074335c2-c415-4d42-95a7-fc8f016e1c3c

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 /**
- *  Get Cake's root directory test
+ *  Get Cake's root directory
  */
 	define('APP_DIR', 'app');
 	define('DS', DIRECTORY_SEPARATOR);
