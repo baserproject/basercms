443a39913039042790e3ccb2cbc4229d4e120b69
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2011-03-15 17:26:27 +0000

    メールフォームの受信メールを管理画面で確認できるようにした fixes #723
    受信データの削除も行えるようにした
    
    git-svn-id: http://svn.e-catchup.jp/basercms/branches/dev-1.6@1023 074335c2-c415-4d42-95a7-fc8f016e1c3c

---
+++ baser/plugins/mail/controllers/mail_messages_controller.php
@@ -0,0 +1,178 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * 受信メールコントローラー
+ *
+ * PHP versions 4 and 5
+ *
+ * BaserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2010, Catchup, Inc.
+ *								9-5 nagao 3-chome, fukuoka-shi
+ *								fukuoka, Japan 814-0123
+ *
+ * @copyright		Copyright 2008 - 2010, Catchup, Inc.
+ * @link			http://basercms.net BaserCMS Project
+ * @package			baser.plugins.mail.controllers
+ * @since			Baser v 0.1.0
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://basercms.net/license/index.html
+ */
+/**
+ * 受信メールコントローラー
+ *
+ * @package			baser.plugins.mail.controllers
+ */
+class MailMessagesController extends MailAppController {
+/**
+ * クラス名
+ *
+ * @var		string
+ * @access	public
+ */
+	var $name = 'MailMessages';
+/**
+ * モデル
+ *
+ * @var		array
+ * @access	public
+ */
+	var $uses = array('Mail.MailContent', 'Mail.MailField', 'Mail.Message');
+/**
+ * ヘルパー
+ *
+ * @var		array
+ * @access	public
+ */
+	var $helpers = array('Mail.maildata', 'Mail.mailfield', 'TextEx', 'Array');
+/**
+ * メールコンテンツデータ
+ *
+ * @var		array
+ * @access	public
+ */
+	var $mailContent;
+/**
+ * サブメニュー
+ *
+ * @var		array
+ * @access	public
+ */
+	var $subMenuElements = array('mail_fields','mail_common');
+/**
+ * beforeFilter
+ *
+ * @return	void
+ * @access	public
+ */
+	function  beforeFilter() {
+		
+		parent::beforeFilter();
+		$this->MailContent->recursive = -1;
+		$this->mailContent = $this->MailContent->read(null,$this->params['pass'][0]);
+		if($this->mailContent['MailContent']['name'] != 'message') {
+			$prefix = $this->mailContent['MailContent']['name']."_";
+			$this->Message = new Message(false,null,null,$prefix);
+		}
+		$this->navis = array(
+			'メールフォーム管理'=>'/admin/mail/mail_contents/index',
+			$this->mailContent['MailContent']['title'].'管理'=>'/admin/mail/mail_fields/index/'.$this->params['pass'][0]
+		);
+		
+	}
+/**
+ * beforeRender
+ *
+ * @return	void
+ * @access	public
+ */
+	function beforeRender() {
+
+		parent::beforeRender();
+		$this->set('mailContent',$this->mailContent);
+
+	}
+/**
+ * [ADMIN] 受信メール一覧
+ *
+ * @param	string	$mailContentId
+ * @return	void
+ * @access	public
+ */
+	function admin_index($mailContentId) {
+
+		$default = array('named' => array('num' => 10));
+		$this->setViewConditions('MailMessage', array('default' => $default));
+		$this->paginate = array(
+			'fields'=>array(),
+			'order'=>'Message.created DESC',
+			'limit'=>$this->passedArgs['num']
+		);
+		$messages = $this->paginate('Message');
+		$mailFields = $this->MailField->find('all', array(
+			'conditions'=> array('MailField.mail_content_id' => $mailContentId),
+			'order'		=> 'MailField.sort'
+		));
+		$this->set(compact('mailFields'));
+		$this->set(compact('messages'));
+		$this->pageTitle = '受信メール一覧';
+
+	}
+/**
+ * [ADMIN] 受信メール詳細
+ *
+ * @param	string	$mailContentId
+ * @param	string	$messageId
+ * @return	void
+ * @access	public
+ */
+	function admin_view($mailContentId, $messageId){
+
+		if(!$mailContentId || !$messageId) {
+			$this->Session->setFlash('無効な処理です。');
+			$this->notFound();
+		}
+		$message = $this->Message->find('first', array(
+			'conditions'=>array('Message.id' => $messageId),
+			'order'=>'created DESC'
+		));
+		$mailFields = $this->MailField->find('all', array(
+			'conditions'=> array('MailField.mail_content_id' => $mailContentId),
+			'order'		=> 'MailField.sort'
+		));
+		$this->navis = am(
+			$this->navis, array('受信メール一覧'=>'/admin/mail/mail_messages/index/'.$this->params['pass'][0])
+		);
+		$this->set(compact('mailFields'));
+		$this->set(compact('message'));
+		$this->pageTitle = '受信メール詳細';
+
+	}
+/**
+ * [ADMIN] 受信メール削除
+ *
+ * @param	string	$mailContentId
+ * @param	string	$messageId
+ * @return	void
+ * @access	public
+ */
+	function admin_delete($mailContentId, $messageId) {
+
+		if(!$mailContentId || !$messageId) {
+			$this->Session->setFlash('無効な処理です。');
+			$this->notFound();
+		}
+		if($this->Message->del($messageId)) {
+			$message = $this->mailContent['MailContent']['title'].'への受信データ NO「'.$messageId.'」 を削除しました。';
+			$this->Session->setFlash($message);
+			$this->Message->saveDbLog($message);
+		}else {
+			$this->Session->setFlash('データベース処理中にエラーが発生しました。');
+		}
+		$this->redirect(array('action'=>'index',$mailContentId));
+
+	}
+
+}
+?>
---
+++ baser/plugins/mail/views/mail_messages/admin/view.ctp
@@ -0,0 +1,64 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * [ADMIN] 受信メール詳細
+ *
+ * PHP versions 4 and 5
+ *
+ * BaserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2010, Catchup, Inc.
+ *								9-5 nagao 3-chome, fukuoka-shi
+ *								fukuoka, Japan 814-0123
+ *
+ * @copyright		Copyright 2008 - 2010, Catchup, Inc.
+ * @link			http://basercms.net BaserCMS Project
+ * @package			baser.plugins.mail.views
+ * @since			Baser v 0.1.0
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://basercms.net/license/index.html
+ */
+?>
+<h2><?php $baser->contentsTitle() ?></h2>
+
+<table cellpadding="0" cellspacing="0" class="admin-row-table-01">
+	<tr><th>NO</th><td><?php echo $message['Message']['id'] ?></td></tr>
+	<tr><th>受信日時</th><td><?php echo $timeEx->format('Y/m/d H:i:s', $message['Message']['created']) ?></td></tr>
+<?php 
+$groupField = null;
+foreach($mailFields as $key => $mailField) {
+	$field = $mailField['MailField'];
+	if($field['use_field'] && $field['type'] != 'hidden') {
+		$nextKey = $key + 1;
+		/* 項目名 */
+		if ($groupField != $field['group_field']  || (!$groupField && !$field['group_field'])) {
+			echo '<tr>';
+			echo '<th class="col-head" width="160">'.$field['head'].'</th>';
+			echo '<td class="col-input">';
+		}
+		if(!empty($message['Message'][$mailField['MailField']['field_name']])) {
+			echo $field['before_attachment'];
+		}
+		if (!$field['no_send']) {
+			echo $textEx->autoLink(nl2br($maildata->control(
+				$mailField['MailField']['type'],
+				$message['Message'][$mailField['MailField']['field_name']],
+				$mailfield->getOptions($mailField['MailField'])
+			)));
+		}
+		if(!empty($message['Message'][$mailField['MailField']['field_name']])) {
+			echo $field['after_attachment'];
+		}
+		echo '&nbsp;';
+		if (($array->last($mailFields, $key)) ||
+				($field['group_field'] != $mailFields[$nextKey]['MailField']['group_field']) ||
+				(!$field['group_field'] && !$mailFields[$nextKey]['MailField']['group_field']) ||
+				($field['group_field'] != $mailFields[$nextKey]['MailField']['group_field'] && $array->first($mailFields,$key))) {
+			echo '</td></tr>';
+		}
+		$groupField=$field['group_field'];
+	}
+}
+?>
+</table>
