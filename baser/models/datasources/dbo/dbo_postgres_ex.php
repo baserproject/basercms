2c3aa10ba34160fd114ed20a3a522b5623c6aa3d
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2011-03-23 15:47:36 +0000

    PostgreSQL環境でWebページ機能・ブログ機能が正常動作しない不具合を改善 fixes #1274
    git-svn-id: http://svn.e-catchup.jp/basercms/branches/dev-1.6@1053 074335c2-c415-4d42-95a7-fc8f016e1c3c

--- baser/models/datasources/dbo/dbo_postgres_ex.php
+++ baser/models/datasources/dbo/dbo_postgres_ex.php
@@ -52,5 +52,95 @@ class DboPostgresEx extends DboPostgres {
 		return $this->execute($sql);
 		
 	}
+/**
+ * Returns a quoted and escaped string of $data for use in an SQL statement.
+ *
+ * @param string $data String to be prepared for use in an SQL statement
+ * @param string $column The column into which this data will be inserted
+ * @param boolean $read Value to be used in READ or WRITE context
+ * @return string Quoted and escaped
+ * @todo Add logic that formats/escapes data based on column type
+ */
+	function value($data, $column = null, $read = true) {
+
+		// >>> CUSTOMIZE MODIFY 2011/03/23 ryuring
+		//$parent = parent::value($data, $column);
+		// ---
+		$parent = $this->__value($data, $column);
+		// <<<
+		if ($parent != null) {
+			return $parent;
+		}
+
+		if ($data === null) {
+			return 'NULL';
+		}
+		if (empty($column)) {
+			$column = $this->introspectType($data);
+		}
+
+		switch($column) {
+			case 'binary':
+				$data = pg_escape_bytea($data);
+			break;
+			case 'boolean':
+				if ($data === true || $data === 't' || $data === 'true') {
+					return 'TRUE';
+				} elseif ($data === false || $data === 'f' || $data === 'false') {
+					return 'FALSE';
+				}
+				return (!empty($data) ? 'TRUE' : 'FALSE');
+			break;
+			case 'float':
+				if (is_float($data)) {
+					$data = sprintf('%F', $data);
+				}
+			case 'inet':
+			case 'integer':
+			case 'date':
+			case 'datetime':
+			case 'timestamp':
+			case 'time':
+				// >>> CUSTOMIZE ADD 2010/03/23 ryuring
+				// postgresql の場合、0000-00-00 00:00:00 を指定すると範囲外エラーとなる為
+				if ($data == '0000-00-00 00:00:00') {
+					return 'NULL';
+				}
+				// <<<
+				if ($data === '') {
+					return $read ? 'NULL' : 'DEFAULT';
+				}
+			default:
+				$data = pg_escape_string($data);
+			break;
+		}
+		return "'" . $data . "'";
+	}
+/**
+ * Prepares a value, or an array of values for database queries by quoting and escaping them.
+ *
+ * @param mixed $data A value or an array of values to prepare.
+ * @param string $column The column into which this data will be inserted
+ * @param boolean $read Value to be used in READ or WRITE context
+ * @return mixed Prepared value or array of values.
+ */
+	function __value($data, $column = null, $read = true) {
+		if (is_array($data) && !empty($data)) {
+			return array_map(
+				array(&$this, 'value'),
+				$data, array_fill(0, count($data), $column), array_fill(0, count($data), $read)
+			);
+		} elseif (is_object($data) && isset($data->type)) {
+			if ($data->type == 'identifier') {
+				return $this->name($data->value);
+			} elseif ($data->type == 'expression') {
+				return $data->value;
+			}
+		} elseif (in_array($data, array('{$__cakeID__$}', '{$__cakeForeignKey__$}'), true)) {
+			return $data;
+		} else {
+			return null;
+		}
+	}
 }
 ?>
