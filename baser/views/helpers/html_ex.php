f86e9c64dcd62f739a4320a2380b56ca74911944
ryuring <ryuring@074335c2-c415-4d42-95a7-fc8f016e1c3c>
2011-06-02 09:48:22 +0000

    HtmlExHelperにhtml5のdocument type 設定を追加
    git-svn-id: http://svn.e-catchup.jp/basercms/branches/dev-1.6@1299 074335c2-c415-4d42-95a7-fc8f016e1c3c

--- baser/views/helpers/html_ex.php
+++ baser/views/helpers/html_ex.php
@@ -37,6 +37,25 @@ class HtmlExHelper extends HtmlHelper {
  */
 	var $helpers = array('Javascript');
 /**
+ * Document type definitions
+ *
+ * CUSTOMIZE MODIFY 2011/06/02 ryuring
+ * html5 を追加
+ * 
+ * @var	array
+ * @access private
+ */
+	var $__docTypes = array(
+		'html5'  => '<!doctype html>',
+		'html4-strict'  => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
+		'html4-trans'  => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
+		'html4-frame'  => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">',
+		'xhtml-strict' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+		'xhtml-trans' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+		'xhtml-frame' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">',
+		'xhtml11' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'
+	);
+/**
  * 画像の即時反映用のパラメータとして乱数を出力
  *
  * @return	int		乱数
