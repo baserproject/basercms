7ed658283dc6a1332a30b4af05c2d18dcfee660c
ryuring <ryuring55@gmail.com>
2012-04-20 16:51:03 +0900

    フィードが表示されてなかったので調整

--- baser/views/layouts/ajax.php
+++ baser/views/layouts/ajax.php
@@ -2,7 +2,7 @@
 /* SVN FILE: $Id$ */
 /**
  * [PUBLISH] Ajaxレイアウト
- *
+ * 
  * PHP versions 5
  *
  * baserCMS :  Based Website Development Project <http://basercms.net>
@@ -10,12 +10,11 @@
  *
  * @copyright		Copyright 2008 - 2012, baserCMS Users Community
  * @link			http://basercms.net baserCMS Project
- * @package			baser.views.layouts
+ * @package			baser.views.layout
  * @since			baserCMS v 0.1.0
  * @version			$Revision$
  * @modifiedby		$LastChangedBy$
  * @lastmodified	$Date$
  * @license			http://basercms.net/license/index.html
  */
-include LIBS.'view'.DS.'layouts'.DS.'ajax.php';
-?>
+$baser->content() ?>
