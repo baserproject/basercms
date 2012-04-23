aa195c5a03782bd56c311b0ab6cab18d36aa23b1
ryuring <ryuring55@gmail.com>
2012-04-24 03:58:23 +0900

    不要メソッド、不要プロパティを削除

--- baser/models/behaviors/bc_cache.php
+++ baser/models/behaviors/bc_cache.php
@@ -24,13 +24,6 @@
  */
 class BcCacheBehavior extends ModelBehavior {
 /**
- * 利用状態
- * 
- * @var boolean
- * @access public
- */
-	var $enabled = true;
-/**
  * setup
  * 
  * @param Model $model
@@ -39,60 +32,6 @@ class BcCacheBehavior extends ModelBehavior {
  * @access public
  */
 	function setup(&$model, $config = array()) {}
-/**
- * キャッシュ処理
- * 
- * @param Model $model
- * @param int $expire
- * @param string $method
- * @args mixed
- * @return mixed
- * @access public
- * @deprecated readCache に移行
- */
-	function cacheMethod(&$model, $expire, $method, $args = array()){
-		
-		static $cacheData = array();
-		$this->enabled = false;
-		// キャッシュキー
-		$tableName = $model->tablePrefix.$model->table;
-		$cachekey = $tableName . '_' . $method . '_'  . $expire . '_' . md5(serialize($args));
-		// 変数キャッシュの場合
-		if(!$expire){
-			if (isset($cacheData[$cachekey])) {
-				$this->enabled = true;
-				return $cacheData[$cachekey];
-			}
-			$ret = call_user_func_array(array($model, $method), $args);
-			$this->enabled = true;
-			$cacheData[$cachekey] = $ret;
-			return $ret;
-		}
-		// サーバーキャッシュの場合
-		$ret = Cache::read($cachekey, '_cake_data_');
-		if($ret !== false){
-			$this->enabled = true;
-			if($ret == "{false}") {
-				$ret = false;
-			}
-			return $ret;
-		}
-		$ret = call_user_func_array(array(&$model, $method), $args);
-		$this->enabled = true;
-		if($ret === false) {
-			$_ret = "{false}";
-		} else {
-			$_ret = $ret;
-		}	
-		Cache::write($cachekey, $_ret, '_cake_data_');
-		// クリア用にモデル毎のキャッシュキーリストを作成
-		$cacheListKey = get_class($model) . '_cacheMethodList';
-		$list = Cache::read($cacheListKey);
-		$list[$cachekey] = 1;
-		Cache::write($cacheListKey, $list);
-		return $ret;
-		
-	}
 /**
  * キャッシュ処理
  * 
@@ -139,7 +78,7 @@ class BcCacheBehavior extends ModelBehavior {
 		$results = $db->read($model, $query);
 		Cache::write($cachekey, ($results === false)? "{false}" : $results, '_cake_data_');
 		// クリア用にモデル毎のキャッシュキーリストを作成
-		$cacheListKey = get_class($model) . '_cacheMethodList';
+		$cacheListKey = get_class($model) . '_dataCacheList';
 		$list = Cache::read($cacheListKey);
 		$list[$cachekey] = 1;
 		Cache::write($cacheListKey, $list);
@@ -155,7 +94,7 @@ class BcCacheBehavior extends ModelBehavior {
  */
 	function delCache(&$model){
 		
-		$cacheListKey = get_class($model) . '_cacheMethodList';
+		$cacheListKey = get_class($model) . '_dataCacheList';
 		$list = Cache::read($cacheListKey);
 		if(empty($list)) return;
 		foreach($list as $key => $tmp){
@@ -174,7 +113,7 @@ class BcCacheBehavior extends ModelBehavior {
  */
 	function afterSave(&$model, $created) {
 		
-		$this->deleteAssocCache($model);
+		$this->delAssockCache($model);
 		
 	}
 /**
@@ -186,7 +125,7 @@ class BcCacheBehavior extends ModelBehavior {
  */
 	function afterDelete(&$model) {
 		
-		$this->deleteAssocCache($model);
+		$this->delAssockCache($model);
 		
 	}
 /**
@@ -197,7 +136,7 @@ class BcCacheBehavior extends ModelBehavior {
  * @access public
  * @todo 現在、3階層まで再帰対応。CakePHPのrecursiveの仕組み合わせたい
  */
-	function deleteAssocCache(&$model, $recursive = 0) {
+	function delAssockCache(&$model, $recursive = 0) {
 		
 		$this->delCache($model);
 		if($recursive <= 3) {
@@ -208,7 +147,7 @@ class BcCacheBehavior extends ModelBehavior {
 					foreach($model->{$assocType} as $assoc) {
 						$className = $assoc['className'];
 						if(isset($model->{$className})) {
-							$this->deleteAssocCache($model->{$className}, $recursive);
+							$this->delAssockCache($model->{$className}, $recursive);
 						}
 					}
 				}
