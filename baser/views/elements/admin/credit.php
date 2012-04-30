806542cf9d721ab91811ec3d0548242a3ab10348
ryuring <ryuring55@gmail.com>
2012-04-30 23:08:45 +0900

    クレジットを更新

--- baser/views/elements/admin/credit.php
+++ baser/views/elements/admin/credit.php
@@ -130,56 +130,55 @@ function resizeScroll() {
 				<h1>Special Thanks Credit</h1>
 
 				<div class="section">
-					
-				<div class="section">
 					<h2>Designers</h2>
 					<ul>
 						<li><a href="http://blog.cgfm.jp/mutsuki/" target="_blank">Kazuko Kaneuchi</a> (<a href="http://twitter.com/mutsuking" target="_blank">mutsuking</a>)</li>
 						<li><a href="http://blog.clipdesign.jp/" target="_blank">Misuzu Nakamura</a> (<a href="http://twitter.com/mutsuking" target="_blank">clipdesign</a>)</li>
 					</ul>
 				</div>
-					
-				<h2>Publishers</h2>
-				<ul>
-					<li><a href="http://blog.cgfm.jp/garyu/" target="_blank">Toru Kaneuchi ( @Garyuten )</a> (<a href="http://twitter.com/Garyuten" target="_blank">garyuten</a>)</li>
-					<li>Ray Komomo (<a href="http://twitter.com/komomoray" target="_blank">komomoray</a>)</li>
-					<li><a href="http://nishiaki.probo.jp/" target="_blank">Akira Nishijima</a> (<a href="http://twitter.com/nishiaki" target="_blank">nishiaki</a>)</li>
-					<li><a href="http://d.hatena.ne.jp/dai4649/" target="_blank">Daisuke Hirata</a> (<a href="http://twitter.com/daichan4649" target="_blank">daichan4649</a>)</li>
-					<li><a href="http://yourcolor.seesaa.net/" target="_blank">Ogawa</a> (<a href="http://twitter.com/a2me" target="_blank">a2me</a>)</li>
-					<li><a href="http://www.skyld.net/" target="_blank">Shiraishi Takayoshi</a> (<a href="http://twitter.com/takapop" target="_blank">takapop</a>)</li>
-				</ul>
-				<ul>
-					<li><a href="http://waka.sadist.jp/port/" target="_blank">Miki Wakata</a> (<a href="http://twitter.com/wakakame" target="_blank">wakakame</a>)</li>
-					<li>Takahiro Wakita (<a href="http://twitter.com/waking" target="_blank">waking</a>)</li>
-					<li><a href="http://www3.coara.or.jp/~morimori/" target="_blank">Kumi Morita</a> (<a href="http://twitter.com/kun_morita" target="_blank">kun_morita</a>)</li>
-					<li><a href="http://blog.masanorihashimoto.com/" target="_blank">Masanori Hashimoto</a> (<a href="http://twitter.com/hsmt" target="_blank">hsmt</a>)</li>
-					<li><a href="http://www.bulanco.net/blog/" target="_blank">Yaive</a> (<a href="http://twitter.com/Yaive" target="_blank">Yaive</a>)</li>
-					<li>Kouyama (<a href="http://twitter.com/lupinkouyama" target="_blank">lupinkouyama</a>)</li>
-				</ul>
-				<ul>
-					<li>Murajun (<a href="http://twitter.com/murajunn" target="_blank">murajunn</a>)</li>
-					<li>Nobuhiko Yoshikawa (<a href="http://twitter.com/nobynoby" target="_blank">nobynoby</a>)</li>
-					<li><a href="http://www.alter-ego.jp/" target="_blank">Evian</a> (<a href="http://twitter.com/evian" target="_blank">evian</a>)</li>
-					<li><a href="http://apollodenki.blog97.fc2.com/" target="_blank">Apollon</a> (<a href="http://twitter.com/apollon_d" target="_blank">apollon_d</a>)</li>
-					<li><a href="http://quaris.jp" target="_blank">Nagachan</a> (<a href="http://twitter.com/QuarisJP" target="_blank">QuarisJP</a>)</li>
-					<li>Kou (<a href="http://twitter.com/k0567" target="_blank">k0567</a>)</li>
-				</ul>
-				<ul>
-					<li><a href="http://www.pictnotes.jp/" target="_blank">Sunao Kiyosue</a> (<a href="http://twitter.com/itm_kiyo" target="_blank">itm_kiyo</a>)</li>
-					<li><a href="http://www.clotho-web.com/wp/" target="_blank">Kazuki</a> (<a href="http://twitter.com/clothoweb" target="_blank">clothoweb</a>)</li>
-					<li><a href="http://www.goreydesign.com/" target="_blank">Matuki Okawa</a> (<a href="http://twitter.com/torchright" target="_blank">torchright</a>)</li>
-					<li><a href="http://one-push.info/" target="_blank">Ueda kazuhiro</a> (<a href="http://twitter.com/masyu21" target="_blank">masyu21</a>)</li>
-					<li><a href="http://jungledan.info/" target="_blank">Yasuyuki Nina</a> (<a href="http://twitter.com/jungledan" target="_blank">jungledan</a>)</li>
-					<li>Hiroyuki Kuwamura (<a href="http://twitter.com/staff_yg" target="_blank">staff_yg</a>)</li>
-				</ul>
-				<ul>
-					<!-- ここからテスター -->
-					<li><a href="http://www.arbalest.or.jp/" target="_blank">Masahiro Kawai</a> (<a href="http://twitter.com/m68k" target="_blank">m68k</a>)</li>
-					<li><a href=" http://www.yaoto.com/" target="_blank">Yasuhiro Konishi</a> (<a href="http://twitter.com/yaotosys" target="_blank">yaotosys</a>)</li>
-					<li><a href="http://www.mmpf-develop.com" target="_blank">Nobuyuki Sato</a> (<a href="http://twitter.com/mmpf_develop" target="_blank">mmpf_develop</a>)</li>
-					<li><a href="http://kototoy.jp/" target="_blank">Taku Fujita</a> (<a href="http://twitter.com/takufujita" target="_blank">takufujita</a>)</li>
-					<li><a href="http://aht-records.com" target="_blank">Ippei Suzuki</a> (<a href="http://twitter.com/aht_Record" target="_blank">aht_Record</a>)</li>
-				</ul>
+				
+				<div class="section">
+					<h2>Publishers</h2>
+					<ul>
+						<li><a href="http://blog.cgfm.jp/garyu/" target="_blank">Toru Kaneuchi ( @Garyuten )</a> (<a href="http://twitter.com/Garyuten" target="_blank">garyuten</a>)</li>
+						<li>Ray Komomo (<a href="http://twitter.com/komomoray" target="_blank">komomoray</a>)</li>
+						<li><a href="http://nishiaki.probo.jp/" target="_blank">Akira Nishijima</a> (<a href="http://twitter.com/nishiaki" target="_blank">nishiaki</a>)</li>
+						<li><a href="http://d.hatena.ne.jp/dai4649/" target="_blank">Daisuke Hirata</a> (<a href="http://twitter.com/daichan4649" target="_blank">daichan4649</a>)</li>
+						<li><a href="http://yourcolor.seesaa.net/" target="_blank">Ogawa</a> (<a href="http://twitter.com/a2me" target="_blank">a2me</a>)</li>
+						<li><a href="http://www.skyld.net/" target="_blank">Shiraishi Takayoshi</a> (<a href="http://twitter.com/takapop" target="_blank">takapop</a>)</li>
+					</ul>
+					<ul>
+						<li><a href="http://waka.sadist.jp/port/" target="_blank">Miki Wakata</a> (<a href="http://twitter.com/wakakame" target="_blank">wakakame</a>)</li>
+						<li>Takahiro Wakita (<a href="http://twitter.com/waking" target="_blank">waking</a>)</li>
+						<li><a href="http://www3.coara.or.jp/~morimori/" target="_blank">Kumi Morita</a> (<a href="http://twitter.com/kun_morita" target="_blank">kun_morita</a>)</li>
+						<li><a href="http://blog.masanorihashimoto.com/" target="_blank">Masanori Hashimoto</a> (<a href="http://twitter.com/hsmt" target="_blank">hsmt</a>)</li>
+						<li><a href="http://www.bulanco.net/blog/" target="_blank">Yaive</a> (<a href="http://twitter.com/Yaive" target="_blank">Yaive</a>)</li>
+						<li>Kouyama (<a href="http://twitter.com/lupinkouyama" target="_blank">lupinkouyama</a>)</li>
+					</ul>
+					<ul>
+						<li>Murajun (<a href="http://twitter.com/murajunn" target="_blank">murajunn</a>)</li>
+						<li>Nobuhiko Yoshikawa (<a href="http://twitter.com/nobynoby" target="_blank">nobynoby</a>)</li>
+						<li><a href="http://www.alter-ego.jp/" target="_blank">Evian</a> (<a href="http://twitter.com/evian" target="_blank">evian</a>)</li>
+						<li><a href="http://apollodenki.blog97.fc2.com/" target="_blank">Apollon</a> (<a href="http://twitter.com/apollon_d" target="_blank">apollon_d</a>)</li>
+						<li><a href="http://quaris.jp" target="_blank">Nagachan</a> (<a href="http://twitter.com/QuarisJP" target="_blank">QuarisJP</a>)</li>
+						<li>Kou (<a href="http://twitter.com/k0567" target="_blank">k0567</a>)</li>
+					</ul>
+					<ul>
+						<li><a href="http://www.pictnotes.jp/" target="_blank">Sunao Kiyosue</a> (<a href="http://twitter.com/itm_kiyo" target="_blank">itm_kiyo</a>)</li>
+						<li><a href="http://www.clotho-web.com/wp/" target="_blank">Kazuki</a> (<a href="http://twitter.com/clothoweb" target="_blank">clothoweb</a>)</li>
+						<li><a href="http://www.goreydesign.com/" target="_blank">Matuki Okawa</a> (<a href="http://twitter.com/torchright" target="_blank">torchright</a>)</li>
+						<li><a href="http://one-push.info/" target="_blank">Ueda kazuhiro</a> (<a href="http://twitter.com/masyu21" target="_blank">masyu21</a>)</li>
+						<li><a href="http://jungledan.info/" target="_blank">Yasuyuki Nina</a> (<a href="http://twitter.com/jungledan" target="_blank">jungledan</a>)</li>
+						<li>Hiroyuki Kuwamura (<a href="http://twitter.com/staff_yg" target="_blank">staff_yg</a>)</li>
+					</ul>
+					<ul>
+						<!-- ここからテスター -->
+						<li><a href="http://www.arbalest.or.jp/" target="_blank">Masahiro Kawai</a> (<a href="http://twitter.com/m68k" target="_blank">m68k</a>)</li>
+						<li><a href=" http://www.yaoto.com/" target="_blank">Yasuhiro Konishi</a> (<a href="http://twitter.com/yaotosys" target="_blank">yaotosys</a>)</li>
+						<li><a href="http://www.mmpf-develop.com" target="_blank">Nobuyuki Sato</a> (<a href="http://twitter.com/mmpf_develop" target="_blank">mmpf_develop</a>)</li>
+						<li><a href="http://kototoy.jp/" target="_blank">Taku Fujita</a> (<a href="http://twitter.com/takufujita" target="_blank">takufujita</a>)</li>
+						<li><a href="http://aht-records.com" target="_blank">Ippei Suzuki</a> (<a href="http://twitter.com/aht_Record" target="_blank">aht_Record</a>)</li>
+					</ul>
 				</div>
 
 				<div class="section">
@@ -196,7 +195,7 @@ function resizeScroll() {
 						<li><a href="http://exittunes.com/index2.html" target="_blank">Programmersan</a> (<a href="http://twitter.com/programmersan" target="_blank">programmersan</a>)</li>
 						<li><a href="http://www.panic-net.org/" target="_blank">Min</a> (<a href="http://twitter.com/min_meou" target="_blank">min_meou</a>)</li>
 						<li><a href="http://blog.grooweb.jp/" target="_blank">Youhei Nishi</a> (<a href="http://twitter.com/nippei" target="_blank">nippei</a>)</li>
-						<li>Ryuji Egashira (<a href="http://twitter.com/ryuring" target="_blank">ryuring</a>)</li>
+						<li><a href="http://ryuring.blogspot.jp/" target="_blank">Ryuji Egashira</a> (<a href="http://twitter.com/ryuring" target="_blank">ryuring</a>)</li>
 						<li>Daichi Shimoyama (<a href="http://twitter.com/daichi_shim" target="_blank">daichi_shim</a>)</li>
 						<li><a href="http://www.ls-e.com" target="_blank">Tetuya Takahashi</a></li>
 					</ul>
