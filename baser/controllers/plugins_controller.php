defa751edf1016cfc4200475c8e780bfac61ea87
ryuring <ryuring55@gmail.com>
2012-07-20 18:20:47 +0900

    プラグインの設定ファイルの場所を変更
    /app/plugins/{プラグイン名}/config/config.php
    　↓
    /app/plugins/{プラグイン名}/config.php
    ※ 互換性の為、古いパスのファイルも読み込む

--- baser/controllers/plugins_controller.php
+++ baser/controllers/plugins_controller.php
@@ -186,10 +186,18 @@ class PluginsController extends AppController {
 
 			// 設定ファイル読み込み
 			$title = $description = $author = $url = $adminLink = '';
-			$appConfigPath = APP.DS.'plugins'.DS.$plugin.DS.'config'.DS.'config.php';
-			$baserConfigPath = BASER_PLUGINS.$plugin.DS.'config'.DS.'config.php';
+			
+			// TODO 互換性のため古いパスも対応
+			$oldAppConfigPath = APP.DS.'plugins'.DS.$plugin.DS.'config'.DS.'config.php';
+			$appConfigPath = APP.DS.'plugins'.DS.$plugin.DS.'config.php';
+			if(!file_exists($appConfigPath)) {
+				$appConfigPath = $oldAppConfigPath;
+			}
+			$baserConfigPath = BASER_PLUGINS.$plugin.DS.'config.php';
 			if(file_exists($appConfigPath)) {
 				include $appConfigPath;
+			} elseif(file_exists($oldAppConfigPath)) {
+				include $oldAppConfigPath;
 			}elseif(file_exists($baserConfigPath)) {
 				include $baserConfigPath;
 			}
