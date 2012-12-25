01cc12c2c5e8730a08e4d114277e8a8d6887370a
ryuring <ryuring55@gmail.com>
2012-12-25 11:57:37 +0900

    アップデート時のログの扱いを変更
    アップデート完了後ログを画面に表示するようにした。

--- baser/views/updaters/admin/exec_script.php
+++ baser/views/updaters/admin/exec_script.php
@@ -18,13 +18,22 @@
  * @license			http://basercms.net/license/index.html
  */
 ?>
-<h2>
-	<?php $bcBaser->contentsTitle() ?>
-</h2>
 
-<div class="corner10" style="background-color:#f2f2f2;padding:15px 5px;">
+
+<div class="corner10 panel-box section">
 	<?php echo $bcForm->create(array('action' => $this->action)) ?>
 	<p><?php echo $bcForm->label('Updater.plugin', 'タイプ') ?>&nbsp;<?php echo $bcForm->input('Updater.plugin', array('type' => 'select', 'options' => $plugins, 'empty' => 'コア')) ?></p>
 	<p><?php echo $bcForm->label('Updater.version', 'バージョン') ?>&nbsp;<?php echo $bcForm->input('Updater.version', array('type' => 'text')) ?></p>
 	<?php echo $bcForm->end(array('label' => '実行', 'class' => 'button btn-red')) ?>
 </div>
+
+<?php if ($log): ?>
+<div class="corner10 panel-box section" id="UpdateLog">
+	<h2>アップデートログ</h2>
+<?php echo $bcForm->textarea('Updater.log', array(
+	'value'		=> $log, 
+	'style'		=> 'width:99%;height:200px;font-size:12px',
+	'readonly'	=> 'readonly'
+)) ?>
+</div>
+<?php endif ?>
