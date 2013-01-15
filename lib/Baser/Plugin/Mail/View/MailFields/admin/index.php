beeb744262104d7f963f2293ea7861eb1fd7c2e9
sunao kiyosue <kiyo@itm.ne.jp>
2013-01-15 19:44:02 +0900

    Mail Plugin周りの表示修正、主に、$mail を $this->mail

--- lib/Baser/Plugin/Mail/View/MailFields/admin/index.php
+++ lib/Baser/Plugin/Mail/View/MailFields/admin/index.php
@@ -17,7 +17,7 @@
  * @lastmodified	$Date$
  * @license			http://basercms.net/license/index.html
  */
-$bcBaser->js(array(
+$this->bcBaser->js(array(
 	'admin/jquery.baser_ajax_data_list', 
 	'admin/jquery.baser_ajax_batch',
 	'admin/jquery.baser_ajax_sort_table', 
@@ -36,14 +36,14 @@ $(function(){
 });
 </script>
 
-<div id="AjaxBatchUrl" style="display:none"><?php $bcBaser->url(array('controller' => 'mail_fields', 'action' => 'ajax_batch', $mailContent['MailContent']['id'])) ?></div>
-<div id="AjaxSorttableUrl" style="display:none"><?php $bcBaser->url(array('controller' => 'mail_fields', 'action' => 'ajax_update_sort', $mailContent['MailContent']['id'])) ?></div>
+<div id="AjaxBatchUrl" style="display:none"><?php $this->bcBaser->url(array('controller' => 'mail_fields', 'action' => 'ajax_batch', $mailContent['MailContent']['id'])) ?></div>
+<div id="AjaxSorttableUrl" style="display:none"><?php $this->bcBaser->url(array('controller' => 'mail_fields', 'action' => 'ajax_update_sort', $mailContent['MailContent']['id'])) ?></div>
 <div id="AlertMessage" class="message" style="display:none"></div>
 
 <div class="em-box align-left">このメールフォームのURL：
-	<?php $bcBaser->link(
-			$bcBaser->getUri('/' . $mailContent['MailContent']['name'] . '/index'),
+	<?php $this->bcBaser->link(
+			$this->bcBaser->getUri('/' . $mailContent['MailContent']['name'] . '/index'),
 			'/' . $mailContent['MailContent']['name'] . '/index') ?>
 </div>
 
-<div id="DataList"><?php $bcBaser->element('mail_fields/index_list') ?></div>
+<div id="DataList"><?php $this->bcBaser->element('mail_fields/index_list') ?></div>
--- lib/Baser/Plugin/Mail/View/MailMessages/admin/ajax_index.php
+++ lib/Baser/Plugin/Mail/View/MailMessages/admin/ajax_index.php
@@ -17,5 +17,5 @@
  * @lastmodified	$Date$
  * @license			http://basercms.net/license/index.html
  */
-$bcBaser->element('mail_messages/index_list');
+$this->bcBaser->element('mail_messages/index_list');
 ?>
