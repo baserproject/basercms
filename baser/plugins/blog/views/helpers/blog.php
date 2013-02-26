5ba592aca3fdf24aee189d20583fcab23af14e32
ryuring <ryuring55@gmail.com>
2013-02-26 16:41:17 +0900

    コード整理

--- baser/plugins/blog/views/helpers/blog.php
+++ baser/plugins/blog/views/helpers/blog.php
@@ -678,9 +678,14 @@ class BlogHelper extends AppHelper {
 	function getPostImg($post, $options = array()) {
 
 		$this->_setBlogContent($post['BlogPost']['blog_content_id']);
-		$_options = array('num' => 1, 'link' => true, 'alt' => $post['BlogPost']['name']);
-		$options = am($_options, $options);
+		$options = array_merge($_options = array(
+			'num'	=> 1, 
+			'link'	=> true,
+			'alt'	=> $post['BlogPost']['name']
+		), $options);
+		
 		extract($options);
+		
 		unset($options['num']);
 		unset($options['link']);
 
