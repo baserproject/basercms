f7402c8fd1d1adfa92d1b04abe20c031249c7faa
ryuring <ryuring55@gmail.com>
2013-04-12 15:15:07 +0900

    インストール完了後、サイト内検索の検索結果がおかしい問題を改善 fixes #3778

--- baser/models/page.php
+++ baser/models/page.php
@@ -418,7 +418,18 @@ class Page extends AppModel {
 		}
 		$_data['Content']['title'] = $data['title'];
 		$parameters = split('/', preg_replace("/^\//", '', $data['url']));
+		
+		// Viewオブジェクトを一旦削除しないと、Helper内で、View::getVar()を利用しても、
+		// 最初に生成したViewの値を使いまわしてしまうので、一旦退避させた上で削除する
+		$View = ClassRegistry::getObject('View');
+		if($View) {
+			ClassRegistry::removeObject('View');
+		}
 		$detail = $this->requestAction(array('controller' => 'pages', 'action' => 'display'), array('pass' => $parameters, 'return') );
+		if($View) {
+			ClassRegistry::addObject('View', $View);
+		}
+		
 		$detail = preg_replace('/<!-- BaserPageTagBegin -->.*?<!-- BaserPageTagEnd -->/is', '', $detail);
 		$_data['Content']['detail'] = $data['description'].' '.$detail;
 		$_data['Content']['url'] = $data['url'];
