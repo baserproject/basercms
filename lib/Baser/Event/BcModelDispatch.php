63b96cdf979acf2f3b151663a19536036358664c
ryuring <ryuring55@gmail.com>
2013-08-19 23:50:46 +0900

    イベント関連によってモデルの更新系がうまくいってなかったので取り急ぎ暫定処置

--- lib/Baser/Event/BcModelDispatch.php
+++ lib/Baser/Event/BcModelDispatch.php
@@ -14,27 +14,35 @@ class BcModelDispatch extends Object implements CakeEventListener {
 		);
 	}
 	public function beforeFind($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.beforeFind', $event->data);
 	}
 	public function afterFind($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.afterFind', $event->data);
 	}
 	public function beforeValidate($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.beforeValidate', $event->data);
 	}
 	public function afterValidate($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.afterValidate', $event->data);
 	}
 	public function beforeSave($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.beforeSave', $event->data);
 	}
 	public function afterSave($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.afterSave', $event->data);
 	}
 	public function beforeDelete($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.beforeDelete', $event->data);
 	}
 	public function afterDelete($event) {
+		return true;
 		return $event->subject->dispatchEvent($event->subject->name . '.afterDelete', $event->data);
 	}
 	
