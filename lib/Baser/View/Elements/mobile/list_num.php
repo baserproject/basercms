1fa51bc97986fc180aa28ad51a2d23bb6e148cdb
ryuring <ryuring55@gmail.com>
2014-01-20 16:39:36 +0900

    fixes #4861 スマートフォンのサイト内検索が動作しない、ページネーションのリンクが有効でない問題を改善

---
+++ lib/Baser/View/Elements/mobile/list_num.php
@@ -0,0 +1,23 @@
+<?php
+
+/* SVN FILE: $Id$ */
+/**
+ * [ADMIN] リスト設定リンク
+ *
+ * PHP versions 4 and 5
+ *
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2013, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+ *
+ * @copyright		Copyright 2008 - 2013, baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Baser.View
+ * @since			baserCMS v 2.0.0
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://basercms.net/license/index.html
+ */
+$this->request->params['action'] = str_replace('mobile_', '', $this->request->params['action']);
+include BASER_VIEWS . 'Elements' . DS . 'list_num' . $this->ext;
+$this->request->params['action'] = 'mobile_' . $this->request->params['action'];
--- lib/Baser/View/Elements/mobile/paginations/default.php
+++ lib/Baser/View/Elements/mobile/paginations/default.php
@@ -19,11 +19,11 @@
  * @license			http://basercms.net/license/index.html
  */
 if (!empty($this->Paginator)) {
-	$this->passedArgs['action'] = str_replace('mobile_', '', $this->passedArgs['action']);
-	$this->passedArgs['plugin'] = '';
+	$this->request->params['action'] = str_replace('mobile_', '', $this->request->params['action']);
 	if ($this->Paginator->counter(array('format' => '%pages%')) > 1) {
 		echo $this->Paginator->prev('<<', null, null, null) . '&nbsp;';
 		echo $this->Paginator->numbers(array('separator' => '&nbsp;', 'modulus' => 4)) . '&nbsp;';
 		echo $this->Paginator->next('>>', null, null, null);
 	}
+	$this->request->params['action'] = 'mobile_' . $this->request->params['action'];
 }
