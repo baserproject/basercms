af754af7853d8539a3efd1dcc32782deaa950356
ryuring <ryuring55@gmail.com>
2014-03-31 10:22:00 +0900

    BcUtil::unserialize() に下位互換の処理を追加

--- lib/Baser/Lib/BcUtil.php
+++ lib/Baser/Lib/BcUtil.php
@@ -207,7 +207,13 @@ class BcUtil extends Object {
  * @return mixed
  */
 	public static function unserialize($value) {
-		return unserialize(base64_decode($value));
+		$_value = $value;
+		$value = @unserialize(base64_decode($value));
+		// 下位互換の為、しばらくの間、失敗した場合の再変換を行う v.3.0.2
+		if($value === false) {
+			$value = unserialize($_value);
+		}
+		return $value;
 	}
 
 }
