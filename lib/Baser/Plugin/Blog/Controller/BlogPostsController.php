3181c319e9b07f1aa4550b9c5e8a7d2ab594a11e
arata <contact@materializing.net>
2014-07-15 15:52:28 +0900

    ブログカテゴリに管理グループを指定し、デバッグモードが有効な際に記事編集を開くとUndefinedが起きて画面が文字化け起こす点を改修

--- lib/Baser/Plugin/Blog/Controller/BlogPostsController.php
+++ lib/Baser/Plugin/Blog/Controller/BlogPostsController.php
@@ -393,6 +393,7 @@ class BlogPostsController extends BlogAppController {
 		$user = $this->BcAuth->user();
 		$editable = false;
 		$blogCategoryId = '';
+		$currentCatOwner = '';
 
 		if (isset($this->request->data['BlogPost']['blog_category_id'])) {
 			$blogCategoryId = $this->request->data['BlogPost']['blog_category_id'];
@@ -402,8 +403,8 @@ class BlogPostsController extends BlogAppController {
 		} else {
 			if (empty($this->request->data['BlogCategory']['owner_id'])) {
 				$data = $this->BlogPost->BlogCategory->find('first', array('conditions' => array('BlogCategory.id' => $this->request->data['BlogPost']['blog_category_id']), 'recursive' => -1));
+				$currentCatOwner = $data['BlogCategory']['owner_id'];
 			}
-			$currentCatOwner = $data['BlogCategory']['owner_id'];
 		}
 
 		$editable = ($currentCatOwner == $user['user_group_id'] ||
