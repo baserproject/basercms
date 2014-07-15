414bdce3e72e7fcc7077271830828eafbc7d3324
ryuring <ryuring55@gmail.com>
2014-07-15 13:41:01 +0900

    fixes #6304 Webランナーでlib/Baser/Test以下のテストケースが表示されない不具合を改善

--- lib/Baser/Lib/TestSuite/BaserTestLoader.php
+++ lib/Baser/Lib/TestSuite/BaserTestLoader.php
@@ -24,6 +24,25 @@ App::uses('CakeTestLoader', 'TestSuite');
 class BaserTestLoader extends CakeTestLoader {
 
 /**
+ * Get the list of files for the test listing.
+ *
+ * @param string $params Path parameters
+ * @return array
+ */
+	public static function generateTestList($params) {
+		$directory = self::_basePath($params);
+		$fileList = self::_getRecursiveFileList($directory);
+		$testCases = array();
+		foreach ($fileList as $testCaseFile) {
+			$case = str_replace($directory . DS, '', $testCaseFile);
+			$case = str_replace('Test.php', '', $case);
+			$testCases[$testCaseFile] = $case;
+		}
+		sort($testCases);
+		return $testCases;
+	}
+	
+/**
  * Generates the base path to a set of tests based on the parameters.
  *
  * @param array $params
