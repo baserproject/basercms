c3a54a852b426d5d4c82e343ca24d745ae5090c0
n1215 <knakae1215@gmail.com>
2014-08-09 01:04:33 +0900

    fix #6475 コンテンツナビとローカルナビの表示問題を修正

--- lib/Baser/View/Helper/BcPageHelper.php
+++ lib/Baser/View/Helper/BcPageHelper.php
@@ -67,7 +67,7 @@ class BcPageHelper extends Helper {
  */
 	public function beforeRender($viewFile) {
 		//if ($this->request->params['controller'] == 'pages' && ($this->request->params['action'] == 'display' || $this->request->params['action'] == 'smartphone_display') && isset($this->request->params['pass'][0])) {
-		if ($this->request->params['controller'] == 'pages' && preg_match('/_display$/', $this->request->params['action']) && isset($this->request->params['pass'][0])) {
+		if ($this->request->params['controller'] == 'pages' && preg_match('/(^|_)display$/', $this->request->params['action']) && isset($this->request->params['pass'][0])) {
 			// @TODO ページ機能が.html拡張子なしに統合できたらコメントアウトされたものに切り替える
 			//$this->request->data = $this->Page->findByUrl('/'.impload('/',$this->request->params['pass'][0]));
 			$param = Configure::read('BcRequest.pureUrl');
