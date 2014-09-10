fe1404490b9517f00772163ae46fa8b5d3efcf7f
ryuring <ryuring55@gmail.com>
2014-09-10 12:57:41 +0900

    モバイルにおいてメールプラグインの確認画面で、メールの確認情報などが表示されないよう調整

--- lib/Baser/Plugin/Mail/View/Elements/mobile/mail_input.php
+++ lib/Baser/Plugin/Mail/View/Elements/mobile/mail_input.php
@@ -41,8 +41,12 @@ if (!isset($blockEnd)) {
 					<font size="1"><?php echo $record['MailField']['before_attachment'] ?></font>
 				<?php endif; ?>
 				
-				<?php echo $this->Mailform->control($record['MailField']['type'], "Message." . $record['MailField']['field_name'] . "", $this->Mailfield->getOptions($record), $this->Mailfield->getAttributes($record)) ?>
-					
+				<?php if ($field['no_send'] && $freezed): ?>
+					<?php echo $this->Mailform->control('hidden', "Message." . $field['field_name'] . "", $this->Mailfield->getOptions($record), $this->Mailfield->getAttributes($record)) ?>
+				<?php else: ?>
+					<?php echo $this->Mailform->control($record['MailField']['type'], "Message." . $record['MailField']['field_name'] . "", $this->Mailfield->getOptions($record), $this->Mailfield->getAttributes($record)) ?>
+				<?php endif ?>
+
 				<?php if (!$freezed): ?>
 					<font size="1"><?php echo $record['MailField']['attention'] ?></font>
 				<?php endif; ?>
