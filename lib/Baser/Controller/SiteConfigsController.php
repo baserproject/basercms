705d489ca1eb83d25bfea4a6eadccaff7d05c3ef
ryuring <ryuring55@gmail.com>
2014-10-29 12:51:00 +0900

    デバッグモード切替の際にキャッシュが削除されてなかったので調整

--- lib/Baser/Controller/SiteConfigsController.php
+++ lib/Baser/Controller/SiteConfigsController.php
@@ -94,7 +94,7 @@ class SiteConfigsController extends AppController {
 				$siteUrl = $sslUrl = '';
 				if (isset($this->request->data['SiteConfig']['mode'])) {
 					$mode = $this->request->data['SiteConfig']['mode'];
-					if(mode > 0) {
+					if($mode > 0) {
 						clearAllCache();
 					}
 				}
