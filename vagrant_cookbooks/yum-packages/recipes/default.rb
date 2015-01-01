67837cdc492509ca71119e7bfd00ed902618959b
n1215 <knakae1215@gmail.com>
2015-01-01 21:48:40 +0900

    add unzip package

--- vagrant_cookbooks/yum-packages/recipes/default.rb
+++ vagrant_cookbooks/yum-packages/recipes/default.rb
@@ -2,7 +2,7 @@ execute "yum" do
   command "yum -y update"
 end
 
-packages = %w{ntpdate vim-enhanced git curl gd}
+packages = %w{ntpdate vim-enhanced git curl gd unzip}
 packages.each do |packagename|
   package packagename do
     action :install
