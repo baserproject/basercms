427e112b9123bb50a3494ca52ded6d94e7f2e801
ryuring <ryuring55@gmail.com>
2015-02-23 15:42:05 +0900

    フロント認証のログインとパスワードリセットのビューを追加

---
+++ lib/Baser/View/Users/login.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * [MYPAGE] ログイン
+ *
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2014, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+ *
+ * @copyright		Copyright 2008 - 2014, baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Baser.View
+ * @since			baserCMS v 3.0.7
+ * @license			http://basercms.net/license/index.html
+ */
+$this->BcBaser->js(array('admin/credit', 'admin/startup'), false);
+include BASER_VIEWS . 'Users' . DS . 'admin' . DS . 'login.php';
