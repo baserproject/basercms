7f2cc7affbe163e14a65a5093039aa6f311e846a
彌田宗利 <yada@catchup-no-iMac.local>
2015-09-11 16:39:15 +0900

    BehaviorのTestSuiteを追加

---
+++ lib/Baser/Test/Case/BcAllBehaviorsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * run all behavior baser tests
+ *
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright 2008 - 2015, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+ *
+ * @copyright		Copyright 2008 - 2015, baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @since			baserCMS v 3.0.0-beta
+ * @license			http://basercms.net/license/index.html
+ */
+
+/**
+ * @package Baser.Test.Case
+ */
+class BcAllBeHaviorTest extends CakeTestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return CakeTestSuite
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All Behavior tests');
+		$suite->addTestDirectory(BASER_TEST_CASES . DS . 'Model' . DS . 'Behavior' . DS);
+		return $suite;
+	}
+
+}
