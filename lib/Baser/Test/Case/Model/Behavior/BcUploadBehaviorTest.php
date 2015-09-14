d1104fc6d315a6efb30480b9e269cac06257c838
彌田宗利 <yada@catchup-no-iMac.local>
2015-09-14 12:46:40 +0900

    BcUploadBehaviorTest::testSaveTmpFilesの修正、Travisで成功するかテスト

--- lib/Baser/Test/Case/Model/Behavior/BcUploadBehaviorTest.php
+++ lib/Baser/Test/Case/Model/Behavior/BcUploadBehaviorTest.php
@@ -155,10 +155,10 @@ class BcUploadBehaviorTest extends BaserTestCase {
 
 		$this->initTestSaveFiles();
 
-		$data = $this->EditorTemplate->saveTmpFiles('hoge', 1);
+		$data = $this->EditorTemplate->saveTmpFiles(array('hoge'), 1);
 		$tmpId = $this->BcUploadBehavior->tmpId;
 
-		$this->assertEquals('hoge', $data, 'saveTmpFiles()の返り値が正しくありません');
+		$this->assertEquals(array('hoge'), $data, 'saveTmpFiles()の返り値が正しくありません');
 		$this->assertEquals(1, $tmpId, 'tmpIdが正しく設定されていません');
 
 		$this->deleteDummyOnTestSaveFiles();
