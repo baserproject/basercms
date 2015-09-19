d82e40e554fad0085ba859b7f76734edfe58834d
Masaharu Takishita <ecworks.masap@gmail.com>
2015-09-20 07:46:53 +0900

    centos6.7変更に伴うvagrant関連の修正

--- vagrant_cookbooks/centos/recipes/default.rb
+++ vagrant_cookbooks/centos/recipes/default.rb
@@ -1,3 +1,19 @@
+cookbook_file "/etc/sysconfig/clock" do
+    source "clock.conf"
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :run, 'execute[centos-timezone-copy]'
+end
+
+execute 'centos-timezone-copy' do
+    command <<-EOH
+        source /etc/sysconfig/clock
+        cp -pf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+    EOH
+    action :nothing
+end
+
 execute 'resolv.conf' do
     command "echo 'options single-request-reopen' >> /etc/resolv.conf"
     not_if "cat /etc/resolv.conf | grep 'options single-request-reopen'"
