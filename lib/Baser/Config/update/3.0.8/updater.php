170799255e66fd6abf311cd45a60d67dcf9effd0
ryuring <ryuring55@gmail.com>
2015-09-22 19:34:10 +0900

    v3.0.8 用アップデーターを作成

--- lib/Baser/Config/update/3.0.8/updater.php
+++ lib/Baser/Config/update/3.0.8/updater.php
@@ -1,18 +1,28 @@
 <?php
 /**
- * シリアライズされたデータを更新
+ * site_configs テーブルを更新
  *
- * SiteConfig.content_categories
+ * SiteConfig.use_universal_analytics
  * SiteConfig.content_types
  */
-App::uses('SiteConfig', 'Model');
-$SiteConfig = new SiteConfig();
-$data = $SiteConfig->findExpanded('all', array('recursive' => -1));
-$data = array_merge($data, array(
-    'use_universal_analytics' => '0'
-));
-if($SiteConfig->saveKeyValue($data)) {
-    $this->setUpdateLog('site_configs テーブルの更新に成功しました。');
-} else {
-    $this->setUpdateLog('site_configs テーブルの更新に失敗しました。', true);
-}
+    App::uses('SiteConfig', 'Model');
+    $SiteConfig = new SiteConfig();
+    $data = $SiteConfig->findExpanded('all', array('recursive' => -1));
+    $data = array_merge($data, array(
+        'use_universal_analytics' => '0'
+    ));
+    if($SiteConfig->saveKeyValue($data)) {
+        $this->setUpdateLog('site_configs テーブルの更新に成功しました。');
+    } else {
+        $this->setUpdateLog('site_configs テーブルの更新に失敗しました。', true);
+    }
+
+/**
+ * 管理システム用アセットの再デプロイ
+ */
+    $this->Components->init('BcManager');
+    if($this->BcManager->deployAdminAssets()) {
+        $this->setUpdateLog('管理システム用アセットの再配置に成功しました。');
+    } else {
+        $this->setUpdateLog('管理システム用アセットの再配置に成功しました。', true);
+    }
