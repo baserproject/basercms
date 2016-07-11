f2f3103340f8b05b160e46af970192ee3abc98e3
shutoo <yada.shuto@gmail.com>
2016-07-11 14:32:34 +0900

    BlogBaserHelperのテストを追加 - BlogBaserHelperTestクラスを追加 - BlogPostBlogBaserHelperFixtureを追加 - BlogCategoryModelFixtureの同じカテゴリ名があったのと関連ファイルを修正

---
+++ lib/Baser/Plugin/Blog/Test/Fixture/View/Helper/BlogPostBlogBaserHelperFixture.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * BlogPostFixture
+ *
+ */
+class BlogPostBlogBaserHelperFixture extends BaserTestFixture {
+/**
+ * Name of the object
+ *
+ * @var string
+ */
+  public $name = 'BlogPost';
+
+/**
+ * Records
+ *
+ * @var array
+ */
+  public $records = array(
+    array(
+      'id' => '1',
+      'blog_content_id' => '1',
+      'no' => '1',
+      'name' => 'name1',
+      'content' => 'content1',
+      'blog_category_id' => '1',
+      'user_id' => '1',
+      'status' => '1',
+      'posts_date' => '2017-02-01 12:57:59',
+      'content_draft' => '',
+      'detail_draft' => '',
+      'publish_begin' => null,
+      'publish_end' => null,
+      'exclude_search' => 0,
+      'eye_catch' => '',
+      'created' => '2017-02-01 12:57:59',
+      'modified' => '2016-01-02 12:57:59'
+    ),
+    array(
+      'id' => '2',
+      'blog_content_id' => '1',
+      'no' => '2',
+      'name' => 'name2',
+      'content' => 'content2',
+      'blog_category_id' => '2',
+      'user_id' => '1',
+      'status' => '1',
+      'posts_date' => '2016-02-02 12:57:59',
+      'content_draft' => '',
+      'detail_draft' => '',
+      'publish_begin' => null,
+      'publish_end' => null,
+      'exclude_search' => 0,
+      'eye_catch' => '',
+      'created' => '2016-02-02 12:57:59',
+      'modified' => '2016-02-02 12:57:59'
+    ),
+    array(
+      'id' => '3',
+      'blog_content_id' => '1',
+      'no' => '1',
+      'name' => 'name3',
+      'content' => 'content3',
+      'blog_category_id' => '3',
+      'user_id' => '1',
+      'status' => '1',
+      'posts_date' => '2016-01-02 12:57:59',
+      'content_draft' => '',
+      'detail_draft' => '',
+      'publish_begin' => null,
+      'publish_end' => null,
+      'exclude_search' => 0,
+      'eye_catch' => '',
+      'created' => '2016-01-02 12:57:59',
+      'modified' => '2016-01-02 12:57:59'
+    ),
+    array(
+      'id' => '4',
+      'blog_content_id' => '2',
+      'no' => '2',
+      'name' => '新商品を販売を開始しました。',
+      'content' => '',
+      'blog_category_id' => '2',
+      'user_id' => '2',
+      'status' => '1',
+      'posts_date' => '2015-01-27 12:57:59',
+      'content_draft' => '',
+      'detail_draft' => '',
+      'publish_begin' => null,
+      'publish_end' => null,
+      'exclude_search' => 0,
+      'eye_catch' => '',
+      'created' => '2015-01-27 12:56:53',
+      'modified' => '2015-01-27 12:57:59'
+    ),
+  );
+
+}
