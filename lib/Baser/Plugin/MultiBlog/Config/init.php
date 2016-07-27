270e4aec4f6a265efb47f96260dcd1cc726084f0
ryuring <ryuring55@gmail.com>
2016-07-27 22:19:42 +0900

    fix #13064 サンプルプラグインのインストーラー作成

---
+++ lib/Baser/Plugin/MultiBlog/Config/init.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Mail.Config
+ * @since			baserCMS v 0.1.0
+ * @license			http://basercms.net/license/index.html
+ */
+
+/**
+ * データベース初期化
+ */
+$this->Plugin->initDb('plugin', 'MultiBlog', array('dbDataPattern'	=> $dbDataPattern));
