98761abfe368974b4312ea13eb110774c33b0b0b
ryuring <ryuring55@gmail.com>
2016-09-13 22:41:11 +0900

    画像のリンク切れを調整

--- lib/Baser/View/Elements/admin/plugins/index_row_market.php
+++ lib/Baser/View/Elements/admin/plugins/index_row_market.php
@@ -18,7 +18,7 @@
 
 <tr>
 	<td class="row-tools">
-		<div><?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_down_on.png', array('title' => 'ダウンロード', 'alt' => 'ダウンロード')), $data['link'], array('target' => '_blank')) ?></div>
+		<div><?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_down.png', array('title' => 'ダウンロード', 'alt' => 'ダウンロード')), $data['link'], array('target' => '_blank')) ?></div>
 	</td>
 	<td>
 		<?php echo $data['title'] ?>
