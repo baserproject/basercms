67a5c414af46656fb2cf88228075e9901a35d01f
ryuring <ryuring55@gmail.com>
2016-09-20 15:50:09 +0900

    リクエスト調整追加

--- lib/Baser/View/PageCategories/admin/form.php
+++ lib/Baser/View/PageCategories/admin/form.php
@@ -77,7 +77,7 @@ function pageTypeChengeHandler() {
 	});
 	
 	$.ajax({
-		type: "POST",
+		type: "GET",
 		url: $("#AjaxCategorySourceUrl").html()+'/'+pageType,
 		data: options,
 		beforeSend: function() {
