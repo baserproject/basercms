4eb2e8b1e178dd902e8d1bedb4cc521acd9d4f34
ryuring <ryuring55@gmail.com>
2016-09-24 20:25:12 +0900

    fix #14138 ブログのコメント入力欄に必須マークがないので調整

--- lib/Baser/Config/theme/bccolumn/Elements/blog_comments.php
+++ lib/Baser/Config/theme/bccolumn/Elements/blog_comments.php
@@ -17,9 +17,9 @@ if ($this->request->params['Site']['alias']) {
 			return false;
 		});
 	});
-/**
- * コメントを送信する
- */
+	/**
+	 * コメントを送信する
+	 */
 	function sendComment() {
 		var msg = '';
 		if (!$("#BlogCommentName").val()) {
@@ -28,11 +28,11 @@ if ($this->request->params['Site']['alias']) {
 		if (!$("#BlogCommentMessage").val()) {
 			msg += 'コメントを入力してください\n';
 		}
-<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
-			if (!$("#BlogCommentAuthCaptcha").val()) {
-				msg += '画象の文字を入力してください\n';
-			}
-<?php endif ?>
+		<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
+		if (!$("#BlogCommentAuthCaptcha").val()) {
+			msg += '画象の文字を入力してください\n';
+		}
+		<?php endif ?>
 		if (!msg) {
 			$.ajax({
 				url: $("#BlogCommentAddForm").attr('action'),
@@ -45,24 +45,24 @@ if ($this->request->params['Site']['alias']) {
 				},
 				success: function(result) {
 					if (result) {
-<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
-							loadAuthCaptcha();
-<?php endif ?>
+						<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
+						loadAuthCaptcha();
+						<?php endif ?>
 						$("#BlogCommentName").val('');
 						$("#BlogCommentEmail").val('');
 						$("#BlogCommentUrl").val('');
 						$("#BlogCommentMessage").val('');
 						$("#BlogCommentAuthCaptcha").val('');
 						var resultMessage = '';
-<?php if ($blogContent['BlogContent']['comment_approve']): ?>
-							resultMessage = '送信が完了しました。送信された内容は確認後公開させて頂きます。';
-	<?php else: ?>
-							var comment = $(result);
-							comment.hide();
-							$("#BlogCommentList").append(comment);
-							comment.show(500);
-							resultMessage = 'コメントの送信が完了しました。';
-<?php endif ?>
+						<?php if ($blogContent['BlogContent']['comment_approve']): ?>
+						resultMessage = '送信が完了しました。送信された内容は確認後公開させて頂きます。';
+						<?php else: ?>
+						var comment = $(result);
+						comment.hide();
+						$("#BlogCommentList").append(comment);
+						comment.show(500);
+						resultMessage = 'コメントの送信が完了しました。';
+						<?php endif ?>
 						$.ajax({
 							url: $("#BlogCommentGetTokenUrl").html(),
 							type: 'GET',
@@ -74,9 +74,9 @@ if ($this->request->params['Site']['alias']) {
 						$("#ResultMessage").html(resultMessage);
 						$("#ResultMessage").slideDown();
 					} else {
-<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
-							loadAuthCaptcha();
-<?php endif ?>
+						<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
+						loadAuthCaptcha();
+						<?php endif ?>
 						$("#ResultMessage").html('コメントの送信に失敗しました。入力内容を見なおしてください。');
 						$("#ResultMessage").slideDown();
 					}
@@ -92,9 +92,9 @@ if ($this->request->params['Site']['alias']) {
 			alert(msg);
 		}
 	}
-/**
- * キャプチャ画像を読み込む
- */
+	/**
+	 * キャプチャ画像を読み込む
+	 */
 	function loadAuthCaptcha() {
 
 		var src = $("#BlogCommentCaptchaUrl").html() + '?' + Math.floor(Math.random() * 100);
@@ -125,18 +125,18 @@ if ($this->request->params['Site']['alias']) {
 		</div>
 
 		<div id="CommentForm">
-		<h3>コメント送信フォーム</h3>
-		<?php echo $this->BcForm->create('BlogComment', array('url' => $prefix . '/blog/blog_comments/add/' . $blogContent['BlogContent']['id'] . '/' . $post['BlogPost']['id'], 'id' => 'BlogCommentAddForm')) ?>
-		<?php echo $this->BcForm->input('BlogComment.captcha_id', ['type' => 'hidden', 'value' => $captchaId]) ?>
-			
-		<table cellpadding="0" cellspacing="0" class="row-table-01">
-			<tbody>
+			<h3>コメント送信フォーム</h3>
+			<?php echo $this->BcForm->create('BlogComment', array('url' => $prefix . '/blog/blog_comments/add/' . $blogContent['BlogContent']['id'] . '/' . $post['BlogPost']['id'], 'id' => 'BlogCommentAddForm')) ?>
+			<?php echo $this->BcForm->input('BlogComment.captcha_id', ['type' => 'hidden', 'value' => $captchaId]) ?>
+
+			<table cellpadding="0" cellspacing="0" class="row-table-01">
+				<tbody>
 				<tr>
-					<th><?php echo $this->BcForm->label('BlogComment.name', 'お名前') ?></th>
+					<th><?php echo $this->BcForm->label('BlogComment.name', 'お名前') ?><span style="color:red">＊</span></th>
 					<td><?php echo $this->BcForm->input('BlogComment.name', array('type' => 'text', 'class' => 'form-m')) ?></td>
 				</tr>
 				<tr>
-					<th><?php echo $this->BcForm->label('BlogComment.email', 'Eメール') ?></th>
+					<th><?php echo $this->BcForm->label('BlogComment.email', 'Eメール') ?><span style="color:red">＊</span></th>
 					<td>
 						<?php echo $this->BcForm->input('BlogComment.email', array('type' => 'text', 'size' => 30, 'class' => 'form-m')) ?><br>
 						<small>※ メールは公開されません</small>
@@ -147,22 +147,22 @@ if ($this->request->params['Site']['alias']) {
 					<td><?php echo $this->BcForm->input('BlogComment.url', array('type' => 'text', 'size' => 30, 'class' => 'form-l')) ?></td>
 				</tr>
 				<tr>
-					<th><?php echo $this->BcForm->label('BlogComment.message', 'コメント') ?></th>
+					<th><?php echo $this->BcForm->label('BlogComment.message', 'コメント') ?><span style="color:red">＊</span></th>
 					<td><?php echo $this->BcForm->input('BlogComment.message', array('type' => 'textarea', 'rows' => 10, 'cols' => 52, 'class' => 'form-l')) ?></td>
 				</tr>
-			</tbody>
-		</table>
+				</tbody>
+			</table>
 
-		<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
-			<div class="auth-captcha clearfix">
-				<img src="" alt="認証画象" class="auth-captcha-image" id="AuthCaptchaImage" style="display:none" />
-				<?php $this->BcBaser->img('admin/captcha_loader.gif', array('alt' => 'Loading...', 'class' => 'auth-captcha-image', 'id' => 'CaptchaLoader')) ?>
-				<?php echo $this->BcForm->text('BlogComment.auth_captcha') ?><br />
-				&nbsp;画像の文字を入力してください<br />
-			</div>
-		<?php endif ?>
+			<?php if ($blogContent['BlogContent']['auth_captcha']): ?>
+				<div class="auth-captcha clearfix">
+					<img src="" alt="認証画象" class="auth-captcha-image" id="AuthCaptchaImage" style="display:none" />
+					<?php $this->BcBaser->img('admin/captcha_loader.gif', array('alt' => 'Loading...', 'class' => 'auth-captcha-image', 'id' => 'CaptchaLoader')) ?>
+					<?php echo $this->BcForm->text('BlogComment.auth_captcha') ?><br />
+					&nbsp;画像の文字を入力してください<br />
+				</div>
+			<?php endif ?>
 
-		<?php echo $this->BcForm->end(array('label' => '送信する', 'id' => 'BlogCommentAddButton', 'class' => 'button')) ?>
+			<?php echo $this->BcForm->end(array('label' => '送信する', 'id' => 'BlogCommentAddButton', 'class' => 'button')) ?>
 			<div id="ResultMessage" class="message" style="display:none;text-align:center">&nbsp;</div>
 		</div>
 	</div>
