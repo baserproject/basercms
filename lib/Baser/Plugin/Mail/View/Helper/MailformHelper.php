52377c67108e4d79312f542cbb38b04160d4857d
arata <contact@materializing.net>
2016-10-03 06:23:56 +0900

    Add メールフォームのフィールドタイプに 西暦日付 を実装

--- lib/Baser/Plugin/Mail/View/Helper/MailformHelper.php
+++ lib/Baser/Plugin/Mail/View/Helper/MailformHelper.php
@@ -162,6 +162,23 @@ class MailformHelper extends BcFreezeHelper {
 				$out = $this->dateTime($fieldName, 'WMD', null, $attributes);
 				break;
 
+			case 'date_time_seireki':
+				unset($attributes['size']);
+				unset($attributes['rows']);
+				unset($attributes['maxlength']);
+				unset($attributes['empty']);
+				$attributes['monthNames'] = false;
+				$attributes['separator'] = '&nbsp;';
+
+				if (isset($attributes['minYear']) && $attributes['minYear'] == 'today') {
+					$attributes['minYear'] = intval(date('Y'));
+				}
+				if (isset($attributes['maxYear']) && $attributes['maxYear'] == 'today') {
+					$attributes['maxYear'] = intval(date('Y'));
+				}
+				$out = $this->dateTime($fieldName, 'YMD', null, $attributes);
+				break;
+
 			case 'textarea':
 				$attributes['cols'] = $attributes['size'];
 				unset($attributes['separator']);
