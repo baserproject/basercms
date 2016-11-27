8bb3b9d1aa58d162af5a9b98b66025bb3fa72142
kiyosue <kiyo@itm.co.jp>
2016-11-27 13:29:16 +0900

    #fixes データベースのバージョンをsetting.phpに記載

--- lib/Baser/Controller/InstallationsController.php
+++ lib/Baser/Controller/InstallationsController.php
@@ -558,16 +558,16 @@ class InstallationsController extends AppController {
 			switch (get_class($db)) {
 				case 'BcMysql' :
 					$result = $db->query("SELECT version() as version");
-					if( version_compare($result[0][0]['version'], '5.0.0') == -1 ) {
-						$this->setMessage("データベースのバージョンを確認してください。", true);
+					if( version_compare($result[0][0]['version'], Configure::read('BcRequire.MySQLVersion')) == -1 ) {
+						$this->setMessage("データベースのバージョンが " . Configure::read('BcRequire.MySQLVersion') . " 以上か確認してください。", true);
 						return false ;
 					}
 					break;
 				case 'BcPostgres' :
 					$result = $db->query("SELECT version() as version");
 					list(,$version) = explode(" ",$result[0][0]['version']);
-					if( version_compare( trim($version), '8.4.0') == -1 ) {
-						$this->setMessage("データベースのバージョンを確認してください。", true);
+					if( version_compare( trim($version), Configure::read('BcRequire.PostgreSQLVersion')) == -1 ) {
+						$this->setMessage("データベースのバージョンが " . Configure::read('BcRequire.PostgreSQLVersion') . " 以上か確認してください。", true);
 						return false ;
 					}
 					break;
