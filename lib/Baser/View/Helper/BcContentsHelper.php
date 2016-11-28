f9fb37f037c84c7cbb9e42bc4e56f81b7efe3da6
kato <kato@e-catchup.jp>
2016-11-28 19:28:27 +0900

    isActionAvailable テスト　コミット

--- lib/Baser/View/Helper/BcContentsHelper.php
+++ lib/Baser/View/Helper/BcContentsHelper.php
@@ -54,6 +54,7 @@ class BcContentsHelper extends AppHelper {
  */
 	public function setup() {
 		$settings = $this->_View->get('contentsSettings');
+                
 		if(!$settings) {
 			return;
 		}
@@ -108,8 +109,19 @@ class BcContentsHelper extends AppHelper {
 		$this->settings = $settings;
 	}
 	
-	public function isActionAvailable($type, $action, $entityId) {
+/**
+ * アクションが利用可能かどうか確認する
+ *
+ * @param string $type コンテンツタイプ
+ * @param string $action アクション
+ * @param string $type コンテンツを特定するID
+ * @return bool
+ */
+        public function isActionAvailable($type, $action, $entityId) {
 		$user = BcUtil::loginUser('admin');
+                if(!isset($this->settings[$type]['url'][$action])) {
+                    return false;
+                }
 		$url = $this->settings[$type]['url'][$action] . '/' . $entityId;
 		return $this->_Permission->check($url, $user['user_group_id']);
 	}
@@ -395,5 +407,5 @@ class BcContentsHelper extends AppHelper {
 			return false;
 		}
 	}
-	
+
 }
