2c47c7dabce7bc4f84317f115cb0ea4b3fa5e0aa
kato <kato@e-catchup.jp>
2016-12-05 15:49:40 +0900

    #14947管理画面のブログ記事一覧で、アイキャッチを使用するとカラムがずれて表示される 固定表示に変更

--- lib/Baser/Plugin/Blog/View/Elements/admin/blog_posts/index_row.php
+++ lib/Baser/Plugin/Blog/View/Elements/admin/blog_posts/index_row.php
@@ -35,13 +35,11 @@ $class = ' class="' . implode(' ', $classies) . '"';
 	</td>
 	<td><?php echo $data['BlogPost']['no']; ?></td>
 	<td><?php echo $this->BcTime->format('Y-m-d', $data['BlogPost']['posts_date']); ?></td>
-	<?php if ($existEyeCatch): ?>
-            <?php if (Hash::get($data, 'BlogPost.eye_catch')): ?>
-                <td class="eye_catch"><?php echo $this->BcUpload->uploadImage('BlogPost.eye_catch',  $data['BlogPost']['eye_catch'], array('imgsize' => 'mobile_thumb')) ?></td>
-            <?php else: ?>
-                <td class="eye_catch"></td>
-            <?php endif ?>
-        <?php endif ?>
+<?php if (Hash::get($data, 'BlogPost.eye_catch')): ?>
+    <td class="eye_catch"><?php echo $this->BcUpload->uploadImage('BlogPost.eye_catch',  $data['BlogPost']['eye_catch'], array('imgsize' => 'mobile_thumb')) ?></td>
+<?php else: ?>
+    <td class="eye_catch"></td>
+<?php endif ?>
 	<td>
 		<?php if (!empty($data['BlogCategory']['title'])): ?>
 			<?php echo $data['BlogCategory']['title']; ?>
