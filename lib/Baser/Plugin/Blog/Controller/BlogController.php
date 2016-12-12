b6fd9a7dbe5457e7f9a72a6d09441e11f217519b
sakaguchi <sakaguchi@e-catchup.jp>
2016-12-12 10:20:36 +0900

    fixes #15071 ブログ詳細の詳細画面の「前に戻る」「次に進む」のリンク先が正しくない場合がある。

--- lib/Baser/Plugin/Blog/Controller/BlogController.php
+++ lib/Baser/Plugin/Blog/Controller/BlogController.php
@@ -718,7 +718,7 @@ class BlogController extends BlogAppController {
 					break;
 			}
 		} else {
-			$order = "BlogPost.{$sort} {$direction}, BlogPost.id";
+			$order = "BlogPost.{$sort} {$direction}, BlogPost.id {$direction}";
 		}
 
 		// 毎秒抽出条件が違うのでキャッシュしない
