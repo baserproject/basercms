0b462c144be7e30caafcea50dfc88bc6041ea33b
ryuring <ryuring55@gmail.com>
2017-02-19 13:57:35 +0900

    fix #16183 メールフォームの必須項目についてアスタリスク（＊）ではなく、必須と表示するようにした

--- lib/Baser/Plugin/Mail/View/Elements/mail_input.php
+++ lib/Baser/Plugin/Mail/View/Elements/mail_input.php
@@ -38,7 +38,9 @@ if (!empty($mailFields)) {
 				}
 				echo '>' . "\n" . '        <th class="col-head" width="150">' . $this->Mailform->label("MailMessage." . $field['field_name'] . "", $field['head']);
 				if ($field['not_empty']) {
-					echo '<span class="required">*</span>';
+					echo '<span class="required">必須</span>';
+				} else {
+					echo '<span class="normal">任意</span>';
 				}
 				echo '</th>' . "\n" . '        <td class="col-input">';
 			}
