f8b7483bc6e270b6bb4f8a86024fef99eee4f5e5
takumi <takumi@takumi-no-MacBook-Air.local>
2017-03-10 13:26:54 +0900

    testControl追加

---
+++ lib/Baser/Plugin/Mail/Test/Case/View/Helper/MaildateHelperTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class MaildateHelperTest extends BaserTestCase {
+
+    public function testControl() {
+        $this->markTestIncomplete('このメソッドは、同一クラス内のメソッドをラッピングしているメソッドの為スキップします。');
+    }
+
+    public function testToDisplayString () {
+
+    }
+}
