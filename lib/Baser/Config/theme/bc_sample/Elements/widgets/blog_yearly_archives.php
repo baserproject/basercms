02a3dfda932e3dd7644d39b8e7c9f68eb08001b1
kato <kato@e-catchup.jp>
2017-07-31 16:59:20 +0900

    #17885 ブログウィジェット 月別・年別アーカイブのリンクURLに現在のURLが入ってしまう問題の解消 年別アーカイブの不要ソースの削除

--- lib/Baser/Config/theme/bc_sample/Elements/widgets/blog_yearly_archives.php
+++ lib/Baser/Config/theme/bc_sample/Elements/widgets/blog_yearly_archives.php
@@ -61,7 +61,7 @@ $baseCurrentUrl = $this->request->params['Content']['name'] . '/archives/date/';
 					<?php $title = $postedDate['year'] . '年' ?>
 				<?php endif ?>
 				<li<?php echo $class ?>>
-					<?php $this->BcBaser->link($title, $this->BcBaser->getContentByEntityId($id, 'BlogContent', 'url') . '/archives/date/' . $postedDate['year'] . '/' . $postedDate['month']) ?>
+					<?php $this->BcBaser->link($title, $this->BcBaser->getContentByEntityId($id, 'BlogContent', 'url') . '/archives/date/' . $postedDate['year']) ?>
 				</li>
 			<?php endforeach; ?>
 		</ul>
