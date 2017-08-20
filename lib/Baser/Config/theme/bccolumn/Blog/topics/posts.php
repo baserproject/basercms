31bd261ae10dec6c0007888d5f1fe206d9037ce9
ryuring <ryuring55@gmail.com>
2017-08-20 10:48:34 +0900

    fix #18268 標準テーマbccolumnのトップページの画像リンクの不具合を改善

--- lib/Baser/Config/theme/bccolumn/Blog/topics/posts.php
+++ lib/Baser/Config/theme/bccolumn/Blog/topics/posts.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * トップページ記事一覧
+ * @var array $posts
  */
 ?>
 
@@ -14,10 +15,9 @@
 			<?php elseif ($this->BcArray->last($posts, $key)): ?>
 				<?php $class[] = 'last' ?>
 			<?php endif ?>
-			<?php $uri = $this->BcBaser->getRoot().$post['BlogContent']['Content']['name'].'/archives/'.$post['BlogPost']['no']; ?>
 			<li class="<?php echo implode(' ', $class) ?>">
 				<div class="thumbnail">
-					<a href="<?php echo $uri ?>">
+					<a href="<?php echo $this->Blog->getPostLinkUrl($post) ?>">
 						<?php $this->Blog->eyeCatch($post, array('link'=>false, 'width'=>'80px', 'noimage'=>'/theme/bccolumn/img/blog/works/noimage.png')) ?>
 					</a>
 				</div>
