b25511130dd09e097cd5fb71d5e6274a50d1f0d2
takumi <kinzi0828@yahoo.co.jp>
2017-08-23 15:19:32 +0900

    vaddy修正

--- secure_test/vaddy.sh
+++ secure_test/vaddy.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-go-vaddy/bin/vaddy-linux-64bit $VADDY_AUTH_KEY $VADDY_USER $VADDY_FQDN | tee result.txt
+git clone https://github.com/vaddy/go-vaddy.git;
 
-msg=`cat result.txt`
-#echo "$msg" | ./slack_webhooks.sh
+while :
+do
+    timeout 2 go-vaddy/bin/vaddy-macosx-64bit $VADDY_AUTH_KEY $VADDY_USER $VADDY_FQDN | tee result.txt
+    msg=`cat result.txt`
 
-if [[ $msg =~ "Scan has already been running" ]]; then
-    while :
-    do
-      echo "既に他のテスト実行中のため５分後にもう一度テストリクエストします。"
-      sleep 300
-      go-vaddy/bin/vaddy-linux-64bit $VADDY_AUTH_KEY $VADDY_USER $VADDY_FQDN | tee result.txt
-      msg=`cat result.txt`
-      if [[ ! $msg =~ "Scan has already been running" ]]; then
+    if [[ $msg =~ "Scan has already been running" ]]; then
+          echo "既に他のテスト実行中のため５分後にもう一度テストリクエストします。"
+          sleep 2
+    fi
+
+    if [[ ! $msg =~ "Scan has already been running" ]]; then
         break
-      fi
-    done
-fi
+    fi
+
+done
+
+openssl aes-256-cbc -k "$SERVER_KEY" -in deploy_key.enc -d -a -out deploy.key
+cp deploy.key ~/.ssh/
+chmod 600 ~/.ssh/deploy.key
+ssh -i ~/.ssh/deploy.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $TEST_SERVER 'bash -s' < script.sh
+
+#MEMO:
+# timeoutコマンドにより、Scanキャンセルが正常にできないため、２回目(pull後)のテスト実行ができないが、
+# テスト中にpullを実行していて影響がでていると考えられるため保留
+#go-vaddy/bin/vaddy-macosx-64bit $VADDY_AUTH_KEY $VADDY_USER $VADDY_FQDN | tee result.txt
+#msg=`cat result.txt`
+
+#MEMO: slack通知スクリプト
+#echo "$msg" | ./slack_webhooks.sh
