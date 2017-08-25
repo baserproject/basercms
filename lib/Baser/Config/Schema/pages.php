83cfdcd4b37f77fbcf2e34701c54f5d9e55d06ab
takumi <kinzi0828@yahoo.co.jp>
2017-08-25 16:55:58 +0900

    Baser/Config/Schema  Array()を[]に変更3

--- lib/Baser/Config/Schema/pages.php
+++ lib/Baser/Config/Schema/pages.php
@@ -10,7 +10,7 @@ class PagesSchema extends CakeSchema {
 
 	public $connection = 'default';
 
-	public function before($event = array()) {
+	public function before($event = []) {
 		return true;
 	}
 
@@ -32,16 +32,16 @@ class PagesSchema extends CakeSchema {
 		}
 	}
 
-	public $pages = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'key' => 'primary'),
-		'contents' => array('type' => 'text', 'null' => true, 'default' => null),
-		'draft' => array('type' => 'text', 'null' => true, 'default' => null),
-		'page_template' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'code' => array('type' => 'text', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci')
-	);
+	public $pages = [
+		'id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'key' => 'primary'],
+		'contents' => ['type' => 'text', 'null' => true, 'default' => null],
+		'draft' => ['type' => 'text', 'null' => true, 'default' => null],
+		'page_template' => ['type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
+		'code' => ['type' => 'text', 'null' => true, 'default' => null],
+		'modified' => ['type' => 'datetime', 'null' => true, 'default' => null],
+		'created' => ['type' => 'datetime', 'null' => true, 'default' => null],
+		'indexes' => ['PRIMARY' => ['column' => 'id', 'unique' => 1]],
+		'tableParameters' => ['charset' => 'utf8', 'collate' => 'utf8_general_ci']
+	];
 
 }
