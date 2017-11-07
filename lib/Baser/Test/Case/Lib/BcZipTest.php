ac3bbe8ee153fa73b6d754abff67e0b51979df02
Tommy <tommyt6073@gmail.com>
2017-11-07 15:59:30 +0900

    Lib/BcZipに未実装テストメソッド追加

---
+++ lib/Baser/Test/Case/Lib/BcZipTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Baser.Test.Case.Lib
+ * @since			baserCMS v 4.0.9
+ * @license			http://basercms.net/license/index.html
+ */
+App::uses('BcZip', 'Lib');
+
+/**
+ * BcZipクラスのテスト
+ *
+ * @package Baser.Test.Case.Lib
+ */
+class BcZipTest extends BaserTestCase {
+
+/**
+ * setUp
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+/**
+ * BcZip constructor.
+ */
+	public function test__construct() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * ZIP を展開する
+ */
+	public function testExtract() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+}
