190680654ab464f4d1f151743cbd77f2100d8245
Tommy <tommyt6073@gmail.com>
2017-11-09 14:44:56 +0900

    Controllerディレクトリの各テストの雛形を新規作成

---
+++ lib/Baser/Test/Case/Controller/FavoritesControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Baser.Test.Case.Controller
+ * @since			baserCMS v 4.0.9
+ * @license			http://basercms.net/license/index.html
+ */
+
+App::uses('FavoritesController', 'Controller');
+
+/**
+ * FavoritesControllerTest class
+ *
+ * @package Baser.Test.Case.Controller
+ * @property  FavoritesController $FavoritesController
+ */
+class FavoritesControllerTest extends BaserTestCase {
+
+/**
+ * set up
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+}
---
+++ lib/Baser/Test/Case/Controller/PluginsControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Baser.Test.Case.Controller
+ * @since			baserCMS v 4.0.9
+ * @license			http://basercms.net/license/index.html
+ */
+
+App::uses('PluginsController', 'Controller');
+
+/**
+ * PluginsControllerTest class
+ *
+ * @package Baser.Test.Case.Controller
+ * @property  PluginsController $PluginsController
+ */
+class PluginsControllerTest extends BaserTestCase {
+
+/**
+ * set up
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+}
