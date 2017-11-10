b7327754c83722f19527dc621a58e360f24f51cf
Tommy <tommyt6073@gmail.com>
2017-11-10 11:46:27 +0900

    Controller/SitesControllerに未実装テストメソッド追加

--- lib/Baser/Test/Case/Controller/SitesControllerTest.php
+++ lib/Baser/Test/Case/Controller/SitesControllerTest.php
@@ -38,4 +38,60 @@ class SitesControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * Before Filter
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * サブサイト一覧
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * サブサイト追加
+ */
+	public function testAdmin_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * サブサイト情報編集
+ */
+	public function testAdmin_edit() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 公開状態にする
+ */
+	public function testAdmin_ajax_unpublish() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 非公開状態にする
+ */
+	public function testAdmin_ajax_publish() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 削除する
+ */
+	public function testAdmin_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 選択可能なデバイスと言語の一覧を取得する
+ */
+	public function testAdmin_ajax_get_selectable_devices_and_lang() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
 }
