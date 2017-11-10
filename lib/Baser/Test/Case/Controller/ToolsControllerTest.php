d62c805eafb7e26c58bf8a3788beed7d539444fa
Tommy <tommyt6073@gmail.com>
2017-11-10 12:14:21 +0900

    Controller/ToolsControllerに未実装テストメソッド追加

--- lib/Baser/Test/Case/Controller/ToolsControllerTest.php
+++ lib/Baser/Test/Case/Controller/ToolsControllerTest.php
@@ -38,4 +38,69 @@ class ToolsControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * ユーティリティ
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * データメンテナンス
+ */
+	public function testAdmin_maintenance() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * モデル名からスキーマファイルを生成する
+ */
+	public function testAdmin_write_schema() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * スキーマファイルを読み込みテーブルを生成する
+ */
+	public function testAdmin_load_schema() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * ログメンテナンス
+ */
+	public function testAdmin_log() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 管理システム用アセットファイルを削除する
+ */
+	public function testAdmin_delete_admin_assets() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 管理システム用アセットファイルを再配置する
+ */
+	public function testAdmin_deploy_admin_assets() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * コンテンツ管理のツリー構造をリセットする
+ */
+	public function testAdmin_reset_contents_tree() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * コンテンツ管理のツリー構造のチェックを行う
+ *
+ * 問題がある場合にはログを出力する
+ */
+	public function testAdmin_verity_contents_tree() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
 }
