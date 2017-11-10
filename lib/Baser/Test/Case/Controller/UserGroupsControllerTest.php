a20b1f3c65465c1f86fe212b5156f15dd9a24aac
Tommy <tommyt6073@gmail.com>
2017-11-10 12:57:12 +0900

    Controller/UserGroupsControllerに未実装テストメソッド追加

--- lib/Baser/Test/Case/Controller/UserGroupsControllerTest.php
+++ lib/Baser/Test/Case/Controller/UserGroupsControllerTest.php
@@ -38,4 +38,60 @@ class UserGroupsControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * beforeFilter
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * ユーザーグループの一覧を表示する
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 登録処理
+ */
+	public function testAdmin_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 編集処理
+ */
+	public function testAdmin_edit() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理 (ajax)
+ */
+	public function testAdmin_ajax_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理
+ */
+	public function testAdmin_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] データコピー（AJAX）
+ */
+	public function testAdmin_ajax_copy() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * ユーザーグループのよく使う項目の初期値を登録する
+ */
+	public function testAdmin_set_default_favorites() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
 }
