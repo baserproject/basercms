845c4a78782e69ef135dcf54945fc8db2a27a051
Tommy <tommyt6073@gmail.com>
2017-11-10 14:37:33 +0900

    Eventディレクトリの各テストの雛形を新規作成

---
+++ lib/Baser/Test/Case/Event/BcModelEventDispatcherTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Baser.Test.Case.Event
+ * @since			baserCMS v 4.0.9
+ * @license			http://basercms.net/license/index.html
+ */
+
+App::uses('BcModelEventDispatcher', 'Event');
+
+/**
+ * BcModelEventDispatcherTest class
+ *
+ * @package Baser.Test.Case.Event
+ * @property  BcModelEventDispatcher $BcModelEventDispatcher
+ */
+class BcModelEventDispatcherTest extends BaserTestCase {
+
+/**
+ * set up
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+}
