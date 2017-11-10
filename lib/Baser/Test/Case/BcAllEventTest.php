cb5a810fecd22ea2c1c52a8084e58943c08ec3f8
Tommy <tommyt6073@gmail.com>
2017-11-10 14:29:31 +0900

    Eventのテストを動作させるためのファイルと記述を追加

---
+++ lib/Baser/Test/Case/BcAllEventTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Baser.Test.Case
+ * @since			baserCMS v 4.0.9
+ * @license			http://basercms.net/license/index.html
+ */
+
+/**
+ * run all event baser tests
+ *
+ * @package Baser.Test.Case
+ */
+class BcAllEventTest extends CakeTestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return CakeTestSuite
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All Event tests');
+		$suite->addTestDirectory(BASER_TEST_CASES . DS . 'Event' . DS);
+		return $suite;
+	}
+
+}
