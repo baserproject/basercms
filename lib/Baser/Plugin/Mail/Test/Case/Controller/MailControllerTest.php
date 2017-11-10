37d10d05b60be07436641714090a7e5318676712
Tommy <tommyt6073@gmail.com>
2017-11-10 17:49:40 +0900

    Mail/Controller/MailControllerに未実装テストメソッド追加

--- lib/Baser/Plugin/Mail/Test/Case/Controller/MailControllerTest.php
+++ lib/Baser/Plugin/Mail/Test/Case/Controller/MailControllerTest.php
@@ -30,11 +30,76 @@ class MailControllerTest extends BaserTestCase {
 	}
 
 /**
+ * beforeFilter.
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * beforeRender
+ */
+	public function testBeforeRender() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
  * [test_index description]
  * @return [type] [description]
  */
 	public function test_index() {
 		// $result = $this->testAction('/contact/index');
+	}
+
+/**
+ * [PUBIC] フォームを表示する
+ *
+ * @return void
+ */
+	public function testIndex() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [PUBIC] データの確認画面を表示
+ */
+	public function testConfirm() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [PUBIC] データ送信
+ */
+	public function testSubmit() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [private] 確認画面から戻る
+ */
+	public function test_back() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 認証用のキャプチャ画像を表示する
+ */
+	public function testCaptcha() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ajax] Tokenのkeyを取得
+ */
+	public function testAjax_get_token() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ajax] Tokenのkeyを取得
+ */
+	public function testSmartphone_ajax_get_token() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
 	}
 
 }
