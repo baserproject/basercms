f277e910cc079d2089e81687a1c7ef5109d7a19c
Tommy <tommyt6073@gmail.com>
2017-11-10 13:10:00 +0900

    Controller/WidgetAreasControllerに未実装テストメソッド追加

--- lib/Baser/Test/Case/Controller/WidgetAreasControllerTest.php
+++ lib/Baser/Test/Case/Controller/WidgetAreasControllerTest.php
@@ -38,4 +38,74 @@ class WidgetAreasControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * beforeFilter
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 一覧
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 新規登録
+ */
+	public function testAdmin_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 編集
+ */
+	public function testAdmin_edit() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理　(ajax)
+ */
+	public function testAdmin_ajax_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [AJAX] タイトル更新
+ */
+	public function testAdmin_update_title() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [AJAX] ウィジェット更新
+ */
+	public function testAdmin_update_widget() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 並び順を更新する
+ */
+	public function testAdmin_update_sort() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [AJAX] ウィジェットを削除
+ */
+	public function testAdmin_del_widget() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * ウィジェットを並び替えた上で取得する
+ */
+	public function testGet_widgets() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
 }
