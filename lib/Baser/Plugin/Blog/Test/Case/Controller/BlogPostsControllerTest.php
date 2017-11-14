f9b3f59ad498b477005078e479e1efa46f285557
Tommy <tommyt6073@gmail.com>
2017-11-14 14:05:08 +0900

    Blog/Controller/BlogPostsControllerに未実装テストメソッド追加

--- lib/Baser/Plugin/Blog/Test/Case/Controller/BlogPostsControllerTest.php
+++ lib/Baser/Plugin/Blog/Test/Case/Controller/BlogPostsControllerTest.php
@@ -38,4 +38,74 @@ class BlogPostsControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * beforeFilter
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * beforeRender
+ */
+	public function testBeforeRender() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 一覧表示
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 登録処理
+ */
+	public function testAdmin_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 編集処理
+ */
+	public function testAdmin_edit() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理　(ajax)
+ */
+	public function testAdmin_ajax_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理
+ */
+	public function testAdmin_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 無効状態にする（AJAX）
+ */
+	public function testAdmin_ajax_unpublish() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 有効状態にする（AJAX）
+ */
+	public function testAdmin_ajax_publish() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] コピー
+ */
+	public function testAdmin_ajax_copy() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
 }
