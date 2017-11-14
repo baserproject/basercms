a374b77ae493698a9e26cfb554db00599db93c34
Tommy <tommyt6073@gmail.com>
2017-11-14 12:40:59 +0900

    Blog/Controller/BlogCommentsControllerに未実装テストメソッド追加

--- lib/Baser/Plugin/Blog/Test/Case/Controller/BlogCommentsControllerTest.php
+++ lib/Baser/Plugin/Blog/Test/Case/Controller/BlogCommentsControllerTest.php
@@ -38,4 +38,74 @@ class BlogCommentsControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * beforeFilter
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * beforeRender
+ */
+	public function testBeforeRender() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] ブログを一覧表示する
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理　(ajax)
+ */
+	public function testAdmin_ajax_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 無効状態にする（AJAX）
+ */
+	public function testAdmin_ajax_unpublish() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 有効状態にする（AJAX）
+ */
+	public function testAdmin_ajax_publish() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [AJAX] ブログコメントを登録する
+ */
+	public function testAdd() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [AJAX] ブログコメントを登録する
+ */
+	public function testSmartphone_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 認証用のキャプチャ画像を表示する
+ */
+	public function testCaptcha() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * コメント送信用にAjax経由でトークンを取得するアクション
+ */
+	public function testGet_token() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
 }
