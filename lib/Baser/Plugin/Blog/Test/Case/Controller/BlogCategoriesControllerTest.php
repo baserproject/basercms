f7e8f8b95112ef414ba0df771cfd865e3ab83478
Tommy <tommyt6073@gmail.com>
2017-11-14 12:31:06 +0900

    Blog/Controller/BlogCategoriesControllerに未実装テストメソッド追加

--- lib/Baser/Plugin/Blog/Test/Case/Controller/BlogCategoriesControllerTest.php
+++ lib/Baser/Plugin/Blog/Test/Case/Controller/BlogCategoriesControllerTest.php
@@ -38,4 +38,60 @@ class BlogCategoriesControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * beforeFilter
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * beforeRender
+ */
+	public function testBeforeRender() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] ブログを一覧表示する
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 登録処理
+ */
+	public function testAdmin_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 編集処理
+ */
+	public function testAdmin_edit() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理　(ajax)
+ */
+	public function testAdmin_ajax_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除処理
+ */
+	public function testAdmin_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 追加処理（AJAX）
+ */
+	public function testAdmin_ajax_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
 }
