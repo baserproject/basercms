ed49d8469f941d1f9aa243d5b3f52ad01cd65de1
Takumi <kinzi0828@yahoo.co.jp>
2017-11-15 11:11:41 +0900

    BcAppHelper/コンストラクタテスト追加 (#769)

--- lib/Baser/Test/Case/View/Helper/BcAppHelperTest.php
+++ lib/Baser/Test/Case/View/Helper/BcAppHelperTest.php
@@ -10,24 +10,34 @@
  * @license			http://basercms.net/license/index.html
  */
 
-App::uses('BcAppView', 'View');
+App::uses('View', 'View');
+App::uses('BcAppHelper', 'View/Helper');
+App::uses('BcHtmlHelper', 'View/Helper');
 
 /**
  * ArrayHelper
  *
  * @package Baser.View.Helper
+ * @property BcAppHelper $BcAppHelper
+ * @property BcHtmlHelper $BcHtmlHelper
+ * @property View $View
  */
 class BcAppHelperTest extends BaserTestCase {
 	public function setUp() {
 		parent::setUp();
+		$View = new View();
+		$this->BcHtmlHelper = new BcHtmlHelper($View);
 	}
 
 	public function tearDown() {
 		parent::tearDown();
 	}
 
+	/**
+	 * コンストラクタ
+	 */
 	public function test__construct() {
-		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+		$this->assertEquals('<input type="checkbox" name="%s[]"%s />&nbsp;', $this->BcHtmlHelper->_tags['checkboxmultiple'], "コンストラクタの結果が違います。");
 	}
 
 	public function testAfterLayout() {
