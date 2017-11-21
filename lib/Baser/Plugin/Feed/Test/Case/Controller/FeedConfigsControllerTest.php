d136ac6a3d02628ae3530337095bcf9a25b640d3
Tommy <tommyt6073@gmail.com>
2017-11-21 10:30:27 +0900

    Feed/Controller/FeedConfigsControllerに未実装テストメソッド追加

--- lib/Baser/Plugin/Feed/Test/Case/Controller/FeedConfigsControllerTest.php
+++ lib/Baser/Plugin/Feed/Test/Case/Controller/FeedConfigsControllerTest.php
@@ -38,4 +38,69 @@ class FeedConfigsControllerTest extends BaserTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * before_filter
+ */
+	public function testBeforeFilter() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 一覧表示
+ */
+	public function testAdmin_index() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 登録
+ */
+	public function testAdmin_add() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 編集
+ */
+	public function testAdmin_edit() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除　(ajax)
+ */
+	public function testAdmin_ajax_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * [ADMIN] 削除
+ */
+	public function testAdmin_delete() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * 読み込んだフィードをプレビュー表示する
+ */
+	public function testAdmin_preview() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * フィードのキャッシュを全て削除する
+ */
+	public function testAdmin_delete_cache() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+/**
+ * フィードのキャッシュを削除する（requestAction用）
+ */
+	public function testAdmin_clear_cache() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+
+
 }
