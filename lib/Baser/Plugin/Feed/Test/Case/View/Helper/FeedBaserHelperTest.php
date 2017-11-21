3367b2060a807cf0c3d9b72e103b7d5c4406804a
Tommy <tommyt6073@gmail.com>
2017-11-21 10:50:15 +0900

    Feed/View/Helper/FeedBaserHelperに未実装テストメソッド追加

---
+++ lib/Baser/Plugin/Feed/Test/Case/View/Helper/FeedBaserHelperTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Feed.Test.Case.View.Helper
+ * @since			baserCMS v 4.0.9
+ * @license			http://basercms.net/license/index.html
+ */
+
+App::uses('FeedBaserHelper', 'Feed.View.Helper');
+
+/**
+ * FeedBaserHelperTest class
+ *
+ * @package Feed.Test.Case.View.Helper
+ * @property  FeedBaserHelper $FeedBaserHelper
+ */
+class FeedBaserHelperTest extends BaserTestCase {
+
+/**
+ * set up
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+/**
+ * フィード出力
+ */
+	public function testFeed() {
+		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	}
+
+}
