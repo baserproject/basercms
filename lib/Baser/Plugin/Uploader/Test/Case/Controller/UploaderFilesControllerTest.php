81ca8184ec512ebcbc4afa4350eb88ea7f487abf
Tommy <tommyt6073@gmail.com>
2017-11-21 11:42:49 +0900

    Uploader/Controllerディレクトリの各テストの雛形を新規作成

---
+++ lib/Baser/Plugin/Uploader/Test/Case/Controller/UploaderFilesControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <http://basercms.net>
+ * Copyright (c) baserCMS Users Community <http://basercms.net/community/>
+ *
+ * @copyright		Copyright (c) baserCMS Users Community
+ * @link			http://basercms.net baserCMS Project
+ * @package			Uploader.Test.Case.Controller
+ * @since			baserCMS v 4.0.9
+ * @license			http://basercms.net/license/index.html
+ */
+
+App::uses('UploaderFilesController', 'Uploader.Controller');
+
+/**
+ * UploaderFilesControllerTest class
+ *
+ * @package Feed.Test.Case.View.Helper
+ * @property  UploaderFilesController $UploaderFilesController
+ */
+class UploaderFilesControllerTest extends BaserTestCase {
+
+/**
+ * set up
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+}
