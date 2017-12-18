ccc85cef37adec908fdab1b52f2da62c9d7261db
seto <seto@catchup.co.jp>
2017-12-18 10:18:29 +0900

    管理画面 フッターのリンク調整
    - SSL化したURLのリンク調整
    - URLが変更になったページのリンク調整

--- lib/Baser/View/Elements/admin/footer.php
+++ lib/Baser/View/Elements/admin/footer.php
@@ -40,19 +40,19 @@
 		<div id="FooterLink">
 			<h2><?php $this->BcBaser->img('admin/head_link.png', ['width' => 36, 'height' => 16, 'alt' => 'LINK']) ?></h2>
 			<ul>
-				<li><a href="http://basercms.net/" target="_blank">baserCMS 公式サイト</a></li>
-				<li><a href="http://sites.google.com/site/baserusers/" target="_blank">baserCMS ユーザー会</a></li>
+				<li><a href="https://basercms.net/" target="_blank">baserCMS 公式サイト</a></li>
+				<li><a href="https://basercms.net/community/" target="_blank">baserCMS ユーザー会</a></li>
 				<li><a href="http://forum.basercms.net/" target="_blank">baserCMS ユーザーズフォーラム</a></li>
 				<li><a href="http://project.e-catchup.jp/projects/basercms" target="_blank">baserCMS コア開発プロジェクト</a></li>
-				<li><a href="http://www.facebook.com/basercms" target="_blank">baserCMS Facebook</a></li>
-				<li><a href="http://twitter.com/basercms" target="_blank">baserCMS Twitter</a></li>
+				<li><a href="https://www.facebook.com/basercms" target="_blank">baserCMS Facebook</a></li>
+				<li><a href="https://twitter.com/basercms" target="_blank">baserCMS Twitter</a></li>
 			</ul>
 		</div>
 
 		<div class="float-right">
 			<ul id="FooterBanner">
-				<li><?php $this->BcBaser->link($this->BcBaser->getImg('baser.power.gif', ['alt' => 'baserCMS Power']), 'http://basercms.net/', ['target' => '_blank', 'title' => 'baserCMS Power']) ?></li>
-				<li><?php $this->BcBaser->link($this->BcBaser->getImg('cake.power.gif', ['alt' => 'CakePHP Power']), 'http://cakephp.jp/', ['target' => '_blank', 'title' => 'CakePHP Power']) ?></li>
+				<li><?php $this->BcBaser->link($this->BcBaser->getImg('baser.power.gif', ['alt' => 'baserCMS Power']), 'https://basercms.net/', ['target' => '_blank', 'title' => 'baserCMS Power']) ?></li>
+				<li><?php $this->BcBaser->link($this->BcBaser->getImg('cake.power.gif', ['alt' => 'CakePHP Power']), 'https://cakephp.org/jp/', ['target' => '_blank', 'title' => 'CakePHP Power']) ?></li>
 			</ul>
 			<p id="BaserVersion">baserCMS <?php echo $baserVersion ?></p>
 			<div id="Copyright">Copyright (C) baserCMS Users Community <?php $this->BcBaser->copyYear(2009) ?> All rights reserved.</div>
