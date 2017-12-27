fc65ca4e95a6933644242471fa84c1ce71460554
takumi <kinzi0828@yahoo.co.jp>
2017-12-27 11:03:37 +0900

    Model/ContentTest testCopy追加

--- lib/Baser/Test/Case/Model/ContentTest.php
+++ lib/Baser/Test/Case/Model/ContentTest.php
@@ -433,9 +433,23 @@ class ContentTest extends BaserTestCase {
 
 /**
  * コピーする
+ *
+ * @dataProvider copyDataProvider
  */
-	public function testCopy() {
-		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	public function testCopy($id, $entityId, $newTitle, $newAuthorId, $newSiteId, $titleExpected) {
+		$result = $this->Content->copy($id, $entityId, $newTitle, $newAuthorId, $newSiteId)['Content'];
+		$this->assertEquals($result['site_id'], $newSiteId);
+		$this->assertEquals($result['entity_id'], $entityId);
+		$this->assertEquals($result['title'], $titleExpected);
+		$this->assertEquals($result['author_id'], $newAuthorId);
+
+	}
+
+	public function copyDataProvider(){
+		return[
+			[1, 2, 'hoge', 3, 4, 'hoge'],
+			[1, 2, '', 3, 4, 'baserCMS inc. [デモ]のコピー'],
+		];
 	}
 
 /**
