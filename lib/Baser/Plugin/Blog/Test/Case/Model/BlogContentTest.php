9577a0423ba67fc48a7bd1dcfc344ce56d2abee6
Takumi <kinzi0828@yahoo.co.jp>
2018-01-12 13:41:44 +0900

    BlogTest/testCheckEyeCatchSize追加 (#852)

--- lib/Baser/Plugin/Blog/Test/Case/Model/BlogContentTest.php
+++ lib/Baser/Plugin/Blog/Test/Case/Model/BlogContentTest.php
@@ -133,11 +133,29 @@ class BlogContentTest extends BaserTestCase {
 
 /**
  * アイキャッチ画像サイズバリデーション
+ *
+ * @dataProvider checkEyeCatchSizeDataProvider
  */
-	public function testCheckEyeCatchSize() {
-		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+	public function testCheckEyeCatchSize($thumb_width, $thumb_height, $mobile_thumb_width, $mobile_thumb_height, $expected) {
+		$this->BlogContent->data['BlogContent']['eye_catch_size'] = BcUtil::serialize([
+			'thumb_width' => $thumb_width,
+			'thumb_height' => $thumb_height,
+			'mobile_thumb_width' => $mobile_thumb_width,
+			'mobile_thumb_height' => $mobile_thumb_height
+		]);
+		$this->assertEquals($this->BlogContent->checkEyeCatchSize(), $expected);
 	}
 
+	public function checkEyeCatchSizeDataProvider() {
+		return[
+			[600, 600, 100, 100, true],
+			['', 600, 100, 100, false],
+			[600, '', 100, 100, false],
+			[600, 600, '', 100, false],
+			[600, 600, 100, '', false],
+		];
+	}
+	
 /**
  * 英数チェック
  */
