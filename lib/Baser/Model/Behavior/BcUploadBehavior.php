bd5ceb43747d6bb6b0a8b6ddbb3cdacd15bc9476
akira.murakami <murakami0427@gmail.com>
2018-04-17 11:43:24 +0900

    fix #21421 既存アイキャッチ画像と同じファイル名のアイキャッチ画像を別のコンテンツ等で登録すると既存アイキャッチ画像のサムネイル画像が上書きされてしまう事象改善

--- lib/Baser/Model/Behavior/BcUploadBehavior.php
+++ lib/Baser/Model/Behavior/BcUploadBehavior.php
@@ -1039,6 +1039,13 @@ class BcUploadBehavior extends ModelBehavior {
 						continue;
 					}
 				}
+				/**
+				 * ファイル名重複回避処理対応
+				 * 元画像ファイルと同様コピー画像ファイルにも適用
+				 */
+				if ($fileName !== $Model->data[$Model->alias]['name']['name']) {
+					$Model->data[$Model->alias]['name']['name'] = $fileName;
+				}
 				$copy['name'] = $field['name'];
 				$copy['ext'] = $field['ext'];
 				$ret = $this->copyImage($Model, $copy);
