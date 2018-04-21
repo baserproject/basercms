ed52765d948825564713eb127df7b5ec311a7130
ryuring <ryuring55@gmail.com>
2018-04-21 18:31:16 +0900

    メールフォームでファイルフィールドを利用する際、送信メールに添付の有無を表示するように変更

--- lib/Baser/Config/theme/bc_sample/Emails/text/mail_data.php
+++ lib/Baser/Config/theme/bc_sample/Emails/text/mail_data.php
@@ -6,19 +6,27 @@
 $group_field = null;
 foreach ($mailFields as $field) {
 	$field = $field['MailField'];
-	if ($field['use_field'] && $field['type'] != 'file' && isset($message[$field['field_name']]) && ($group_field != $field['group_field'] || (!$group_field && !$field['group_field']))) {
+	if ($field['use_field'] && isset($message[$field['field_name']]) && ($group_field != $field['group_field'] || (!$group_field && !$field['group_field']))) {
 ?>
 
 
-◇◆ <?php echo $field['head']; ?> 
+◇◆ <?php echo $field['head']; ?>　
 ----------------------------------------
 <?php
 	}
 	if ($field['type'] != 'file' && !empty($field['before_attachment']) && isset($message[$field['field_name']])) {
 		echo " " . $field['before_attachment'];
 	}
-	if ($field['type'] != 'file' && isset($message[$field['field_name']]) && !$field['no_send'] && $field['use_field']) {
-		echo $this->Maildata->control($field['type'], $message[$field['field_name']], $this->Mailfield->getOptions($field));
+	if (isset($message[$field['field_name']]) && !$field['no_send'] && $field['use_field']) {
+		if($field['type'] != 'file') {
+			echo $this->Maildata->control($field['type'], $message[$field['field_name']], $this->Mailfield->getOptions($field));
+		} else {
+			if($message[$field['field_name']]) {
+				echo '添付あり';
+			} else {
+				echo '添付なし';
+			}
+		}
 	}
 	if ($field['type'] != 'file' && !empty($field['after_attachment']) && isset($message[$field['field_name']])) {
 		echo " " . $field['after_attachment'];
--- lib/Baser/Plugin/Mail/View/Emails/text/mobile/mail_data.php
+++ lib/Baser/Plugin/Mail/View/Emails/text/mobile/mail_data.php
@@ -16,17 +16,25 @@
 ?>
 <?php $group_field = null; ?>
 <?php foreach($mailFields as $fields): ?>
-<?php if ($field['use_field'] && $field['type'] != 'file' && isset($message[$field['field_name']]) && ($group_field != $field['group_field'] || (!$group_field && !$field['group_field']))) : ?>
+<?php if ($field['use_field'] && isset($message[$field['field_name']]) && ($group_field != $field['group_field'] || (!$group_field && !$field['group_field']))) : ?>
 
 
-◇◆ <?php echo $fields['MailField']['head']; ?> 
+◇◆ <?php echo $fields['MailField']['head']; ?>　
 ----------------------------
 <?php endif; ?>
 <?php if ($field['type'] != 'file' && !empty($fields['MailField']['before_attachment']) && !empty($message[$fields['MailField']['field_name']])): ?>
 <?php echo " " . $fields['MailField']['before_attachment'] ?>
 <?php endif; ?>
-<?php if ($field['type'] != 'file' && !empty($message[$fields['MailField']['field_name']]) && !$fields['MailField']['no_send']): ?>
+<?php if (!empty($message[$fields['MailField']['field_name']]) && !$fields['MailField']['no_send']): ?>
+<?php if ($field['type'] != 'file'): ?>
 <?php echo $this->Maildata->control($fields['MailField']['type'], $message[$fields['MailField']['field_name']], $this->Mailfield->getOptions($fields)); ?>
+<?php else: ?>
+<?php if($message[$field['field_name']]): ?>
+<?php echo '添付あり' ?>
+<?php else: ?>
+<?php echo '添付なし' ?>
+<?php endif ?>
+<?php endif ?>
 <?php endif; ?>
 <?php if ($field['type'] != 'file' && !empty($fields['MailField']['after_attachment']) && !empty($message[$fields['MailField']['field_name']])): ?>
 <?php echo " " . $fields['MailField']['after_attachment']; ?>
