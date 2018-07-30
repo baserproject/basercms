ba773f3560f6241fa632204fd879d781a72ada23
kato <kato@e-catchup.jp>
2018-07-30 12:11:20 +0900

    fix #22129 固定ページをコピーした際に、オプションの説明文がコピーできない問題を解決
    ・Google検索で問題が発生しないように
    ・descriptionが入っている場合、descriptionの末尾に「のコピー」の文字をつけるように調整

--- lib/Baser/Model/Page.php
+++ lib/Baser/Model/Page.php
@@ -730,6 +730,7 @@ class Page extends AppModel {
 		$siteId = $data['Content']['site_id'];
 		$name = $data['Content']['name'];
 		$eyeCatch = $data['Content']['eyecatch'];
+		$description = !empty($data['Content']['description']) ? __d('baser', $data['Content']['description'].'のコピー') : '';
 		unset($data['Page']['id']);
 		unset($data['Page']['created']);
 		unset($data['Page']['modified']);
@@ -740,7 +741,7 @@ class Page extends AppModel {
 			'title'		=> $newTitle,
 			'author_id' => $newAuthorId,
 			'site_id' 	=> $newSiteId,
-			'description' => ''
+			'description' => $description
 		];
 		if(!is_null($newSiteId) && $siteId != $newSiteId) {
 			$data['Content']['site_id'] = $newSiteId;
