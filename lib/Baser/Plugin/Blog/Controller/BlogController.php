13e796c7093803399ffb479eda164e923a00028e
arata <contact@materializing.net>
2019-11-10 01:37:58 +0900

    Revert "modify アーカイブ情報を持つ変数について説明を付与するため"
    This reverts commit ee3f23b53bbdcb74ef4d601167e011556d485a07.

--- lib/Baser/Plugin/Blog/Controller/BlogController.php
+++ lib/Baser/Plugin/Blog/Controller/BlogController.php
@@ -252,7 +252,7 @@ class BlogController extends BlogAppController {
 		$crumbs = $posts = [];
 		$single = false;
 		$posts = [];
-		$blogArchiveTypeData = []; // 各アーカイブ別の情報（例: category時はブログカテゴリ情報、tag時はブログタグ情報）
+		$blogArchiveTypeData = [];
 
 		if ($pass[0] == 'category') {
 			$type = 'category';
