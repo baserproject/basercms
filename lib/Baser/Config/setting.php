b84a99474d62c48ab5e03e3a9ed81d2d0ebaf8ee
ryuring <ryuring55@gmail.com>
2020-04-03 17:12:47 +0900

    fix #1274 オートプレフィックスの除外設定を追加
    「すべてのリンクをサブサイト用に変換する」指定している場合、全てのリンクに対して強制的にプレフィックスを備えるため、除外指定できるようにするため
    - 指定した絶対URLの場合はプレフィックスを備えない
    - 指定したディレクトリ配下の場合はプレフィックスを備えない

--- lib/Baser/Config/setting.php
+++ lib/Baser/Config/setting.php
@@ -168,7 +168,16 @@ $config['BcApp'] = [
 	// 管理システムのデフォルトテーマ
 	'defaultAdminTheme' => 'admin-third',
 	// コンテンツの作成日を自動で更新する
-	'autoUpdateContentCreatedDate' => true
+	'autoUpdateContentCreatedDate' => true,
+	// オートプレフィックス除外設定（絶対URL）
+	// 「すべてのリンクをサブサイト用に変換する」指定時、全てのリンクに対してプレフィックスを備える箇所に除外指定できる
+	// 指定した絶対URLを記載しているリンクは変換しない
+	// 例: 'https://basercms.net/'と記載 → https://basercms.net/s/ は s が付かなくなる
+	'excludeAbsoluteUrlAddPrefix' => [],
+	// オートプレフィックス除外設定（ディレクトリ）
+	// 指定したディレクトリURLを記載しているリンクは変換しない
+	// 例: 'test/' と記載 → https://basercms.net/s/test/ は s が付かなくなる
+	'excludeListAddPrefix' => [],
 ];
 
 /**
