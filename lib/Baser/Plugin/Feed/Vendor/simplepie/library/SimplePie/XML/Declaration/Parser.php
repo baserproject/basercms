5283c8f3611b7607995e78c391027361cb20a4a5
katokaisya <kato.kaisya@gmail.com>
2020-04-08 11:52:21 +0900

    fix #1415 FEED読み込み時にatom等だとWarningが出る問題を解決 (#1416)

--- lib/Baser/Plugin/Feed/Vendor/simplepie/library/SimplePie/XML/Declaration/Parser.php
+++ lib/Baser/Plugin/Feed/Vendor/simplepie/library/SimplePie/XML/Declaration/Parser.php
@@ -5,7 +5,7 @@
  * A PHP-Based RSS and Atom Feed Framework.
  * Takes the hard work out of managing a complete RSS/Atom solution.
  *
- * Copyright (c) 2004-2012, Ryan Parman, Geoffrey Sneddon, Ryan McCue, and contributors
+ * Copyright (c) 2004-2016, Ryan Parman, Geoffrey Sneddon, Ryan McCue, and contributors
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
@@ -33,8 +33,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package SimplePie
- * @version 1.3.1
- * @copyright 2004-2012 Ryan Parman, Geoffrey Sneddon, Ryan McCue
+ * @copyright 2004-2016 Ryan Parman, Geoffrey Sneddon, Ryan McCue
  * @author Ryan Parman
  * @author Geoffrey Sneddon
  * @author Ryan McCue
@@ -54,42 +53,48 @@ class SimplePie_XML_Declaration_Parser
 	/**
 	 * XML Version
 	 *
-		 * @var string
+	 * @access public
+	 * @var string
 	 */
 	var $version = '1.0';
 
 	/**
 	 * Encoding
 	 *
-		 * @var string
+	 * @access public
+	 * @var string
 	 */
 	var $encoding = 'UTF-8';
 
 	/**
 	 * Standalone
 	 *
-		 * @var bool
+	 * @access public
+	 * @var bool
 	 */
 	var $standalone = false;
 
 	/**
 	 * Current state of the state machine
 	 *
-		 * @var string
+	 * @access private
+	 * @var string
 	 */
 	var $state = 'before_version_name';
 
 	/**
 	 * Input data
 	 *
-		 * @var string
+	 * @access private
+	 * @var string
 	 */
 	var $data = '';
 
 	/**
 	 * Input data length (to avoid calling strlen() everytime this is needed)
 	 *
-		 * @var int
+	 * @access private
+	 * @var int
 	 */
 	var $data_length = 0;
 
@@ -97,13 +102,15 @@ class SimplePie_XML_Declaration_Parser
 	 * Current position of the pointer
 	 *
 	 * @var int
-		 */
+	 * @access private
+	 */
 	var $position = 0;
 
 	/**
 	 * Create an instance of the class with the input data
 	 *
-		 * @param string $data Input data
+	 * @access public
+	 * @param string $data Input data
 	 */
 	public function __construct($data)
 	{
@@ -114,7 +121,8 @@ class SimplePie_XML_Declaration_Parser
 	/**
 	 * Parse the input data
 	 *
-		 * @return bool true on success, false on failure
+	 * @access public
+	 * @return bool true on success, false on failure
 	 */
 	public function parse()
 	{
@@ -140,7 +148,8 @@ class SimplePie_XML_Declaration_Parser
 	/**
 	 * Check whether there is data beyond the pointer
 	 *
-		 * @return bool true if there is further data, false if not
+	 * @access private
+	 * @return bool true if there is further data, false if not
 	 */
 	public function has_data()
 	{
