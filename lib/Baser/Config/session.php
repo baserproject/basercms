2cc5a59c9a7072090c02d43d3d4183b0f84ab4c8
ryuring <ryuring55@gmail.com>
2020-05-15 12:55:24 +0900

    /lib/Baser/session.php を整理

--- lib/Baser/Config/session.php
+++ lib/Baser/Config/session.php
@@ -10,7 +10,29 @@
  * @license			http://basercms.net/license/index.html
  */
 
+/**
+ * セッション設定
+ * 
+ * `/app/Config/setting.php` より先に読み込まれるため
+ * `Configure` の値を変更するには、`/app/Config/session.php` で設定する
+ * 
+ * 【ブラウザを開いた状態においてセッションが切れる条件】
+ * session.gc_maxlifetime で設定された秒数ごとに一定の確率ごとにセッションが切れる
+ * 確率は、session.gc_probability で設定する。確実にタイムアウトさせたい場合は、100を設定する。
+ */
+
 if (empty($_SESSION)) {
+
+/**
+ * セッションタイムアウト
+ */
+	$timeout = 60 * 24;
+	
+/**
+ * ブラウザを閉じた後のセッションの有効期限
+ */
+	$cookieTimeout = $timeout;
+	
 /**
  * モバイル設定 
  */
@@ -30,52 +52,42 @@ if (empty($_SESSION)) {
 		$useTransSid = 1;
 		ini_set('url_rewriter.tags', '');
 	}
+	
 /**
- * セッションタイムアウト
- * 《ブラウザを開いた状態においてセッションが切れる条件》
- * session.gc_maxlifetime で設定された秒数ごとに一定の確率ごとにセッションが切れる
- * 確率は、session.gc_probability で設定する。確実にタイムアウトさせたい場合は、100を設定する。
- * デフォルト：１日１回、100分の１の確率でセッションが切れる
+ * Cookie Secure
+ * 
+ * 全てHTTPSでサイトが利用されている場合に有効とする
+ * URL設定がどちらとも同じドメインでかつ、 HTTPS で設定されている
+ * 異なるサブドメインにてcookie共有を許容しているサイトは対象外
  */
-	$timeout = 60 * 24;
-/**
- * ブラウザを閉じた後のセッションの有効期限
- * デフォルト：１日
- */
-	$cookieTimeout = $timeout;
-/**
- * 設定
- */
-	Configure::write('Session', array_merge(Configure::read('Session'), array(
-		'cookie' => 'BASERCMS',
-		'timeout' => $timeout,
-		'cookieTimeout' => $cookieTimeout,
-		'ini' => array(
-			'session.serialize_handler' => 'php',
-			'session.save_path'			=> TMP . 'sessions',
-			'session.use_cookies'		=> $useCookies,
-			'session.use_trans_sid'		=> $useTransSid,
-			'session.gc_maxlifetime'	=> $timeout * 60,
-			'session.gc_divisor'		=> 100,
-			'session.gc_probability'	=> 1,
-			'session.cookie_secure' => 0
-		)
-	)));
-/*
- * 全てHTTPSでサイトが利用されていたらsession.cookie_secureを1とする
- */
+ 	$cookieSecure = 0;
 	if (strpos(strtolower(Configure::read('BcEnv.siteUrl')), 'https') === 0) {
-		// URL設定がどちらとも同じドメインでかつ、HTTPSで設定されている
-		// 異なるサブドメインにてcookie共有を許容しているサイトは対象外
 		$siteUrlDomainMatches = null;
 		$sslUrlDomainMatches  = null;
 		preg_match("/^https:\/\/([a-zA-Z0-9\.-]+)(:\d+|)\//", Configure::read('BcEnv.siteUrl'), $siteUrlDomainMatches);
 		preg_match("/^https:\/\/([a-zA-Z0-9\.-]+)(:\d+|)\//", Configure::read('BcEnv.sslUrl'), $sslUrlDomainMatches);
 		if (isset($siteUrlDomainMatches[0]) && isset($sslUrlDomainMatches[0]) &&
 			$siteUrlDomainMatches[0] == $sslUrlDomainMatches[0]) {
-			Configure::write('Session.ini', array_merge(Configure::read('Session.ini'), array(
-				'session.cookie_secure' => 1
-			)));
+			Configure::write('Session.ini.session.cookie_secure', 1);
+			$cookieSecure = 1;
 		}
 	}
+	
+	
+	Configure::write('Session', array_merge(Configure::read('Session'), [
+		'cookie' => 'BASERCMS',
+		'timeout' => $timeout,
+		'cookieTimeout' => $cookieTimeout,
+		'ini' => [
+			'session.serialize_handler' => 'php',
+			'session.save_path' => TMP . 'sessions',
+			'session.use_cookies' => $useCookies,
+			'session.use_trans_sid' => $useTransSid,
+			'session.gc_maxlifetime' => $timeout * 60,
+			'session.gc_divisor' => 100,
+			'session.gc_probability' => 1,
+			'session.cookie_secure' => $cookieSecure
+		]
+	]));
+	
 }
