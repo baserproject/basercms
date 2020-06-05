ce3b22cd1bc3db451c60a99d94c5dabeaf967f10
gondoh <gondoh@catchup.co.jp>
2020-06-05 12:33:21 +0900

    fix #1488 不要なversion情報の表示の改善

--- app/webroot/theme/admin-third/Elements/admin/footer.php
+++ app/webroot/theme/admin-third/Elements/admin/footer.php
@@ -32,7 +32,7 @@
   <div class="bca-footer__inner">
     <div class="bca-footer__header">
       <?php $this->BcBaser->link($this->BcBaser->getImg('admin/logo_icon.svg', ['alt' => '', 'class'=>'bca-footer__logo']), ['controller' => 'dashboard', 'action' => 'index']) ?>
-      <div class="bca-footer__baser-version">baserCMS <strong><?php echo $baserVersion ?></strong></div>
+      <div class="bca-footer__baser-version">baserCMS</strong></div>
     </div>
 
     <ul class="bca-footer__sns">
