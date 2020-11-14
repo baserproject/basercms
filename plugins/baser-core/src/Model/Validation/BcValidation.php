d5f8fdc9aa5e6f1dd2818d5bb463d9e55fd1fa4b
ryuring <ryuring55@gmail.com>
2020-11-14 12:48:32 +0900

    BcValidation にコメントヘッダを追加

--- plugins/baser-core/src/Model/Validation/BcValidation.php
+++ plugins/baser-core/src/Model/Validation/BcValidation.php
@@ -1,8 +1,21 @@
 <?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) baserCMS User Community <https://basercms.net/community/>
+ *
+ * @copyright     Copyright (c) baserCMS User Community
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       http://basercms.net/license/index.html MIT License
+ */
 namespace BaserCore\Model\Validation;
 
 use Cake\Validation\Validation;
 
+/**
+ * Class BcValidation
+ * @package BaserCore\Model\Validation
+ */
 class BcValidation extends Validation {
 
     /**
