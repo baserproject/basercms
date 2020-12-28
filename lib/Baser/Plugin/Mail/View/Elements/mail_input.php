780543c3a4b56d8eb093bf2931d32ee79701c8aa
yama <yamamoto@kyms.jp>
2020-12-29 00:28:29 +0900

    Refactor mail_input.php

--- lib/Baser/Plugin/Mail/View/Elements/mail_input.php
+++ lib/Baser/Plugin/Mail/View/Elements/mail_input.php
@@ -22,90 +22,105 @@
 if (empty($mailFields)) {
 	return;
 }
-
 $group_field = null;
 $iteration = 0;
 if (!isset($blockEnd)) {
 	$blockEnd = 0;
 }
 
+$title = null;
+$form = [];
 foreach ($mailFields as $key => $record) {
 
-	$field = $record['MailField'];
 	$iteration++;
-	if ($field['use_field'] && ($blockStart && $iteration >= $blockStart) && (!$blockEnd || $iteration <= $blockEnd)) {
+	if (!$record['MailField']['use_field']) {
+		continue;
+	}
+	if (!$blockStart || $iteration < $blockStart) {
+		continue;
+	}
+	if ($blockEnd && $iteration > $blockEnd) {
+		continue;
+	}
+	$field = $record['MailField'];
+	$next_key = $key + 1;
 
-		$next_key = $key + 1;
-		$description = $field['description'];
+	/* 項目名 */
+	if ($group_field != $field['group_field'] || (!$group_field && !$field['group_field'])) {
+		$title = $this->mail->formatText(
+			'<tr id="{%row_id%}"{%display%}><th class="col-head" width="150">{%title%}<span class="{%required_class%}">{%required_word%}</span></th><td class="col-input">{%form%}</td></tr>',
+			[
+				'row_id'  => 'RowMessage' . Inflector::camelize($field['field_name']),
+				'display' => $field['type'] === 'hidden' ? ' style="display:none"' : '',
+				'title'   =>$this->Mailform->label(
+					sprintf(
+						'MailMessage.%s',
+						$field['field_name']
+					),
+					$field['head']
+				),
+				'required_class' => $field['not_empty'] ? 'required' : 'normal',
+				'required_word'  => $field['not_empty'] ? __('必須') : __('任意')
+			]
+		);
+	}
 
-		/* 項目名 */
-		if ($group_field != $field['group_field'] || (!$group_field && !$field['group_field'])) {
-			echo '    <tr id="RowMessage' . Inflector::camelize($record['MailField']['field_name']) . '"';
-			if ($field['type'] === 'hidden') {
-				echo ' style="display:none"';
-			}
-			echo '>' . "\n" . '        <th class="col-head" width="150">' . $this->Mailform->label("MailMessage." . $field['field_name'] . "", $field['head']);
-			if ($field['not_empty']) {
-				echo '<span class="required">' . __('必須') . '</span>';
-			} else {
-				echo '<span class="normal">' . __('任意') . '</span>';
-			}
-			echo '</th>' . "\n" . '        <td class="col-input">';
-		}
-
-		echo '<span id="FieldMessage' . Inflector::camelize($record['MailField']['field_name']) . '">';
-		if (!$freezed && $description) {
-			echo '<span class="mail-description">' . $description . '</span>';
-		}
-		/* 入力欄 */
-		if (!$freezed || $this->Mailform->value("MailMessage." . $field['field_name']) !== '') {
-			echo '<span class="mail-before-attachment">' . $field['before_attachment'] . '</span>';
-		}
-
-		// =========================================================================================================
-		// 2018/02/06 ryuring
-		// no_send オプションは、確認画面に表示しないようにするために利用されている可能性が高い
-		//（メールアドレスのダブル入力、プライバシーポリシーへの同意に利用されている）
-		// 本来であれば、not_display_confirm 等のオプションを別途準備し、そちらを利用するべきだが、
-		// 後方互換のため残す
-		// =========================================================================================================
-		if ($freezed && $field['no_send']) {
-			echo $this->Mailform->control('hidden', "MailMessage." . $field['field_name'] . "", $this->Mailfield->getOptions($record), $this->Mailfield->getAttributes($record));
-		} else {
-			echo $this->Mailform->control($field['type'], "MailMessage." . $field['field_name'] . "", $this->Mailfield->getOptions($record), $this->Mailfield->getAttributes($record));
-		}
-
-		if (!$freezed || $this->Mailform->value("MailMessage." . $field['field_name']) !== '') {
-			echo '<span class="mail-after-attachment">' . $field['after_attachment'] . '</span>';
-		}
-		if (!$freezed) {
-			echo '<span class="mail-attention">' . $field['attention'] . '</span>';
-		}
-
-		/* 説明欄 */
-		$isGroupValidComplate = in_array('VALID_GROUP_COMPLATE', explode(',', $field['valid_ex']));
-		if (!$isGroupValidComplate) {
-			echo $this->Mailform->error("MailMessage." . $field['field_name']);
-		}
-		$isRequiredToClose = true;
-		if ($this->Mailform->isGroupLastField($mailFields, $field)) {
-			if ($isGroupValidComplate) {
-				$groupValidErrors = $this->Mailform->getGroupValidErrors($mailFields, $field['group_valid']);
-				if ($groupValidErrors) {
-					foreach ($groupValidErrors as $groupValidError) {
-						echo $groupValidError;
-					}
+	// =========================================================================================================
+	// 2018/02/06 ryuring
+	// no_send オプションは、確認画面に表示しないようにするために利用されている可能性が高い
+	//（メールアドレスのダブル入力、プライバシーポリシーへの同意に利用されている）
+	// 本来であれば、not_display_confirm 等のオプションを別途準備し、そちらを利用するべきだが、
+	// 後方互換のため残す
+	// =========================================================================================================
+	$errors = [];
+	if ($this->Mailform->isGroupLastField($mailFields, $field)) {
+		if ($isGroupValidComplate) {
+			$groupValidErrors = $this->Mailform->getGroupValidErrors(
+				$mailFields,
+				$field['group_valid']
+			);
+			if ($groupValidErrors) {
+				foreach ($groupValidErrors as $groupValidError) {
+					$errors[] = $groupValidError;
 				}
 			}
-			echo $this->Mailform->error("MailMessage." . $field['group_valid'] . "_not_same", __("入力データが一致していません。"));
-			echo $this->Mailform->error("MailMessage." . $field['group_valid'] . "_not_complate", __("入力データが不完全です。"));
-		} elseif (!empty($field['group_field'])) {
-			$isRequiredToClose = false;
 		}
-		echo '</span>';
-		if ($isRequiredToClose) {
-			echo "</td>\n    </tr>\n";
-		}
-		$group_field = $field['group_field'];
+		$errors[] = $this->Mailform->error(
+			'MailMessage.' . $field['group_valid'] . "_not_same",
+			__('入力データが一致していません。')
+		);
+		$errors[] = $this->Mailform->error(
+			'MailMessage.' . $field['group_valid'] . '_not_complate',
+			__('入力データが不完全です。')
+		);
 	}
+	$hasDescription = (!$freezed && $field['description']);
+	$hasAttachment = (!$freezed || $this->Mailform->value('MailMessage.' . $field['field_name']) !== '');
+	$isGroupValidComplate = in_array('VALID_GROUP_COMPLATE', explode(',', $field['valid_ex']));
+	$form[] = $this->Mail->formatText(
+		'<span id="{%id%}">{%description%}{%before-attach%}{%form%}{%after-attach%}{%attention%}{%error%}{%lastErrors%}</span>',
+		[
+			'id'=>'FieldMessage' . Inflector::camelize($field['field_name']),
+			'description' => $hasDescription ? sprintf('<span class="mail-description">%s</span>', $field['description']) : '',
+			'before-attach' => $hasAttachment  ? sprintf('<span class="mail-before-attachment">%s</span>', $field['before_attachment']) : '',
+			'form' => $this->Mailform->control(
+				($freezed && $field['no_send']) ? 'hidden' : $field['type'],
+				'MailMessage.' . $field['field_name'],
+				$this->Mailfield->getOptions($record),
+				$this->Mailfield->getAttributes($record)
+			),
+			'after-attach'=>$hasAttachment ? sprintf('<span class="mail-after-attachment">%s</span>', $field['after_attachment']) : '',
+			'attention'=>!$freezed ? sprintf('<span class="mail-attention">%s</span>', $field['attention']) : '',
+			'error'=>!$isGroupValidComplate ? $this->Mailform->error('MailMessage.' . $field['field_name']) : '',
+			'lastErrors' => implode("\n", $errors)
+
+		]
+	);
+
+	if ($this->Mailform->isGroupLastField($mailFields, $field) || empty($field['group_field'])) {
+		echo $this->Mail->formatText($title,['form'=>implode("\n", $form)]);
+		$title = null;
+		$form = [];
+	}
+	$group_field = $field['group_field'];
 }
