6dc1a53807e408a2201343709e5447ffcaf8b8ec
yama <yamamoto@kyms.jp>
2020-12-29 00:24:25 +0900

    Refactor MailHelper.php

--- lib/Baser/Plugin/Mail/View/Helper/MailHelper.php
+++ lib/Baser/Plugin/Mail/View/Helper/MailHelper.php
@@ -170,11 +170,11 @@ class MailHelper extends AppHelper {
  * @return boolean 設定されている場合 true を返す
  */
 	public function descriptionExists() {
-		if (!empty($this->mailContent['description'])) {
-			return true;
-		} else {
+		if (empty($this->mailContent['description'])) {
 			return false;
 		}
+
+		return true;
 	}
 
 /**
@@ -243,7 +243,7 @@ class MailHelper extends AppHelper {
  * @param string $viewFile
  */
 	public function beforeRender($viewFile) {
-		if($this->request->params['controller'] == 'mail' && in_array($this->request->params['action'], ['index', 'confirm', 'submit'])) {
+		if($this->request->params['controller'] === 'mail' && in_array($this->request->params['action'], ['index', 'confirm', 'submit'])) {
 			// メールフォームをショートコードを利用する際、ショートコードの利用先でキャッシュを利用している場合、
 			// セキュリティコンポーネントで発行するトークンが更新されない為、強制的にキャッシュをオフにする
 			if (!empty($this->request->params['requested'])) {
