899283bce8a509ed7bc3c935022341dc00a32b31
Ryosuke Momoi <ryosuke.momoi@gmail.com>
2021-01-24 01:29:42 +0900

    BcAdminHelperのテスト追加

--- plugins/baser-core/tests/TestCase/View/Helper/BcAdminHelperTest.php
+++ plugins/baser-core/tests/TestCase/View/Helper/BcAdminHelperTest.php
@@ -20,6 +20,7 @@ use BaserCore\View\Helper\BcAdminHelper;
  * @property BcAdminHelper $BcAdmin
  */
 class BcAdminHelperTest extends BcTestCase {
+
     /**
      * setUp method
      *
@@ -28,8 +29,11 @@ class BcAdminHelperTest extends BcTestCase {
     public function setUp(): void
     {
         parent::setUp();
-        $this->BcAdmin = new BcAdminHelper(new BcAdminAppView($this->getRequest()));
+        $BcAdminAppView = new BcAdminAppView($this->getRequest());
+        $BcAdminAppView->setTheme('BcAdminThird');
+        $this->BcAdmin = new BcAdminHelper($BcAdminAppView);
     }
+
     public function testIsAvailableSideBar() {
         // 未ログイン
         $results = $this->BcAdmin->isAvailableSideBar();
@@ -45,5 +49,140 @@ class BcAdminHelperTest extends BcTestCase {
         $this->assertEquals(false, $results);
         $results = $this->BcAdmin->isAvailableSideBar();
         $this->assertEquals(false, $results);
+    }
+
+    /**
+     * Test setTitle
+     *
+     * @return void
+     */
+    public function testSetTitle() {
+        $title = 'test';
+        $this->BcAdmin->setTitle($title);
+        $this->assertEquals($title, $this->BcAdmin->getView()->get('title'));
+    }
+
+    /**
+     * Test setHelp
+     *
+     * @return void
+     */
+    public function testSetHelp() {
+        $help = 'test';
+        $this->BcAdmin->setHelp($help);
+        $this->assertEquals($help, $this->BcAdmin->getView()->get('help'));
+    }
+
+    /**
+     * Test setSearch
+     *
+     * @return void
+     */
+    public function testSetSearch() {
+        $search = 'test';
+        $this->BcAdmin->setSearch($search);
+        $this->assertEquals($search, $this->BcAdmin->getView()->get('search'));
+    }
+
+    /**
+     * Test title
+     *
+     * @return void
+     */
+    public function testTitle() {
+        ob_start();
+        $this->BcAdmin->title();
+        $actualEmpty = ob_get_clean();
+        $this->assertEmpty($actualEmpty);
+
+        $title = 'test';
+        $this->BcAdmin->getView()->assign('title', $title);
+        ob_start();
+        $this->BcAdmin->title();
+        $actual = ob_get_clean();
+        $this->assertEquals($title, $actual);
+    }
+
+    /**
+     * Test help
+     *
+     * @return void
+     */
+    public function testHelp() {
+        ob_start();
+        $this->BcAdmin->help();
+        $actualEmpty = ob_get_clean();
+        $this->assertEmpty($actualEmpty);
+
+        $help = 'users_index';
+        $expected = $this->BcAdmin->getView()->element('Admin/help', ['help' => $help]);
+
+        $this->BcAdmin->getView()->set('help', $help);
+        ob_start();
+        $this->BcAdmin->help();
+        $actual = ob_get_clean();
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test search
+     *
+     * @return void
+     */
+    public function testSearch() {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test contentsMenu
+     *
+     * @return void
+     */
+    public function testContentsMenu() {
+        // ヘルプなし 未ログイン
+        $expected = $this->BcAdmin->getView()->element('Admin/contents_menu', [
+            'isHelp' => false,
+            'isLogin' => false
+        ]);
+        ob_start();
+        $this->BcAdmin->contentsMenu();
+        $actual = ob_get_clean();
+        $this->assertEquals($expected, $actual);
+
+        // ヘルプあり 未ログイン
+        $expectedIsHelp = $this->BcAdmin->getView()->element('Admin/contents_menu', [
+            'isHelp' => true,
+            'isLogin' => false
+        ]);
+        $this->BcAdmin->getView()->set('help', 'test');
+        ob_start();
+        $this->BcAdmin->contentsMenu();
+        $actualIsHelp = ob_get_clean();
+        $this->assertEquals($expectedIsHelp, $actualIsHelp);
+
+        $session = $this->BcAdmin->getView()->getRequest()->getSession();
+        $session->write('AuthAdmin', true);
+
+        // ヘルプなし ログイン済
+        $expectedIsLogin = $this->BcAdmin->getView()->element('Admin/contents_menu', [
+            'isHelp' => false,
+            'isLogin' => true
+        ]);
+        $this->BcAdmin->getView()->set('help', null);
+        ob_start();
+        $this->BcAdmin->contentsMenu();
+        $actualIsLogin = ob_get_clean();
+        $this->assertEquals($expectedIsLogin, $actualIsLogin);
+
+        // ヘルプあり ログイン済
+        $expectedIsHelpIsLogin = $this->BcAdmin->getView()->element('Admin/contents_menu', [
+            'isHelp' => true,
+            'isLogin' => true
+        ]);
+        $this->BcAdmin->setHelp('test');
+        ob_start();
+        $this->BcAdmin->contentsMenu();
+        $actualIsHelpIsLogin = ob_get_clean();
+        $this->assertEquals($expectedIsHelpIsLogin, $actualIsHelpIsLogin);
     }
 }
