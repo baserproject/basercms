5c26e9c7c46ee928300b8e085a9e01059669426a
ryuring <ryuring55@gmail.com>
2021-03-20 13:23:51 +0900

    Revert "BcBaserHelper を一旦移行"
    This reverts commit 007ff35a3892dc11fbd1b5ab112ff9ea421ced72.

--- plugins/baser-core/src/View/Helper/BcHtmlHelper.php
+++ plugins/baser-core/src/View/Helper/BcHtmlHelper.php
@@ -1,116 +1,23 @@
 <?php
 /**
  * baserCMS :  Based Website Development Project <https://basercms.net>
- * Copyright (c) baserCMS Users Community <https://basercms.net/community/>
+ * Copyright (c) baserCMS User Community <https://basercms.net/community/>
  *
- * @copyright       Copyright (c) baserCMS Users Community
- * @link            https://basercms.net baserCMS Project
- * @package         Baser.View.Helper
- * @since           baserCMS v 0.1.0
- * @license         https://basercms.net/license/index.html
+ * @copyright     Copyright (c) baserCMS User Community
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       http://basercms.net/license/index.html MIT License
  */
 
 namespace BaserCore\View\Helper;
 
 use Cake\View\Helper\HtmlHelper;
-use BaserCore\Annotation\UnitTest;
-use BaserCore\Annotation\NoTodo;
-use BaserCore\Annotation\Checked;
 
 /**
- * Htmlヘルパーの拡張クラス
- *
- * @package Baser.View.Helper
+ * Class BcHtmlHelper
+ * @package BaserCore\View\Helper
  */
 class BcHtmlHelper extends HtmlHelper
 {
 
-// CUSTOMIZE ADD 2014/07/03 ryuring
-// >>>
-	/**
-	 * Included helpers.
-	 *
-	 * @var array
-	 */
-	public $helpers = ['Js', 'Url'];
-
-	/**
-	 * タグにラッピングされていないパンくずデータを取得する
-	 * @return array
-	 */
-	public function getStripCrumbs()
-	{
-		return $this->_crumbs;
-	}
-
-	/**
-	 * JavaScript に変数を引き渡す
-	 *
-	 * @param string $variable 変数名（グローバル変数）
-	 * @param array $value 値（連想配列）
-	 *  - `inline` : インラインに出力するかどうか。（初期値 : false）
-	 *  - `declaration` : var 宣言を行うかどうか（初期値 : true）
-	 *  - `escape` : 値のエスケープを行うかどうか（初期値 : true）
-	 */
-	public function setScript($variable, $value, $options = [])
-	{
-		$options = array_merge([
-			'inline' => false,
-			'declaration' => true,
-			'escape' => true
-		], $options);
-		$code = '';
-		if ($options['declaration']) {
-			$code = 'var ';
-		}
-		if ($options['escape']) {
-			$value = h($value);
-		}
-		$code .= h($variable) . ' = ' . json_encode($value) . ';';
-		unset($options['declaration'], $options['escape']);
-		$result = $this->scriptBlock($code, $options);
-		if ($options['inline']) {
-			return $result;
-		}
-		return '';
-	}
-
-	/**
-	 * i18n 用の変数を宣言する
-	 * @return string
-	 */
-	public function declarationI18n()
-	{
-		return $this->setScript('bcI18n', [], ['inline' => true]);
-	}
-
-	/**
-	 * JavaScript に、翻訳データを引き渡す
-	 * `bcI18n.キー名` で参照可能
-	 * （例）bcI18n.alertMessage
-	 *
-	 * @param array $value 値（連想配列）
-	 *  - `inline` : インラインに出力するかどうか。（初期値 : false）
-	 *  - `declaration` : var 宣言を行うかどうか（初期値 : false）
-	 *  - `escape` : 値のエスケープを行うかどうか（初期値 : true）
-	 */
-	public function i18nScript($data, $options = [])
-	{
-		$options = array_merge([
-			'inline' => false,
-			'declaration' => false,
-			'escape' => true
-		], $options);
-		if (is_array($data)) {
-			$result = '';
-			foreach($data as $key => $value) {
-				$result .= $this->setScript('bcI18n.' . $key, $value, $options) . "\n";
-			}
-			if ($options['inline']) {
-				return $result;
-			}
-		}
-		return '';
-	}
-// <<<
 }
