8e4b560e5b624ebfa7cfef4f9560b75e6194a225
humu <35215680+humuhimi@users.noreply.github.com>
2021-03-25 16:24:40 +0800

    AppViewTest&BcAdminAppViewTest追加 (#47)

---
+++ plugins/baser-core/tests/TestCase/View/AppViewTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) baserCMS User Community <https://basercms.net/community/>
+ *
+ * @copyright     Copyright (c) baserCMS User Community
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       http://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\View;
+
+use BaserCore\TestSuite\BcTestCase;
+use BaserCore\View\AppView;
+
+/**
+ * Class AppViewTest
+ * @package BaserCore\Test\TestCase\View;
+ * @property AppView $AppView
+ */
+class AppViewTest extends BcTestCase
+{
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->AppView = new AppView();
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        unset($this->AppView);
+        parent::tearDown();
+    }
+
+
+    public function testInitialize()
+    {
+        $this->assertNotEmpty($this->AppView->BcAdminForm);
+    }
+}
