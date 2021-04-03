c2d78da412c1c80637bf78d2fe79d21711fbb7a5
yamamoto <yamamoto@kyms.jp>
2021-04-03 13:08:46 +0900

    fix #1618 str_contains() str_starts_with() str_ends_with() 追加 (#1672)

--- lib/Baser/basics.php
+++ lib/Baser/basics.php
@@ -1163,3 +1163,27 @@ function retry($times, callable $callback, $interval = 0)
 		}
 	}
 }
+
+if (!function_exists('str_contains'))
+{
+	function str_contains($haystack, $needle)
+	{
+		return $needle === '' || strpos($haystack, $needle) !== false;
+	}
+}
+
+if (!function_exists('str_starts_with'))
+{
+	function str_starts_with($haystack, $needle)
+	{
+		return strncmp($haystack, $needle, strlen($needle)) === 0;
+	}
+}
+
+if (!function_exists('str_ends_with'))
+{
+	function str_ends_with($haystack, $needle)
+	{
+		return $needle === '' || ($haystack !== '' && substr_compare($haystack, $needle, -strlen($needle)) === 0);
+	}
+}
