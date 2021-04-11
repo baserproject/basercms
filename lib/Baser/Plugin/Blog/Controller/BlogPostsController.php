1013fa5a312941c662709645ff99b280c88c5633
Yamamoto <yamamoto@kyms.jp>
2021-04-11 17:45:47 +0900

    fix #1587 管理画面内の記事一覧においてURLでパラメータを与えられるようにする

--- lib/Baser/Plugin/Blog/Controller/BlogPostsController.php
+++ lib/Baser/Plugin/Blog/Controller/BlogPostsController.php
@@ -120,56 +120,15 @@ class BlogPostsController extends BlogAppController
 			$this->redirect(['plugin' => '', 'controller' => 'contents', 'action' => 'index']);
 		}
 
-		$default = ['named' => [
-			'num' => $this->siteConfigs['admin_list_num'],
-			'sort' => 'no',
-			'direction' => 'desc',
-		]];
-		$this->setViewConditions('BlogPost', ['group' => $blogContentId, 'default' => $default]);
-
-		if($named = Hash::get($this->request->params, 'named')) {
-			$this->request->data['BlogPost'] = Hash::merge(
-				$this->request->data['BlogPost'],
-				$named
-			);
-		}
-		if($this->request->query) {
-			$this->request->data['BlogPost'] = Hash::merge(
-				$this->request->data['BlogPost'],
-				$this->request->query
-			);
-		}
-
-		$joins = [];
-		if (!empty($this->request->data['BlogPost']['blog_tag_id'])) {
-			$db = ConnectionManager::getDataSource($this->BlogPost->useDbConfig);
-			$joins = [
-				[
-					'table' => $db->config['prefix'] . 'blog_posts_blog_tags',
-					'alias' => 'BlogPostsBlogTag',
-					'type' => 'inner',
-					'conditions' => ['BlogPostsBlogTag.blog_post_id = BlogPost.id']
-				],
-				[
-					'table' => $db->config['prefix'] . 'blog_tags',
-					'alias' => 'BlogTag',
-					'type' => 'inner',
-					'conditions' => ['BlogTag.id = BlogPostsBlogTag.blog_tag_id', 'BlogTag.id' => $this->request->data['BlogPost']['blog_tag_id']]
-				]];
-		}
-
-		$conditions = $this->_createAdminIndexConditions($blogContentId, $this->request->data);
-		if (strpos($this->passedArgs['sort'], '.') === false) {
-			$order = 'BlogPost.' . $this->passedArgs['sort'];
-		}
-		if ($order && $this->passedArgs['direction']) {
-			$order .= ' ' . $this->passedArgs['direction'];
-		}
 		$options = [
-			'conditions' => $conditions,
-			'joins' => $joins,
-			'order' => $order,
-			'limit' => $this->passedArgs['num'],
+			'conditions' => $this->_createAdminIndexConditions($blogContentId, $this->request->query),
+			'joins' => $this->request->query('blog_tag_id') ? $this->joinBlogTags($this->request->query('blog_tag_id')) : [],
+			'order' => sprintf(
+				'BlogPost.%s %s',
+				$this->request->query('sort')?:'no',
+				$this->request->query('direction') ?:'desc'
+			),
+			'limit' => $this->request->query('num') ?: $this->siteConfigs['admin_list_num'],
 			'recursive' => 2
 		];
 
@@ -178,31 +137,74 @@ class BlogPostsController extends BlogAppController
 			'options' => $options
 		]);
 		if ($event !== false) {
-			$options = ($event->result === null || $event->result === true)? $event->data['options'] : $event->result;
+			if ($event->result === null || $event->result === true) {
+				$options = $event->data['options'];
+			} else {
+				$options = $event->result;
+			}
 		}
 
-		$this->BlogPost->BlogContent->unbindModel(['hasMany' => ['BlogPost', 'BlogCategory']]);
-		$this->BlogPost->BlogCategory->unbindModel(['hasMany' => ['BlogPost']]);
-		$this->BlogPost->User->unbindModel(['hasMany' => ['Favorite'], 'belongsTo' => ['UserGroup']]);
+		$this->BlogPost->BlogContent->unbindModel(
+			['hasMany' => ['BlogPost', 'BlogCategory']]
+		);
+		$this->BlogPost->BlogCategory->unbindModel(
+			['hasMany' => ['BlogPost']]
+		);
+		$this->BlogPost->User->unbindModel(
+			[
+				'hasMany' => ['Favorite'],
+				'belongsTo' => ['UserGroup']
+			]
+		);
 
 		$this->paginate = $options;
 		$this->set('posts', $this->paginate('BlogPost'));
 
 		$this->_setAdminIndexViewData();
 
-		if ($this->RequestHandler->isAjax() || !empty($this->request->query['ajax'])) {
+		if ($this->request->is('ajax') || !empty($this->request->query['ajax'])) {
 			$this->render('ajax_index');
 			return;
 		}
 
-		if ($this->request->params['Content']['status']) {
-			$this->set('publishLink', $this->Content->getUrl($this->request->params['Content']['url'], true, $this->request->params['Site']['use_subdomain']));
+		if ($this->request->param('Content.status')) {
+			$this->set(
+				'publishLink',
+				$this->Content->getUrl(
+					$this->request->param('Content.url'),
+					true,
+					$this->request->param('Site.use_subdomain')
+				)
+			);
 		}
-		$this->pageTitle = sprintf(__d('baser', '%s｜記事一覧'), strip_tags($this->request->params['Content']['title']));
+		$this->pageTitle = sprintf(
+			__d('baser', '%s｜記事一覧'),
+			strip_tags($this->request->param('Content.title'))
+		);
 		$this->search = 'blog_posts_index';
 		$this->help = 'blog_posts_index';
 	}
 
+
+	protected function joinBlogTags($tag_id) {
+		$db = ConnectionManager::getDataSource($this->BlogPost->useDbConfig);
+		return [
+			[
+				'table' => $db->config['prefix'] . 'blog_posts_blog_tags',
+				'alias' => 'BlogPostsBlogTag',
+				'type' => 'inner',
+				'conditions' => ['BlogPostsBlogTag.blog_post_id = BlogPost.id']
+			],
+			[
+				'table' => $db->config['prefix'] . 'blog_tags',
+				'alias' => 'BlogTag',
+				'type' => 'inner',
+				'conditions' => [
+					'BlogTag.id = BlogPostsBlogTag.blog_tag_id',
+					'BlogTag.id' => $tag_id
+				]
+			]];
+	}
 	/**
 	 * 一覧の表示用データをセットする
 	 *
@@ -228,64 +230,40 @@ class BlogPostsController extends BlogAppController
 	 */
 	protected function _createAdminIndexConditions($blogContentId, $data)
 	{
-		unset($data['ListTool']);
-		$name = $blogCategoryId = '';
-		if (isset($data['BlogPost']['name'])) {
-			$name = $data['BlogPost']['name'];
-		}
-
-		unset($data['BlogPost']['name']);
-		unset($data['_Token']);
-		if (isset($data['BlogPost']['status']) && $data['BlogPost']['status'] === '') {
-			unset($data['BlogPost']['status']);
-		}
-		if (isset($data['BlogPost']['user_id']) && $data['BlogPost']['user_id'] === '') {
-			unset($data['BlogPost']['user_id']);
-		}
-		if (!empty($data['BlogPost']['blog_category_id'])) {
-			$blogCategoryId = $data['BlogPost']['blog_category_id'];
-		}
-		unset($data['BlogPost']['blog_category_id']);
-
 		$conditions = ['BlogPost.blog_content_id' => $blogContentId];
 
-		// CSVの場合はHABTM先のテーブルの条件を直接設定できない為、タグに関連するポストを抽出して条件を生成
+		// CSVの場合はHABTM先のテーブルの条件を直接設定できないため、タグに関連するポストを抽出して条件を生成
 		$db = ConnectionManager::getDataSource($this->BlogPost->useDbConfig);
-
 		if ($db->config['datasource'] == 'Database/BcCsv') {
-			if (!empty($data['BlogPost']['blog_tag_id'])) {
-				$blogTags = $this->BlogPost->BlogTag->read(null, $data['BlogPost']['blog_tag_id']);
+			if (Hash::get($data, 'blog_tag_id')) {
+				$blogTags = $this->BlogPost->BlogTag->read(null, Hash::get($data, 'blog_tag_id'));
 				if ($blogTags) {
 					$conditions['BlogPost.id'] = Hash::extract($blogTags, '{n}.BlogPost.id');
 				}
 			}
 		}
 
-		unset($data['BlogPost']['blog_tag_id']);
-
 		// ページカテゴリ（子カテゴリも検索条件に入れる）
-		if ($blogCategoryId) {
-			$blogCategoryIds = [$blogCategoryId];
-			$children = $this->BlogCategory->children($blogCategoryId);
+		if (Hash::get($data, 'blog_category_id')) {
+			$blogCategoryIds = [$data['blog_category_id']];
+			$children = $this->BlogCategory->children(Hash::get($data, 'blog_category_id'));
 			if ($children) {
 				foreach($children as $child) {
 					$blogCategoryIds[] = $child['BlogCategory']['id'];
 				}
 			}
 			$conditions['BlogPost.blog_category_id'] = $blogCategoryIds;
-		} else {
-			unset($data['BlogPost']['blog_category_id']);
 		}
 
-		if (isset($data['BlogPost']['status'])) {
-			$conditions['BlogPost.status'] = $data['BlogPost']['status'];
+		if (Hash::get($data, 'status')) {
+			$conditions['BlogPost.status'] = Hash::get($data, 'status');
 		}
-		if (isset($data['BlogPost']['user_id'])) {
-			$conditions['BlogPost.user_id'] = $data['BlogPost']['user_id'];
+		if (Hash::get($data, 'user_id')) {
+			$conditions['BlogPost.user_id'] = Hash::get($data, 'user_id');
 		}
 
-		if ($name) {
-			$conditions['BlogPost.name LIKE'] = '%' . $name . '%';
+		if (Hash::get($data, 'name')) {
+			$conditions['BlogPost.name LIKE'] = '%' . Hash::get($data, 'name') . '%';
 		}
 
 		return $conditions;
@@ -371,13 +349,24 @@ class BlogPostsController extends BlogAppController
 		if (!empty($this->siteConfigs['editor_styles'])) {
 			App::uses('CKEditorStyleParser', 'Vendor');
 			$CKEditorStyleParser = new CKEditorStyleParser();
-			$editorStyles = ['default' => $CKEditorStyleParser->parse($this->siteConfigs['editor_styles'])];
+			$editorStyles = [
+				'default' => $CKEditorStyleParser->parse($this->siteConfigs['editor_styles'])
+			];
 			$editorOptions = array_merge($editorOptions, [
 				'editorStylesSet' => 'default',
 				'editorStyles' => $editorStyles
 			]);
 		}
-		$this->crumbs[] = ['name' => sprintf(__d('baser', '%s 記事一覧'), $this->request->params['Content']['title']), 'url' => ['controller' => 'blog_posts', 'action' => 'index', $blogContentId]];
+		$this->crumbs[] = [
+			'name' => sprintf(
+				__d('baser', '%s 記事一覧'),
+				$this->request->params['Content']['title']
+			),
+			'url' => [
+				'controller' => 'blog_posts',
+				'action' => 'index', $blogContentId
+			]
+		];
 		$this->set('hasNewCategoryAddablePermission', $this->BlogPost->BlogCategory->hasNewCategoryAddablePermission($user['user_group_id'], $blogContentId));
 		$this->set('hasNewTagAddablePermission', $this->BlogPost->BlogTag->hasNewTagAddablePermission($user['user_group_id'], $blogContentId));
 		$this->set('editable', true);
