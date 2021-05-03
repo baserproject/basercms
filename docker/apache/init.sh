75bbdd8982c481b2961f003bc676853cd5ff3e81
ryuring <ryuring55@gmail.com>
2021-05-03 13:20:27 +0900

    初期化処理を Migration に変更

--- docker/apache/init.sh
+++ docker/apache/init.sh
@@ -2,6 +2,8 @@ if [ ! -e '/check' ]; then
     touch /check
     composer install --no-plugins
     cp /var/www/html/config/.env.example /var/www/html/config/.env
+    sleep 10
 	bin/cake migrations migrate --plugin baser-core
+	bin/cake migrations seed --plugin baser-core
     echo "container setup is complete"
 fi
