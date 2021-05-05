a695a60f2bfd2930de69cf2cdaa0fcbe7f5540b6
ryuring <ryuring55@gmail.com>
2021-05-05 17:13:09 +0900

    CsrfProtectionMiddleware を有効化

--- src/Application.php
+++ src/Application.php
@@ -97,13 +97,13 @@ class Application extends BaseApplication
             // Parse various types of encoded request bodies so that they are
             // available as array through $request->getData()
             // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
-            ->add(new BodyParserMiddleware());
+            ->add(new BodyParserMiddleware())
 
             // Cross Site Request Forgery (CSRF) Protection Middleware
             // https://book.cakephp.org/4/en/controllers/middleware.html#cross-site-request-forgery-csrf-middleware
-//            ->add(new CsrfProtectionMiddleware([
-//                'httponly' => true,
-//            ]));
+            ->add(new CsrfProtectionMiddleware([
+                'httponly' => true,
+            ]));
 
         return $middlewareQueue;
     }
