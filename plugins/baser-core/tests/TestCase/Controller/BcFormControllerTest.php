186beafa9d203291a5bcc09b9e909afeb7cf1569
humuhimi <masa19951009@gmail.com>
2021-05-16 01:17:44 +0800

    BcFormControllerTest追加

---
+++ plugins/baser-core/tests/TestCase/Controller/BcFormControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) baserCMS User Community <https://basercms.net/community/>
+ *
+ * @copyright     Copyright (c) baserCMS User Community
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       http://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestTrait;
+use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Controller\BcFormController;
+
+/**
+ * BaserCore\Controller\BcFormController Test Case
+ */
+class BcFormControllerTest extends BcTestCase
+{
+    use IntegrationTestTrait;
+    /**
+     * set up
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->BcFormController = new BcFormController($this->getRequest());
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test get_token
+     *
+     * @return void
+     */
+    public function testGet_token()
+    {
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        $this->get('/baser/baser-core/bc_form/get_token?requestview=false');
+        $this->assertNotEmpty($this->_getBodyAsString());
+    }
+
+}
