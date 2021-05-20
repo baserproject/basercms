b6e3f10333e81e9f1db97feb39b36b3c3a8edd1c
gondoh <gondoh@gufii.net>
2021-05-20 15:53:15 +0900

    DBログにて削除されたユーザが*systemと表示される点を削除ユーザーとして改善

---
+++ app/webroot/theme/admin-third/Dblogs/admin/ajax_index_list.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) baserCMS Users Community <https://basercms.net/community/>
+ *
+ * @copyright       Copyright (c) baserCMS Users Community
+ * @link            https://basercms.net baserCMS Project
+ * @package         Baser.View
+ * @since           baserCMS v 4.0.0
+ * @license         https://basercms.net/license/index.html
+ */
+header('Content-type: text/html; charset=utf-8');
+$this->BcBaser->element('dblogs/index_list');
