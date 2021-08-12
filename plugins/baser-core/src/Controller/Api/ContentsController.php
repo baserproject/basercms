ca82bf89e67bee66e90b5276810921fe940bd0a9
humuhimi <masa19951009@gmail.com>
2021-08-12 11:36:52 +0800

    Api/ContentsController型だけ追加

---
+++ plugins/baser-core/src/Controller/Api/ContentsController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) baserCMS User Community <https://basercms.net/community/>
+ *
+ * @copyright     Copyright (c) baserCMS User Community
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       http://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Controller\Api;
+
+use BaserCore\Service\ContentsServiceInterface;
+use Exception;
+use BaserCore\Annotation\UnitTest;
+use BaserCore\Annotation\NoTodo;
+use BaserCore\Annotation\Checked;
+
+/**
+ * Class ContentsController
+ * @package BaserCore\Controller\Api
+ */
+class ContentsController extends BcApiController
+{
+
+    /**
+     * index
+     *
+     * @param  ContentsServiceInterface $contents
+     * @return void
+     */
+    public function index(ContentsServiceInterface $contents)
+    {
+        $this->set([
+            'contents' => $this->paginate($contents->getTableIndex($this->request->getQueryParams()))
+        ]);
+        $this->viewBuilder()->setOption('serialize', ['contents']);
+    }
+}
