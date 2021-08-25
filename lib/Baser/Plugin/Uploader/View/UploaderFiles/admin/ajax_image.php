568d4cab5ba1cdee7bbf0133c676d02a98f6d7bc
ryuring <ryuring55@gmail.com>
2021-08-25 21:52:17 +0900

    Merge pull request from GHSA-hgjr-632x-qpp3

--- lib/Baser/Plugin/Uploader/View/UploaderFiles/admin/ajax_image.php
+++ lib/Baser/Plugin/Uploader/View/UploaderFiles/admin/ajax_image.php
@@ -9,12 +9,18 @@
  * @since           baserCMS v 3.0.10
  * @license         https://basercms.net/license/index.html
  */
-$url = $this->BcBaser->getUrl($this->Uploader->getFileUrl($file['UploaderFile']['name']));
+
+/**
+ * @var BcAppView $this
+ * @var array $file
+ * @var string $size
+ */
+$url = h($this->BcBaser->getUrl($this->Uploader->getFileUrl($file['UploaderFile']['name'])));
 ?>
 
 
 <p class="url">
-	<a href="<?php echo $url ?>" target="_blank"><?php echo FULL_BASE_URL . $url ?></a>
+	<a href="<?php echo $url ?>" target="_blank"><?php echo Router::url($url, true) ?></a>
 </p>
 <p class="image">
 	<a href="<?php echo $url ?>"
