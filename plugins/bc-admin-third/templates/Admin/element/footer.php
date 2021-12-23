d0e1ec991e36ee1546f6782ae838a76e0fedf6ca
humuhimi <masa19951009@gmail.com>
2021-12-23 20:12:11 +0800

    strongタグ不足分追加

--- plugins/bc-admin-third/templates/Admin/element/footer.php
+++ plugins/bc-admin-third/templates/Admin/element/footer.php
@@ -21,7 +21,7 @@ use BaserCore\View\AppView;
 <div id="Footer" class="bca-footer" data-loggedin="">
   <div class="bca-footer__inner--full">
     <div class="bca-footer__main">
-      <div class="bca-footer__baser-version">baserCMS</strong></div>
+      <div class="bca-footer__baser-version"><strong>baserCMS</strong></div>
       <ul class="bca-footer__banner">
         <li class="bca-footer__banner__item"></li>
         <li class="bca-footer__banner__item"></li>
