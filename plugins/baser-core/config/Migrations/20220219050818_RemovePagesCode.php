52f3c7528de42eb39ceb6533f49f899104d1a2f7
seto1 <30764014+seto1@users.noreply.github.com>
2022-02-19 17:00:27 +0900

    pagesテーブルからcodeカラムを削除 (#448)

---
+++ plugins/baser-core/config/Migrations/20220219050818_RemovePagesCode.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class RemovePagesCode extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('pages');
+        $table->removeColumn('code')
+            ->save();
+    }
+}
