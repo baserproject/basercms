f682ea21765a2a7dad81f8801aea965d83243521
ryuring <ryuring55@gmail.com>
2022-02-19 15:59:59 +0900

    Dockerの初期化ファイルを調整
    権限の問題に対応

--- docker/apache/init.sh
+++ docker/apache/init.sh
@@ -1,14 +1,19 @@
 if [ ! -e '/var/www/html/docker/check' ]; then
-    touch /var/www/html/docker/check
+    rm -rf /var/www/html/tmp
+    rm -rf /var/www/html/logs
+	rm /var/www/html/config/jwt.key
+	rm /var/www/html/config/jwt.pem
     composer install --no-plugins
     cp /var/www/html/config/.env.example /var/www/html/config/.env
     sleep 10
 	/var/www/html/bin/cake migrations migrate --plugin BaserCore
 	/var/www/html/bin/cake migrations seed --plugin BaserCore
-	/var/www/html/bin/cake migrations migrate --plugin BcPage
-	/var/www/html/bin/cake migrations seed --plugin BcPage
 	/var/www/html/bin/cake plugin assets symlink
     openssl genrsa -out /var/www/html/config/jwt.key 1024
     openssl rsa -in /var/www/html/config/jwt.key -outform PEM -pubout -out /var/www/html/config/jwt.pem
+    chown www-data.www-data /var/www/html/config/jwt.key
+    touch /var/www/html/docker/check
+    chmod 777 -R /var/www/html/tmp
+    chmod 777 -R /var/www/html/logs
     echo "container setup is complete"
 fi
