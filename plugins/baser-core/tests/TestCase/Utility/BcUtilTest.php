7d6ffbca59f6a12ffea1bca73cf74a779d5ace83
uchiba <uchiba@catchup.co.jp>
2022-03-14 16:07:52 +0900

    [todocheck#1168] assertCountの評価値をintに変更

--- plugins/baser-core/tests/TestCase/Utility/BcUtilTest.php
+++ plugins/baser-core/tests/TestCase/Utility/BcUtilTest.php
@@ -389,9 +389,9 @@ class BcUtilTest extends BcTestCase
     {
         return [
             // ログイン
-            [1, true],
+            [1, 1],
             // 非ログイン
-            [0, false],
+            [0, 0],
         ];
     }
 
