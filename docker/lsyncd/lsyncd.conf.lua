79615f6e7eb4d8fc38a9156a5e9a12748b9f49b2
ryuring <ryuring55@gmail.com>
2022-03-27 17:48:39 +0900

    Docker で利用する lsyncd の設定を変更
    逆同期について tmp / logs / files を明示的に指定するようにした

--- docker/lsyncd/lsyncd.conf.lua
+++ docker/lsyncd/lsyncd.conf.lua
@@ -13,9 +13,22 @@ sync {
 }
 sync {
         default.rsync,
-        source = "/var/www/html/",
-        target = "/var/www/shared/",
-        delay = 1,
+        source = "/var/www/html/tmp",
+        target = "/var/www/shared/tmp",
+        delay = 0,
         delete="running",
-        exclude={"node_modules", "tmp", ".git", ".idea", ".DS_Store", "docker"}
+}
+sync {
+        default.rsync,
+        source = "/var/www/html/logs",
+        target = "/var/www/shared/logs",
+        delay = 0,
+        delete="running",
+}
+sync {
+        default.rsync,
+        source = "/var/www/html/webroot/files",
+        target = "/var/www/shared/webroot/files",
+        delay = 0,
+        delete="running",
 }
