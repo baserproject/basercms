31808de3920f939669411428ced87e9b7e98bf49
egashira <egashira@catchup.co.jp>
2022-04-11 15:11:03 +0900

    初回起動時のDB初期化処理を調整
    MySQLへの疎通確認を追加

--- docker/init.sh
+++ docker/init.sh
@@ -5,42 +5,37 @@
 # マイグレーションを実行する際、DBの起動より先に実行すると失敗してしまうため sleep で待つようにしている
 #
 rsync -a /var/www/shared/ /var/www/html --exclude='node_modules' --exclude='tmp' --exclude='logs' --exclude='.git' --exclude='.idea' --exclude='.DS_Store' --exclude='docker' --exclude='config/jwt.key' --exclude='config/jwt.pem'
-
-if [ ! -d '/var/www/html/tmp' ]; then
-    mkdir /var/www/html/tmp
-    mkdir /var/www/html/tmp/cache
-    mkdir /var/www/html/tmp/cache/models
-    mkdir /var/www/html/tmp/cache/persistent
-    mkdir /var/www/html/tmp/cache/views
-    mkdir /var/www/html/tmp/sessions
-    mkdir /var/www/html/tmp/tests
-fi
-if [ ! -d '/var/www/html/logs' ]; then
-    mkdir /var/www/html/logs
-fi
-chmod 777 -R /var/www/html/tmp
-chmod 777 -R /var/www/html/logs
+service lsyncd start
 
 if [ ! -e '/var/www/shared/docker/check' ]; then
-    if [ -e '/var/www/shared/config/jwt.key' ]; then
-	    rm /var/www/shared/config/jwt.key
-	fi
-	if [ -e '/var/www/shared/config/jwt.pem' ]; then
-	    rm /var/www/shared/config/jwt.pem
-	fi
     composer install --no-plugins
     cp /var/www/html/config/.env.example /var/www/html/config/.env
-    sleep 10
-	/var/www/html/bin/cake migrations migrate --plugin BaserCore
-	/var/www/html/bin/cake migrations seed --plugin BaserCore
-	/var/www/html/bin/cake plugin assets symlink
-	chmod 777 -R /var/www/html/tmp
-	chmod 777 -R /var/www/html/logs
     openssl genrsa -out /var/www/html/config/jwt.key 1024
     openssl rsa -in /var/www/html/config/jwt.key -outform PEM -pubout -out /var/www/html/config/jwt.pem
     chown www-data.www-data /var/www/html/config/jwt.key
+    TIMES=0
+    LIMIT_TIMES=50
+    CONNECTED=1
+    while [ "$(mysqladmin ping -h bc5-db -uroot -proot)" != "mysqld is alive" ]
+    do
+        echo "try connect $TIMES times"
+        sleep 1
+        TIMES=`expr $TIMES + 1`
+        if [ $TIMES -eq $LIMIT_TIMES ]; then
+            CONNECTED=0
+            echo "MySQL timeout."
+            break
+        fi
+    done
+    if [ $CONNECTED -eq 1 ]; then
+        /var/www/html/bin/cake migrations migrate --plugin BaserCore
+        /var/www/html/bin/cake migrations seed --plugin BaserCore
+        /var/www/html/bin/cake plugin assets symlink
+        rm -rf /var/www/html/tmp/cache
+    else
+        echo "Migration failed."
+	fi
     touch /var/www/shared/docker/check
-    echo "container setup is complete"
+    echo "Container setup is complete."
 fi
 
-service lsyncd start
