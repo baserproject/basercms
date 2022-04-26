dc5485f93199ac7e7232b32578a6d12868d83189
ryuring <ryuring55@gmail.com>
2022-04-26 23:43:40 +0900

    lsyncd の設定変更
    tmp / logs などを双方間同期に変更

--- docker/lsyncd/lsyncd.conf.lua
+++ docker/lsyncd/lsyncd.conf.lua
@@ -20,8 +20,22 @@ sync {
 }
 sync {
         default.rsync,
+        source = "/var/www/shared/tmp",
+        target = "/var/www/html/tmp",
+        delay = 0,
+        delete="running",
+}
+sync {
+        default.rsync,
         source = "/var/www/html/logs",
         target = "/var/www/shared/logs",
+        delay = 0,
+        delete="running",
+}
+sync {
+        default.rsync,
+        source = "/var/www/shared/logs",
+        target = "/var/www/html/logs",
         delay = 0,
         delete="running",
 }
@@ -34,8 +48,22 @@ sync {
 }
 sync {
         default.rsync,
+        source = "/var/www/shared/webroot/files",
+        target = "/var/www/html/webroot/files",
+        delay = 0,
+        delete="running",
+}
+sync {
+        default.rsync,
         source = "/var/www/html/vendor",
         target = "/var/www/shared/vendor",
+        delay = 0,
+        delete="running",
+}
+sync {
+        default.rsync,
+        source = "/var/www/shared/vendor",
+        target = "/var/www/html/vendor",
         delay = 0,
         delete="running",
 }
