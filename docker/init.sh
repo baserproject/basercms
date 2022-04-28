e82ccc2789e8a0220c6937ccf054a406ffc9c105
ryuring <ryuring55@gmail.com>
2022-04-28 16:57:05 +0900

    Dockerの初期化処理を改善
    書込み権限のエラーが出ていたため

--- docker/init.sh
+++ docker/init.sh
@@ -4,16 +4,33 @@
 # /var/www/shared と /var/www/html の同期、マイグレーション、書き込み権限の変更などを行う
 # マイグレーションを実行する際、DBの起動より先に実行すると失敗してしまうため sleep で待つようにしている
 #
-rsync -a /var/www/shared/ /var/www/html --exclude='node_modules' --exclude='tmp' --exclude='logs' --exclude='.git' --exclude='.idea' --exclude='.DS_Store' --exclude='docker' --exclude='config/jwt.key' --exclude='config/jwt.pem'
-if [ ! -d '/var/www/html/tmp' ]; then
-    mkdir /var/www/html/tmp
-fi
-if [ ! -d '/var/www/html/logs' ]; then
-    mkdir /var/www/html/logs
-fi
 
 if [ ! -e '/var/www/shared/docker/check' ]; then
     rm -rf /var/www/shared/vendor/*
+    if [ -d '/var/www/shared/tmp' ]; then
+        rm -rf /var/www/shared/tmp
+    fi
+    mkdir /var/www/shared/tmp
+    if [ -d '/var/www/shared/logs' ]; then
+        rm -rf /var/www/shared/logs
+    fi
+    mkdir /var/www/shared/logs
+fi
+
+rsync -a /var/www/shared/ /var/www/html --exclude='node_modules' --exclude='tmp' --exclude='logs' --exclude='.git' --exclude='.idea' --exclude='.DS_Store' --exclude='docker' --exclude='config/jwt.key' --exclude='config/jwt.pem'
+
+if [ ! -d '/var/www/html/tmp' ]; then
+    mkdir /var/www/html/tmp
+
+fi
+if [ ! -d '/var/www/html/logs' ]; then
+    mkdir /var/www/html/logs
+
+fi
+chmod -R 777 /var/www/html/tmp
+chmod -R 777 /var/www/html/logs
+
+if [ ! -e '/var/www/shared/docker/check' ]; then
     composer install --no-plugins
     cp /var/www/html/config/.env.example /var/www/html/config/.env
     openssl genrsa -out /var/www/html/config/jwt.key 1024
