4f8c71ba10745cb647e6941b8b846bd5346b0569
ryuring <ryuring55@gmail.com>
2022-06-12 12:40:28 +0900

    Dockerコンテナの再構築時に書込み権限の問題でアクセスできなくなる問題を改善

--- docker/init.sh
+++ docker/init.sh
@@ -2,21 +2,14 @@
 # init.sh
 # コンテナの初期化
 #
-rsync -av /var/www/shared/ /var/www/html --exclude='node_modules' --exclude='app/tmp' --exclude='app/webroot/files'  --exclude='app/View/Pages' --exclude='.git' --exclude='.idea' --exclude='.DS_Store' --exclude='.vagrant' --exclude='docker'
-if [ ! -d '/var/www/html/app/tmp' ]; then
-    mkdir /var/www/html/app/tmp
-fi
-if [ ! -d '/var/www/html/app/View/Pages' ]; then
-    mkdir /var/www/html/app/View/Pages
-fi
-if [ ! -d '/var/www/html/app/webroot/files' ]; then
-    mkdir /var/www/html/app/webroot/files
-fi
-if [ ! -d '/var/www/html/docker' ]; then
-    mkdir /var/www/html/docker
-fi
+rsync -av /var/www/shared/ /var/www/html --exclude='node_modules' --exclude='.git' --exclude='.idea' --exclude='.DS_Store' --exclude='.vagrant' --exclude='docker'
 
 if [ ! -e '/var/www/shared/docker/check' ]; then
+    chmod -R 777 /var/www/shared/app/tmp/*
+    mkdir /var/www/html/app/tmp
+    mkdir /var/www/html/app/View/Pages
+    mkdir /var/www/html/app/webroot/files
+    mkdir /var/www/html/docker
     chmod -R 777 /var/www/html/app/Config
     chmod -R 777 /var/www/html/app/tmp
     chmod -R 777 /var/www/html/app/View/Pages
@@ -25,9 +18,9 @@ if [ ! -e '/var/www/shared/docker/check' ]; then
     chmod -R 777 /var/www/html/app/webroot/js
     chmod -R 777 /var/www/html/app/webroot/css
     chmod -R 777 /var/www/html/app/webroot/theme
+    touch /var/www/shared/docker/check
     mkdir /etc/lsyncd
     cp /var/www/shared/docker/lsyncd/lsyncd.conf.lua /etc/lsyncd/
-    touch /var/www/shared/docker/check
 fi
 
 service lsyncd start
