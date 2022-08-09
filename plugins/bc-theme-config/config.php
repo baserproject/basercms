9f2e3656fc2d79bf8202048252a89363cd0c9404
ryuring <ryuring55@gmail.com>
2022-08-09 17:44:35 +0900

    bc-editor-template / bc-theme-config / bc-theme-file / bc-widget-area を別プラグイン化

---
+++ plugins/bc-theme-config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+return [
+    'type' => 'CorePlugin',
+    'title' => __d('baser', 'テーマ設定'),
+    'description' => __d('baser', 'テーマ拡張の設定機能'),
+    'author' => 'baserCMS User Community',
+    'url' => 'https://basercms.net',
+    'installMessage' => ''
+];
---
+++ plugins/bc-theme-file/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+return [
+    'type' => 'CorePlugin',
+    'title' => __d('baser', 'テーマファイル管理'),
+    'description' => __d('baser', 'テーマ内のファイルを管理する機能'),
+    'author' => 'baserCMS User Community',
+    'url' => 'https://basercms.net',
+    'installMessage' => ''
+];
