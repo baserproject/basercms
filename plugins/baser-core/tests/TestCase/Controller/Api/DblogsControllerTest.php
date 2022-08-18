6f61114c002493ff3e37f2c6f6689d16b198c892
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2022-08-18 15:24:40 +0900

    fix #764　ログ削除API (#773)

---
+++ plugins/baser-core/tests/TestCase/Controller/Api/DblogsControllerTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\Controller\Api;
+
+use BaserCore\TestSuite\BcTestCase;
+use Cake\TestSuite\IntegrationTestTrait;
+
+class DblogsControllerTest extends BcTestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+        'plugin.BaserCore.Dblogs',
+        'plugin.BaserCore.Users',
+        'plugin.BaserCore.UsersUserGroups',
+        'plugin.BaserCore.UserGroups',
+        'plugin.BaserCore.SiteConfigs',
+        'plugin.BaserCore.Sites',
+        'plugin.BaserCore.Contents',
+    ];
+
+    /**
+     * Access Token
+     * @var string
+     */
+    public $accessToken = null;
+
+    /**
+     * Refresh Token
+     * @var null
+     */
+    public $refreshToken = null;
+    /**
+     * Set Up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $token = $this->apiLoginAdmin(1);
+        $this->accessToken = $token['access_token'];
+        $this->refreshToken = $token['refresh_token'];
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test delete_all
+     */
+    public function testDelete_all()
+    {
+        $this->get('/baser/api/baser-core/dblogs/delete_all/dblogs.json?token=' . $this->accessToken);
+        $this->assertResponseCode(405);
+
+        $this->post('/baser/api/baser-core/dblogs/delete_all/dblogs.json?token=' . $this->accessToken);
+        $this->assertResponseSuccess();
+        $result = json_decode((string)$this->_response->getBody());
+        $this->assertEquals('最近の動きのログを削除しました。', $result->message);
+
+        $this->post('/baser/api/baser-core/dblogs/delete_all/test.json?token=' . $this->accessToken);
+        $this->assertResponseCode(400);
+        $result = json_decode((string)$this->_response->getBody());
+        $this->assertEquals('最近の動きのログ削除に失敗しました。', $result->message);
+    }
+}
