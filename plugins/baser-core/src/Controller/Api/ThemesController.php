ffc1154db9caff2400b910c68883008f11bc4732
Đỗ Văn Hùng <HungDV2022>
2022-09-12 17:40:33 +0900

    fix #931 新しいテーマをアップロードするAPIを実装（dev-#8） #931 APIを実装

---
+++ plugins/baser-core/src/Controller/Api/ThemesController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Controller\Api;
+
+use BaserCore\Error\BcException;
+use BaserCore\Service\ThemesServiceInterface;
+use BaserCore\Annotation\UnitTest;
+use BaserCore\Annotation\NoTodo;
+use BaserCore\Annotation\Checked;
+
+/**
+ * Class ThemesController
+ *
+ * https://localhost/baser/api/baser-core/themes/action_name.json で呼び出す
+ *
+ * @package BaserCore\Controller\Api
+ */
+class ThemesController extends BcApiController
+{
+
+    /**
+     * [API] 新しいテーマをアップロードする
+     * @param ThemesServiceInterface $service
+     * @noTodo
+     */
+    public function add(ThemesServiceInterface $service)
+    {
+        $this->request->allowMethod(['post']);
+
+        $theme = null;
+        $errors = null;
+        try {
+            $theme = $service->add($this->getRequest()->getData());
+            $message = __d('baser', 'テーマファイル「' . $theme->name . '」を追加しました。');
+        } catch (BcException $e) {
+            $errors = $e->getMessage();
+            $this->setResponse($this->response->withStatus(400));
+            $message = __d('baser', 'ファイルのアップロードに失敗しました。' . $e->getMessage());
+        }
+
+        $this->set([
+            'message' => $message,
+            'theme' => $theme,
+            'errors' => $errors
+        ]);
+        $this->viewBuilder()->setOption('serialize', ['message', 'theme', 'errors']);
+    }
+
+}
