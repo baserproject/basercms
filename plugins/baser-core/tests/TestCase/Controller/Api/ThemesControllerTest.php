97ccf01c0367fcad367677929ce766861ae07725
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2022-09-14 14:57:42 +0900

    fix #929 テーマ一覧を取得するAPIを実装（dev-#8） (#946)

---
+++ plugins/baser-core/tests/TestCase/Controller/Api/ThemesControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\Controller\Api;
+
+use BaserCore\Test\Scenario\InitAppScenario;
+use Cake\Core\Configure;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
+
+class ThemesControllerTest extends \BaserCore\TestSuite\BcTestCase
+{
+
+    /**
+     * Trait
+     */
+    use ScenarioAwareTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/Sites',
+    ];
+
+    /**
+     * Access Token
+     * @var string
+     */
+    public $accessToken = null;
+
+    /**
+     * Refresh Token
+     * @var null
+     */
+    public $refreshToken = null;
+
+    /**
+     * set up
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $token = $this->apiLoginAdmin(1);
+        $this->accessToken = $token['access_token'];
+        $this->refreshToken = $token['refresh_token'];
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        Configure::clear();
+        parent::tearDown();
+    }
+
+    /**
+     * test View
+     */
+    public function testIndex(): void
+    {
+        $this->get('/baser/api/baser-core/themes/index.json?token=' . $this->accessToken);
+        $this->assertResponseOk();
+        $result = json_decode((string)$this->_response->getBody());
+        $this->assertCount(2, $result->themes);
+        $this->assertEquals('BcThemeSample', $result->themes[0]->name);
+        $this->assertEquals('BcFront', $result->themes[1]->name);
+    }
+
+}
