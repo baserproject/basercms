94ebc69fb0e2eed3bbf389490c22bd8d41871a6b
Đỗ Văn Hùng <HungDV2022>
2022-10-05 09:04:09 +0900

    テストメソードを追加

---
+++ plugins/baser-core/tests/TestCase/Database/Schema/BcSchemaTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\Database\Schema;
+
+use BaserCore\TestSuite\BcTestCase;
+
+/**
+ * Class BcSchemaTest
+ *
+ * @package BaserCore\Test\TestCase\Database\Schema
+ */
+class BcSchemaTest extends BcTestCase
+{
+
+    /**
+     * set up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * tearDown
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test __construct
+     */
+    public function test_construct()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * Test connection
+     */
+    public function test_connection()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * Test init
+     */
+    public function test_init()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * Test create
+     */
+    public function test_create()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+}
