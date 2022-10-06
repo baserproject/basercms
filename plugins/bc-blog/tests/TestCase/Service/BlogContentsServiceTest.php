ec02ebd1befef71f6a156b0cfbcf95d05ca3e8ba
Đỗ Văn Hùng <HungDV2022>
2022-10-06 17:29:45 +0900

    ユニットテストを追加

---
+++ plugins/bc-blog/tests/TestCase/Service/BlogContentsServiceTest.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BcBlog\Test\TestCase\Service;
+
+use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcContainerTrait;
+use BcBlog\Service\BlogContentsService;
+use Cake\TestSuite\IntegrationTestTrait;
+
+/**
+ * BlogContentsServiceTest
+ * @property BlogContentsService $BlogContentsService
+ */
+class BlogContentsServiceTest extends BcTestCase
+{
+
+    /**
+     * Trait
+     */
+    use BcContainerTrait;
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+    ];
+
+    /**
+     * Set Up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->setFixtureTruncate();
+        parent::setUp();
+        $this->BlogContentsService = new BlogContentsService();
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * test __construct
+     */
+    public function test__construct()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test get
+     */
+    public function test_get()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test getIndex
+     */
+    public function test_getIndex()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test getList
+     */
+    public function test_getList()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test getNew
+     */
+    public function test_getNew()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test update
+     */
+    public function test_update()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test create
+     */
+    public function test_create()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test copy
+     */
+    public function test_copy()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test delete
+     */
+    public function test_delete()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+}
