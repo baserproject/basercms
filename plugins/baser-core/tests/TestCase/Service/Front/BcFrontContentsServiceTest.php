de3eedfd8234ef19e7b347222029dd90e660cc2d
Đỗ Văn Hùng <HungDV2022>
2022-10-18 15:23:11 +0900

    ユニットテストメソードを追加

---
+++ plugins/baser-core/tests/TestCase/Service/Front/BcFrontContentsServiceTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\Service\Front;
+
+use BaserCore\Service\Front\BcFrontContentsService;
+use BaserCore\TestSuite\BcTestCase;
+
+/**
+ * BcFrontContentsServiceTest
+ *
+ * @property BcFrontContentsService $BcFrontContentsService
+ */
+class BcFrontContentsServiceTest extends BcTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+    ];
+
+    /**
+     * Set Up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->BcFrontContentsService = new BcFrontContentsService();
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        unset($this->BcFrontContentsService);
+        parent::tearDown();
+    }
+
+    /**
+     * test getViewVarsForFront
+     */
+    public function test_getViewVarsForFront()
+    {
+
+    }
+
+    /**
+     * test getCrumbs
+     */
+    public function test_getCrumbs()
+    {
+
+    }
+}
