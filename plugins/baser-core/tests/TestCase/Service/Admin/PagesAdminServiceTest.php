e67d35ab0abcf317129b08850f9c75b0de18a0dc
Đỗ Văn Hùng <HungDV2022>
2022-10-21 15:51:12 +0900

    fix #1200 ユニットテストを調整

--- plugins/baser-core/tests/TestCase/Service/Admin/PagesAdminServiceTest.php
+++ plugins/baser-core/tests/TestCase/Service/Admin/PagesAdminServiceTest.php
@@ -29,14 +29,7 @@ class PagesAdminServiceTest extends \BaserCore\TestSuite\BcTestCase
      * @var array
      */
     public $fixtures = [
-        'plugin.BaserCore.Factory/Sites',
-        'plugin.BaserCore.Factory/SiteConfigs',
-        'plugin.BaserCore.Factory/Users',
-        'plugin.BaserCore.Factory/UsersUserGroups',
-        'plugin.BaserCore.Factory/UserGroups',
         'plugin.BaserCore.Factory/Contents',
-        'plugin.BaserCore.Factory/ContentFolders',
-        'plugin.BaserCore.Factory/Permissions',
         'plugin.BaserCore.Factory/Pages',
     ];
 
