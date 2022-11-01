142cd282a6d9db962127f98926c95bb94f2e1c09
ryuring <ryuring55@gmail.com>
2022-11-01 17:32:40 +0900

    BcThemeFile のサービスクラスを追加

---
+++ plugins/bc-theme-file/src/ServiceProvider/BcThemeFileServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BcThemeFile\ServiceProvider;
+
+use BaserCore\Annotation\UnitTest;
+use BaserCore\Annotation\NoTodo;
+use BaserCore\Annotation\Checked;
+use BcThemeFile\Service\ThemeFilesService;
+use BcThemeFile\Service\ThemeFilesServiceInterface;
+use Cake\Core\ServiceProvider;
+
+/**
+ * Class BcThemeFileServiceProvider
+ */
+class BcThemeFileServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Provides
+     * @var string[]
+     */
+    protected $provides = [
+        ThemeFilesServiceInterface::class
+    ];
+
+    /**
+     * Services
+     * @param \Cake\Core\ContainerInterface $container
+     * @checked
+     * @noTodo
+     */
+    public function services($container): void
+    {
+        $container->defaultToShared(true);
+        // Installations サービス
+        $container->add(ThemeFilesServiceInterface::class, ThemeFilesService::class);
+    }
+
+}
