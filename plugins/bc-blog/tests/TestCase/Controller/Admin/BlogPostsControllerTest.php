3808b64bfc1fe0c1989104dcf2377d650dbd2e46
Đỗ Văn Hùng <HungDV2022>
2022-11-11 21:03:36 +0900

    ユニットテストメソードを追加

--- plugins/bc-blog/tests/TestCase/Controller/Admin/BlogPostsControllerTest.php
+++ plugins/bc-blog/tests/TestCase/Controller/Admin/BlogPostsControllerTest.php
@@ -1,6 +1,5 @@
 <?php
-// TODO : コード確認要
-return;
+
 /**
  * baserCMS :  Based Website Development Project <https://basercms.net>
  * Copyright (c) baserCMS Users Community <https://basercms.net/community/>
@@ -12,25 +11,51 @@ return;
  * @license         https://basercms.net/license/index.html
  */
 
-App::uses('BlogPostsController', 'BcBlog.Controller');
+namespace BcBlog\Test\TestCase\Controller\Admin;
+
+use BaserCore\Test\Scenario\InitAppScenario;
+use BaserCore\TestSuite\BcTestCase;
+use BcBlog\Controller\Admin\BlogPostsController;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * Class BlogPostsControllerTest
  *
- * @package Blog.Test.Case.Controller
- * @property  BlogPostsController $BlogPostsController
+ * @property BlogPostsController $BlogPostsController
  */
-class BlogPostsControllerTest extends BaserTestCase
+class BlogPostsControllerTest extends BcTestCase
 {
+
+    /**
+     * Trait
+     */
+    use ScenarioAwareTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups',
+        'plugin.BcBlog.Factory/BlogPosts',
+    ];
 
     /**
      * set up
      *
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
+        $this->setFixtureTruncate();
         parent::setUp();
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $this->BlogPostsController = new BlogPostsController($this->loginAdmin($this->getRequest()));
     }
 
     /**
@@ -38,13 +63,21 @@ class BlogPostsControllerTest extends BaserTestCase
      *
      * @return void
      */
-    public function tearDown()
+    public function tearDown(): void
     {
         parent::tearDown();
     }
 
     /**
-     * beforeFilter
+     * test initialize
+     */
+    public function testInitialize(): void
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test beforeFilter
      */
     public function testBeforeFilter()
     {
@@ -52,74 +85,66 @@ class BlogPostsControllerTest extends BaserTestCase
     }
 
     /**
-     * beforeRender
+     * test index
+     * [ADMIN] ブログ記事一覧表示
      */
-    public function testBeforeRender()
+    public function testIndex()
     {
         $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
 
     /**
-     * [ADMIN] 一覧表示
+     * test add
+     * [ADMIN] ブログ記事追加処理
      */
-    public function testAdmin_index()
+    public function testAdd()
     {
         $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
 
     /**
-     * [ADMIN] 登録処理
+     * test edit
+     * [ADMIN] ブログ記事編集処理
      */
-    public function testAdmin_add()
+    public function testEdit()
     {
         $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
 
     /**
-     * [ADMIN] 編集処理
+     * test delete
+     * [ADMIN] ブログ記事削除処理
      */
-    public function testAdmin_edit()
+    public function testDelete()
     {
         $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
 
     /**
-     * [ADMIN] 削除処理 (ajax)
+     * test unpublish
+     * [ADMIN] ブログ記事を非公開状態にする
      */
-    public function testAdmin_ajax_delete()
+    public function testUnpublish()
     {
         $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
 
     /**
-     * [ADMIN] 削除処理
+     * test publish
+     * [ADMIN] ブログ記事を公開状態にする
      */
-    public function testAdmin_delete()
+    public function testPublish()
     {
         $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
 
     /**
-     * [ADMIN] 無効状態にする（AJAX）
-     */
-    public function testAdmin_ajax_unpublish()
-    {
-        $this->markTestIncomplete('このテストは、まだ実装されていません。');
-    }
-
-    /**
-     * [ADMIN] 有効状態にする（AJAX）
-     */
-    public function testAdmin_ajax_publish()
-    {
-        $this->markTestIncomplete('このテストは、まだ実装されていません。');
-    }
-
-    /**
+     * test copy
      * [ADMIN] コピー
      */
-    public function testAdmin_ajax_copy()
+    public function testCopy()
     {
         $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
+
 }
