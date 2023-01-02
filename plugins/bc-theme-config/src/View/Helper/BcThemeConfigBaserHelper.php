d617fb869dcfe99efab5e6f4a4b2e675a01aa94d
ryuring <ryuring55@gmail.com>
2023-01-02 17:28:15 +0900

    アノテーションの use 漏れを追加

--- plugins/bc-theme-config/src/View/Helper/BcThemeConfigBaserHelper.php
+++ plugins/bc-theme-config/src/View/Helper/BcThemeConfigBaserHelper.php
@@ -12,6 +12,9 @@
 namespace BcThemeConfig\View\Helper;
 
 use Cake\View\Helper;
+use BaserCore\Annotation\UnitTest;
+use BaserCore\Annotation\NoTodo;
+use BaserCore\Annotation\Checked;
 
 /**
  * BcThemeConfigBaserHelper
