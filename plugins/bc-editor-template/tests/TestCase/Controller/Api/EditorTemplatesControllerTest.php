51f386226f6950d8f29c7298f9ace050c48adcd6
Đỗ Văn Hùng <HungDV2022>
2023-03-07 14:16:11 +0900

    エディターテンプレートAPI　必要なメソッドを生成

---
+++ plugins/bc-editor-template/tests/TestCase/Controller/Api/EditorTemplatesControllerTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BcEditorTemplate\Test\TestCase\Controller\Api;
+
+use BaserCore\Test\Scenario\InitAppScenario;
+use BaserCore\TestSuite\BcTestCase;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
+use Cake\TestSuite\IntegrationTestTrait;
+
+/**
+ * Class EditorTemplatesControllerTest
+ */
+class EditorTemplatesControllerTest extends BcTestCase
+{
+
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups',
+        'plugin.BcEditorTemplate.Factory/EditorTemplates',
+    ];
+
+    /**
+     * Access Token
+     * @var string
+     */
+    public $accessToken = null;
+
+    /**
+     * Refresh Token
+     * @var null
+     */
+    public $refreshToken = null;
+
+    /**
+     * set up
+     */
+    public function setUp(): void
+    {
+        $this->setFixtureTruncate();
+        parent::setUp();
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $token = $this->apiLoginAdmin(1);
+        $this->accessToken = $token['access_token'];
+        $this->refreshToken = $token['refresh_token'];
+    }
+
+    /**
+     * Tear Down
+     *
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * test index
+     */
+    public function test_index()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test view
+     */
+    public function test_view()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test add
+     */
+    public function test_add()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test edit
+     */
+    public function test_edit()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test delete
+     */
+    public function test_delete()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test batch
+     */
+    public function test_list()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+}
