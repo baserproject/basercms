6acf198f6da64990254c13dec4339e92e9f0dbf4
Đỗ Văn Hùng <HungDV2022>
2023-03-08 11:59:14 +0900

    カスタムコンテンツの必要なクラスとメソードを追加

--- plugins/bc-custom-content/src/Controller/Api/CustomContentsController.php
+++ plugins/bc-custom-content/src/Controller/Api/CustomContentsController.php
@@ -12,16 +12,37 @@
 namespace BcCustomContent\Controller\Api;
 
 use BaserCore\Controller\Api\BcApiController;
+use BcCustomContent\Service\CustomContentsServiceInterface;
+use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\ORM\Exception\PersistenceFailedException;
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
 use BaserCore\Annotation\Checked;
-use BcCustomContent\Service\CustomContentsServiceInterface;
 
 /**
  * CustomContentsController
  */
 class CustomContentsController extends BcApiController
 {
+    /**
+     * 一覧取得API
+     *
+     * @param CustomContentsServiceInterface $service
+     */
+    public function index(CustomContentsServiceInterface $service)
+    {
+        //todo 一覧取得API
+    }
+
+    /**
+     * 単一データAPI
+     *
+     * @param CustomContentsServiceInterface $service
+     */
+    public function view(CustomContentsServiceInterface $service)
+    {
+        //todo 単一データAPI
+    }
 
     /**
      * カスタムコンテンツの新規追加
@@ -53,4 +74,33 @@ class CustomContentsController extends BcApiController
         ]);
     }
 
+    /**
+     * 編集API
+     *
+     * @param CustomContentsServiceInterface $service
+     */
+    public function edit(CustomContentsServiceInterface $service)
+    {
+        //todo 編集API
+    }
+
+    /**
+     * 削除API
+     *
+     * @param CustomContentsServiceInterface $service
+     */
+    public function delete(CustomContentsServiceInterface $service)
+    {
+        //todo 削除API
+    }
+
+    /**
+     * リストAPI
+     *
+     * @param CustomContentsServiceInterface $service
+     */
+    public function list(CustomContentsServiceInterface $service)
+    {
+        //todo リストAPI
+    }
 }
--- plugins/bc-custom-content/src/Controller/Api/CustomTablesController.php
+++ plugins/bc-custom-content/src/Controller/Api/CustomTablesController.php
@@ -12,6 +12,9 @@
 namespace BcCustomContent\Controller\Api;
 
 use BaserCore\Controller\Api\BcApiController;
+use BcCustomContent\Service\CustomTablesServiceInterface;
+use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\ORM\Exception\PersistenceFailedException;
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
 use BaserCore\Annotation\Checked;
@@ -21,5 +24,63 @@ use BaserCore\Annotation\Checked;
  */
 class CustomTablesController extends BcApiController
 {
+    /**
+     * 一覧取得API
+     *
+     * @param CustomTablesServiceInterface $service
+     */
+    public function index(CustomTablesServiceInterface $service)
+    {
+        //todo 一覧取得API
+    }
 
+    /**
+     * 単一データAPI
+     *
+     * @param CustomTablesServiceInterface $service
+     */
+    public function view(CustomTablesServiceInterface $service)
+    {
+        //todo 単一データAPI
+    }
+
+    /**
+     * 新規追加API
+     *
+     * @param CustomTablesServiceInterface $service
+     */
+    public function add(CustomTablesServiceInterface $service)
+    {
+        //todo 新規追加API
+    }
+
+    /**
+     * 編集API
+     *
+     * @param CustomTablesServiceInterface $service
+     */
+    public function edit(CustomTablesServiceInterface $service)
+    {
+        //todo 編集API
+    }
+
+    /**
+     * 削除API
+     *
+     * @param CustomTablesServiceInterface $service
+     */
+    public function delete(CustomTablesServiceInterface $service)
+    {
+        //todo 削除API
+    }
+
+    /**
+     * リストAPI
+     *
+     * @param CustomTablesServiceInterface $service
+     */
+    public function list(CustomTablesServiceInterface $service)
+    {
+        //todo リストAPI
+    }
 }
