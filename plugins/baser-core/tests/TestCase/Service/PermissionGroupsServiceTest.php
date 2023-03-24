fc97d392cfe487f6c9e2f9d1c52deb48162dfe0f
nghiem-mb <127474186+nghiem-mb@users.noreply.github.com>
2023-03-24 07:01:24 +0700

    fix #2096 PermissionGroupsService::deleteByUserGroup のユニットテスト実装 #2096 (#2103)
    Co-authored-by: Nghiem <nguyennghiem1205@gmail.com>

--- plugins/baser-core/tests/TestCase/Service/PermissionGroupsServiceTest.php
+++ plugins/baser-core/tests/TestCase/Service/PermissionGroupsServiceTest.php
@@ -12,11 +12,14 @@
 namespace BaserCore\Test\TestCase\Service;
 
 use BaserCore\Service\PermissionGroupsService;
+use BaserCore\Service\PermissionsService;
 use BaserCore\Service\PermissionGroupsServiceInterface;
+use BaserCore\Service\PermissionsServiceInterface;
 use BaserCore\Test\Scenario\PermissionGroupsScenario;
 use BaserCore\TestSuite\BcTestCase;
 use BaserCore\Utility\BcContainerTrait;
 use BaserCore\Test\Factory\PermissionGroupFactory;
+use BaserCore\Test\Factory\PermissionFactory;
 use Cake\Datasource\Exception\RecordNotFoundException;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
@@ -24,6 +27,7 @@ use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
  * PermissionGroupsServiceTest
  *
  * @property PermissionGroupsService $PermissionGroups
+ * @property PermissionsService $Permissions
  */
 class PermissionGroupsServiceTest extends BcTestCase
 {
@@ -55,6 +59,7 @@ class PermissionGroupsServiceTest extends BcTestCase
         $this->setFixtureTruncate();
         parent::setUp();
         $this->PermissionGroups = $this->getService(PermissionGroupsServiceInterface::class);
+        $this->Permissions = $this->getService(PermissionsServiceInterface::class);
     }
 
     /**
@@ -119,5 +124,41 @@ class PermissionGroupsServiceTest extends BcTestCase
         $this->assertNotEmpty($data2);
         $this->expectException(RecordNotFoundException::class);
         $this->PermissionGroups->get(-1);
+    }
+
+
+    /**
+     * Test deleteByUserGroup
+     *
+     * @return void
+     */
+    public function testDeleteByUserGroup(): void
+    {
+        $this->loadFixtureScenario(PermissionGroupsScenario::class);
+        PermissionFactory::make(
+            [
+                'no' => 1,
+                'sort' => 1,
+                'permission_group_id' => 1,
+                'name' => 'nghiem',
+                'url' => 'abc',
+                'user_group_id' => 99
+            ]
+        )->persist();
+        PermissionFactory::make(
+            [
+                'no' => 2,
+                'sort' => 2,
+                'permission_group_id' => 1,
+                'name' => 'nghiem 2',
+                'url' => 'abc',
+                'user_group_id' => 99
+            ]
+        )->persist();
+        $data1 = $this->PermissionGroups->get(1, 99);
+        $this->assertCount(2, $data1->permissions);
+        $this->PermissionGroups->deleteByUserGroup(99);
+        $data2 = $this->PermissionGroups->get(1, 99);
+        $this->assertCount(0, $data2->permissions);
     }
 }
