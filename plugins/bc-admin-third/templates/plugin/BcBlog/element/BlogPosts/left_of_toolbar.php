cb9cbf1302b1e945a39ae8792a393f3b349f3ce3
ryuring <egashira@catchup.co.jp>
2023-04-04 08:57:44 +0900

    【ブログ】ツールバー「編集」リンクの隣に「新規記事追加」リンクを追加 fix #2030

---
+++ plugins/bc-admin-third/templates/plugin/BcBlog/element/BlogPosts/left_of_toolbar.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+/**
+ * @var \BaserCore\View\BcFrontAppView $this
+ */
+$content = $this->getRequest()->getAttribute('currentContent');
+if(!$content) return;
+?>
+
+
+<div class="bca-toolbar__tools-button bca-toolbar__tools-button-add">
+	<?php $this->BcBaser->link(__d('baser', '新規記事追加'), [
+		'plugin' => 'BcBlog',
+		'prefix' => 'Admin',
+		'controller' => 'BlogPosts',
+		'action' => 'add', $content->entity_id
+	], ['class' => 'tool-menu']); ?>
+</div>
