179b53fac32064273ec771cdfdf9a4d969b746d6
ryuring <egashira@catchup.co.jp>
2023-04-03 20:13:14 +0900

    4系改修の5系移行 fix #2017

--- plugins/baser-core/src/Utility/BcUtil.php
+++ plugins/baser-core/src/Utility/BcUtil.php
@@ -698,7 +698,10 @@ class BcUtil
         $value = @unserialize(base64_decode($value));
         // 下位互換のため、しばらくの間、失敗した場合の再変換を行う v.3.0.2
         if ($value === false) {
-            $value = unserialize($_value);
+			$value = @unserialize($_value);
+			if($value === false) {
+				return '';
+			}
         }
         return $value;
     }
