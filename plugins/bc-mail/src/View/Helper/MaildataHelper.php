f8810cfa07f2d0ef5f9fd71201ebbb68855960a0
ryuring <egashira@catchup.co.jp>
2023-04-03 20:27:52 +0900

    fix #2025 メールメッセージダウンロード時に各項目の先頭にスペースを入れない仕様とした

--- plugins/bc-mail/src/View/Helper/MaildataHelper.php
+++ plugins/bc-mail/src/View/Helper/MaildataHelper.php
@@ -11,6 +11,7 @@
 
 namespace BcMail\View\Helper;
 
+use BaserCore\Utility\BcUtil;
 use BaserCore\View\Helper\BcTextHelper;
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
@@ -42,16 +43,17 @@ class MaildataHelper extends BcTextHelper
         return $escape ? h($toDisplayString) : $toDisplayString;
     }
 
-    /**
-     * メール表示用のデータを出力する
-     *
-     * @param string $type コントロールタイプ
-     * @param mixed $value 変換前の値
-     * @param array|string $options コントロールソース
-     * @return string メール用データ
-     */
-    public function toDisplayString($type, $value)
+	/**
+	 * メール表示用のデータを出力する
+	 *
+	 * @param string $type コントロールタイプ
+	 * @param mixed $value 変換前の値
+	 * @param bool $prefixSpace
+	 * @return string メール用データ
+	 */
+    public function toDisplayString(string $type, $value, bool $prefixSpace = true)
     {
+    	$result = '';
         switch ($type) {
             case 'text':
             case 'radio':
@@ -59,7 +61,7 @@ class MaildataHelper extends BcTextHelper
             case 'email':
             case 'tel':
             case 'password':
-                return ' ' . $value;
+                $result = $value;
 
             case 'pref':
                 $prefs = $this->prefList();
@@ -67,77 +69,75 @@ class MaildataHelper extends BcTextHelper
                 foreach ($prefs as $pref) {
                     $options[$pref] = $pref;
                 }
-                if (isset($options[$value])) {
-                    return ' ' . $options[$value];
-                }
-                return '';
+				if (isset($options[$value])) $result = $options[$value];
+				break;
 
             case 'multi_check':
-                if (empty($value)) {
-                    return '';
-                }
-
-                if (!is_array($value)) {
-                    $value = explode("|", $value);
-                }
-
-                $out = '';
-                foreach ($value as $key => $data) {
-                    $out .= " ・" . $data . PHP_EOL;
-                }
-                return $out;
+				if ($value === '') break;
+				if (!is_array($value)) $value = explode("|", $value);
+				$result = $value;
+				break;
 
             case 'file':
-                if (empty($value)) {
-                    return '';
-                }
+				$prefixSpace = false;
+				if (empty($value)) break;
 
                 $mailContent = $this->_View->get('mailContent');
                 $aryFile = explode('/', $value);
                 $file = $aryFile[count($aryFile) - 1];
-                $ext = decodeContent(null, $file);
-                $link = array_merge(
-                    [
-                        'admin' => true,
-                        'controller' => 'mail_messages',
-                        'action' => 'attachment',
-                        $mailContent['MailContent']['id']
-                    ],
-                    $aryFile
-                );
+                $ext = BcUtil::decodeContent(null, $file);
+                $link = array_merge([
+					'admin' => true,
+					'controller' => 'mail_messages',
+					'action' => 'attachment',
+					$mailContent['MailContent']['id']
+				], $aryFile);
                 if (in_array($ext, ['gif', 'jpg', 'png'])) {
-                    return $this->BcBaser->getLink(
+                    $result = $this->BcBaser->getLink(
                         $this->BcBaser->getImg($link, ['width' => 400]),
                         $link,
                         ['target' => '_blank']
                     );
+				} else {
+					$result = $this->BcBaser->getLink($file, $link);
                 }
 
                 return $this->BcBaser->getLink($file, $link);
 
             case 'date_time_calender':
-                if (is_array($value)) {
-                    $value = $this->dateTime($value);
-                }
-                if ($value) {
-                    return ' ' . date(__d('baser_core', 'Y年 m月 d日'), strtotime($value));
-                }
-                return '';
+				if (is_array($value)) $value = $this->dateTime($value);
+				if ($value) $result = date(__d('baser', 'Y年 m月 d日'), strtotime($value));
+				break;
 
             case 'date_time_wareki':
-                if (!is_array($value)) {
-                    $value = $this->BcTime->convertToWarekiArray($value);
-                }
-                return ' ' . $this->dateTimeWareki($value);
+				if (!is_array($value)) $value = $this->BcTime->convertToWarekiArray($value);
+				$result = $this->dateTimeWareki($value);
+				break;
 
             case 'autozip':
-                if (strlen($value) == 7) {
-                    return substr($value, 0, 3) . '-' . substr($value, 3, 7);
-                }
-                return ' ' . $value;
+				if (strlen($value) == 7) {
+					$result = substr($value, 0, 3) . '-' . substr($value, 3, 7);
+				} else {
+					$result = $value;
+				}
+				break;
 
             default:
-                return $value;
+				$prefixSpace = false;
+				$result = $value;
         }
+
+		if(is_array($result)) {
+			$out = '';
+			foreach($result as $v) {
+				$v = "・" . $v . PHP_EOL;
+				if($prefixSpace) $v = ' ' . $v;
+				$out .= $v;
+			}
+			$result = $out;
+		} else {
+			if($prefixSpace && $result) $result = ' ' . $result;
+		}
+		return $result;
     }
 }
