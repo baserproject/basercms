a8ed5b64c0a0217f25891a8adf912fdf01febb18
加藤 朗 <kato.kaisya@gmail.com>
2023-04-04 18:06:17 +0900

    fix #1894 メールフォーム ラジオボタンがphp8.1~に対応していない問題を解決 (#2225)
    Co-authored-by: kato <kato@e-catchup.jp>

--- plugins/baser-core/src/View/Helper/BcFormHelper.php
+++ plugins/baser-core/src/View/Helper/BcFormHelper.php
@@ -1499,7 +1499,9 @@ SCRIPT_END;
             $div = $attributes['div'];
             unset($attributes['div']);
         }
-        unset($label['label']);
+        if (isset($label['label'])) {
+            unset($label['label']);
+        }
 
         $this->_domIdSuffixes = [];
         foreach($options as $optValue => $optTitle) {
