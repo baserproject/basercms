d640d3a22fcb3e6279e7b86dc4b10649f7a16e02
ryuring <egashira@catchup.co.jp>
2023-04-04 09:29:35 +0900

    コンテンツ管理にて新規追加したばかりのコンテンツの公開状態を変更すると Warning になってしまう問題を改善 fix #2041

--- plugins/baser-core/src/Model/Behavior/BcUploadBehavior.php
+++ plugins/baser-core/src/Model/Behavior/BcUploadBehavior.php
@@ -158,11 +158,11 @@ class BcUploadBehavior extends Behavior
      */
     public function afterSave(EventInterface $event, EntityInterface $entity)
     {
-        if ($entity->id) {
+        if ($entity->id && !empty($this->oldEntity[$this->table()->getAlias()])) {
             $this->BcFileUploader[$this->table()->getAlias()]->deleteExistingFiles($entity);
         }
         $this->BcFileUploader[$this->table()->getAlias()]->saveFiles($entity);
-        if ($entity->id) {
+        if ($entity->id && !empty($this->oldEntity[$this->table()->getAlias()])) {
             $this->BcFileUploader[$this->table()->getAlias()]->deleteFiles($this->oldEntity[$this->table()->getAlias()], $entity);
         }
         if ($this->BcFileUploader[$this->table()->getAlias()]->isUploaded()) {
