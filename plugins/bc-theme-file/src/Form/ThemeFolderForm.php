77e89cd8f2d70ebc0af2158aa36a7b50a2d74ca9
ryuring <egashira@catchup.co.jp>
2023-04-12 16:55:11 +0900

    テーマフォルダー新規作成のバリデーションを追加

--- plugins/bc-theme-file/src/Form/ThemeFolderForm.php
+++ plugins/bc-theme-file/src/Form/ThemeFolderForm.php
@@ -17,6 +17,7 @@ use Cake\Form\Schema;
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
 use BaserCore\Annotation\Checked;
+use Cake\Validation\Validator;
 
 /**
  * ThemeFolderForm
@@ -56,6 +57,50 @@ class ThemeFolderForm extends Form
             return $folder->move($newPath, ['from' => $data['fullpath'], 'chmod' => 0777, 'skip' => ['_notes']]);
         }
         return false;
+    }
+
+    /**
+     * Validation default
+     * @param Validator $validator
+     * @return Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->scalar('name')
+            ->requirePresence('name', 'create', __d('baser_core', 'テーマフォルダー名を入力してください。'))
+            ->notEmptyString('name', __d('baser_core', 'テーマフォルダー名を入力してください。'))
+            ->add('name', [
+                'duplicateThemeFolder' => [
+                    'provider' => 'form',
+                    'rule' => ['duplicateThemeFolder'],
+                    'message' => __d('baser_core', '入力されたテーマフォルダー名は、同一階層に既に存在します。')
+                ]])
+            ->add('name', [
+                'nameAlphaNumericPlus' => [
+                    'rule' => ['alphaNumericPlus'],
+                    'provider' => 'bc',
+                    'message' => __d('baser_core', 'テーマフォルダー名は半角英数字とハイフン、アンダースコアのみが利用可能です。')
+                ]]);
+        return $validator;
+    }
+
+    /**
+     * フォルダーの重複チェック
+     *
+     * @param array $check
+     * @return    boolean
+     */
+    public function duplicateThemeFolder($value, $context = null)
+    {
+        if (!$value) return true;
+        if($context['data']['mode'] !== 'create') return true;
+        $target = $context['data']['parent'] . $value;
+        if (is_dir($target)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
