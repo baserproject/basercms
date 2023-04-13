5d2725d719d0a4ec8c3931c72b9a176d37da8433
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2023-04-13 09:08:41 +0900

    【BcThemeConfig】更新ログを保存するようにする (#2264)
    Co-authored-by: Đỗ Văn Hùng <HungDV2022>

--- plugins/bc-theme-config/src/Controller/Api/Admin/ThemeConfigsController.php
+++ plugins/bc-theme-config/src/Controller/Api/Admin/ThemeConfigsController.php
@@ -58,6 +58,7 @@ class ThemeConfigsController extends BcAdminApiController
         try {
             $themeConfig = $service->update($this->request->getData());
             $message = __d('baser_core', 'テーマ設定を保存しました。');
+            $this->BcMessage->setSuccess($message, true, false);
         } catch (PersistenceFailedException $e) {
             $errors = $e->getEntity()->getErrors();
             $message = __d('baser_core', "入力エラーです。内容を修正してください。");
