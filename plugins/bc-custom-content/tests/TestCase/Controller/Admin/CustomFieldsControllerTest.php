56fc03f5db304ac1e5989423f992a5d6aa259383
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2023-04-18 17:42:03 +0900

    「カスタムフィールドの登録前・登録後・編集前・編集後」のイベントを追加 (#2051)
    Co-authored-by: Đỗ Văn Hùng <HungDV2022>

--- plugins/bc-custom-content/tests/TestCase/Controller/Admin/CustomFieldsControllerTest.php
+++ plugins/bc-custom-content/tests/TestCase/Controller/Admin/CustomFieldsControllerTest.php
@@ -11,13 +11,43 @@
 
 namespace BcCustomContent\Test\TestCase\Controller\Admin;
 
+use BaserCore\Test\Scenario\InitAppScenario;
 use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcContainerTrait;
+use BcCustomContent\Controller\Admin\CustomFieldsController;
+use BcCustomContent\Test\Factory\CustomFieldFactory;
+use BcCustomContent\Test\Scenario\CustomFieldsScenario;
+use Cake\Event\Event;
+use Cake\ORM\TableRegistry;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * CustomFieldsControllerTest
+ *  * @property CustomFieldsController $CustomFieldsController
  */
 class CustomFieldsControllerTest extends BcTestCase
 {
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use BcContainerTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups',
+        'plugin.BcCustomContent.Factory/CustomFields',
+        'plugin.BcCustomContent.Factory/CustomLinks',
+        'plugin.BcCustomContent.Factory/CustomTables',
+    ];
 
     /**
      * Set up
@@ -25,6 +55,11 @@ class CustomFieldsControllerTest extends BcTestCase
     public function setUp(): void
     {
         parent::setUp();
+        $this->setFixtureTruncate();
+        parent::setUp();
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $request = $this->getRequest('/baser/admin/bc-custom-content/custom_fields/');
+        $this->loginAdmin($request);
     }
 
     /**
@@ -34,5 +69,115 @@ class CustomFieldsControllerTest extends BcTestCase
     {
         parent::tearDown();
     }
+    /**
+     * Test beforeAddEvent
+     */
+    public function testBeforeAddEvent()
+    {
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        //イベントをコル
+        $this->entryEventToMock(self::EVENT_LAYER_CONTROLLER, 'BcCustomContent.CustomFields.beforeAdd', function (Event $event) {
+            $data = $event->getData('data');
+            $data['title'] = 'beforeAdd';
+            $event->setData('data', $data);
+        });
+        //Postデータを生成
+        $data = [
+            'title' => '求人分類',
+            'name' => 'recruit_category',
+            'type' => 'BcCcRelated',
+            'status' => 1,
+            'default_value' => '新卒採用',
+        ];
+        //対象URLをコル
+        $this->post('/baser/admin/bc-custom-content/custom_fields/add', $data);
+        //イベントに入るかどうか確認
+        $customFields = $this->getTableLocator()->get('BcCustomContent.CustomFields');
+        $query = $customFields->find()->where(['title' => 'beforeAdd']);
+        $this->assertEquals(1, $query->count());
+    }
 
+    /**
+     * Test beforeAddEvent
+     */
+    public function testAfterAddEvent()
+    {
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        //イベントをコル
+        $this->entryEventToMock(self::EVENT_LAYER_CONTROLLER, 'BcCustomContent.CustomFields.afterAdd', function (Event $event) {
+            $data = $event->getData('data');
+            $contentLinks = TableRegistry::getTableLocator()->get('BcCustomContent.CustomFields');
+            $data->title = 'afterAdd';
+            $contentLinks->save($data);
+        });
+        //Postデータを生成
+        $data = [
+            'title' => '求人分類',
+            'name' => 'recruit_category_2',
+            'type' => 'BcCcRelated',
+            'status' => 1,
+            'default_value' => '新卒採用',
+        ];
+        //対象URLをコル
+        $this->post('/baser/admin/bc-custom-content/custom_fields/add', $data);
+        //イベントに入るかどうか確認
+        $customFields = $this->getTableLocator()->get('BcCustomContent.CustomFields');
+        $query = $customFields->find()->where(['title' => 'afterAdd']);
+        $this->assertEquals(1, $query->count());
+    }
+
+    /**
+     * Test beforeAddEvent
+     */
+    public function testBeforeEditEvent()
+    {
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        //データーを生成
+        $this->loadFixtureScenario(CustomFieldsScenario::class);
+        //イベントをコル
+        $this->entryEventToMock(self::EVENT_LAYER_CONTROLLER, 'BcCustomContent.CustomFields.beforeEdit', function (Event $event) {
+            $data = $event->getData('data');
+            $data['title'] = 'beforeEdit';
+            $event->setData('data', $data);
+        });
+        //Postデータを生成
+        $data = CustomFieldFactory::get(1);
+        $data['title'] = 'test edit title';
+        //対象URLをコル
+        $this->post('/baser/admin/bc-custom-content/custom_fields/edit/1', $data);
+        //イベントに入るかどうか確認
+        $customFields = $this->getTableLocator()->get('BcCustomContent.CustomFields');
+        $query = $customFields->find()->where(['title' => 'beforeEdit']);
+        $this->assertEquals(1, $query->count());
+    }
+
+    /**
+     * Test beforeAddEvent
+     */
+    public function testAfterEditEvent()
+    {
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        //データーを生成
+        $this->loadFixtureScenario(CustomFieldsScenario::class);
+        //イベントをコル
+        $this->entryEventToMock(self::EVENT_LAYER_CONTROLLER, 'BcCustomContent.CustomFields.afterEdit', function (Event $event) {
+            $data = $event->getData('data');
+            $contentLinks = TableRegistry::getTableLocator()->get('BcCustomContent.CustomFields');
+            $data->title = 'afterEdit';
+            $contentLinks->save($data);
+        });
+        //Postデータを生成
+        $data = CustomFieldFactory::get(1);
+        $data['title'] = 'test edit title';
+        //対象URLをコル
+        $this->post('/baser/admin/bc-custom-content/custom_fields/edit/1', $data);
+        //イベントに入るかどうか確認
+        $customFields = $this->getTableLocator()->get('BcCustomContent.CustomFields');
+        $query = $customFields->find()->where(['title' => 'afterEdit']);
+        $this->assertEquals(1, $query->count());
+    }
 }
