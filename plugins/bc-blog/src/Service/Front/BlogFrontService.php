b66c5c38d5688ae812310093b882c424404ce174
HungDV <dovanhungk57@gmail.com>
2023-06-07 09:05:53 +0900

    fix #2275 BlogFrontService::getViewVarsForBlogCategoryArchivesWidget のユニットテスト実装

--- plugins/bc-blog/src/Service/Front/BlogFrontService.php
+++ plugins/bc-blog/src/Service/Front/BlogFrontService.php
@@ -531,6 +531,7 @@ class BlogFrontService implements BlogFrontServiceInterface
      * @return array
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function getViewVarsForBlogCategoryArchivesWidget(
         int $blogContentId,
