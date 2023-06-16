ea31b19848a3f3ab84e82b0cf1b565972b8c8035
HungDV <dovanhungk57@gmail.com>
2023-06-16 10:32:36 +0900

    fix #2377 Front/CustomContentFrontService::getCustomContent のユニットテスト実装

--- plugins/bc-custom-content/tests/TestCase/Service/Front/CustomContentsFrontServiceTest.php
+++ plugins/bc-custom-content/tests/TestCase/Service/Front/CustomContentsFrontServiceTest.php
@@ -12,10 +12,13 @@
 namespace BcCustomContent\Test\TestCase\Service\Front;
 
 
+use BaserCore\Service\BcDatabaseServiceInterface;
 use BaserCore\TestSuite\BcTestCase;
 use BaserCore\Utility\BcContainerTrait;
+use BcCustomContent\Service\CustomTablesServiceInterface;
 use BcCustomContent\Service\Front\CustomContentFrontService;
 use BcCustomContent\Service\Front\CustomContentFrontServiceInterface;
+use BcCustomContent\Test\Scenario\CustomContentsScenario;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
@@ -79,7 +82,31 @@ class CustomContentsFrontServiceTest extends BcTestCase
      */
     public function test_getCustomContent()
     {
-        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+        $dataBaseService = $this->getService(BcDatabaseServiceInterface::class);
+        $customTable = $this->getService(CustomTablesServiceInterface::class);
+
+        //カスタムテーブルとカスタムエントリテーブルを生成
+        $customTable->create([
+            'id' => 1,
+            'name' => 'recruit_categories',
+            'title' => '求人情報',
+            'type' => '1',
+            'display_field' => 'title',
+            'publish_begin' => '2021-10-01 00:00:00',
+            'publish_end' => '9999-11-30 23:59:59',
+            'has_child' => 0
+        ]);
+
+        //フィクチャーからデーターを生成
+        $this->loadFixtureScenario(CustomContentsScenario::class);
+
+        //対象メソッドをコール
+        $rs = $this->CustomContentFrontService->getCustomContent(1);
+        $this->assertEquals('サービステスト', $rs->description);
+        $this->assertEquals('サービスタイトル', $rs->content->title);
+
+        //不要なテーブルを削除
+        $dataBaseService->dropTable('custom_entry_1_recruit_categories');
     }
 
     /**
