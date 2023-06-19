3de6a73346f445785d7fc6b2f91565f0257627bc
HungDV <dovanhungk57@gmail.com>
2023-06-19 09:01:04 +0900

    fix #2380 Front/CustomContentFrontService::getViewVarsForView のユニットテスト実装

--- plugins/bc-custom-content/tests/TestCase/Service/Front/CustomContentsFrontServiceTest.php
+++ plugins/bc-custom-content/tests/TestCase/Service/Front/CustomContentsFrontServiceTest.php
@@ -67,6 +67,7 @@ class CustomContentsFrontServiceTest extends BcTestCase
      */
     public function setUp(): void
     {
+        $this->setFixtureTruncate();
         parent::setUp();
         $this->CustomContentFrontService = $this->getService(CustomContentFrontServiceInterface::class);
     }
@@ -200,7 +201,39 @@ class CustomContentsFrontServiceTest extends BcTestCase
      */
     public function test_getViewVarsForView()
     {
-        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+        //サービスクラス
+        $dataBaseService = $this->getService(BcDatabaseServiceInterface::class);
+        $customTable = $this->getService(CustomTablesServiceInterface::class);
+        $customContent = $this->getService(CustomContentsServiceInterface::class);
+
+        //カスタムテーブルとカスタムエントリテーブルを生成
+        $customTable->create([
+            'id' => 1,
+            'name' => 'recruit_categories',
+            'title' => '求人情報',
+            'type' => '1',
+            'display_field' => 'title',
+            'publish_begin' => '2021-10-01 00:00:00',
+            'publish_end' => '9999-11-30 23:59:59',
+            'has_child' => 0
+        ]);
+
+        //フィクチャーからデーターを生成
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $this->loadFixtureScenario(CustomContentsScenario::class);
+        $this->loadFixtureScenario(CustomEntriesScenario::class);
+        $this->loginAdmin($this->getRequest('/baser/admin/'));
+        //対象メソッドをコール
+        $rs = $this->CustomContentFrontService->getViewVarsForView($customContent->get(1), 1, true);
+
+        //戻る値を確認
+        $this->assertArrayHasKey('customContent', $rs);
+        $this->assertArrayHasKey('customEntry', $rs);
+        $this->assertArrayHasKey('currentWidgetAreaId', $rs);
+        $this->assertArrayHasKey('editLink', $rs);
+
+        //不要なテーブルを削除
+        $dataBaseService->dropTable('custom_entry_1_recruit_categories');
     }
 
     /**
