46283ec79c7b432b673c8c7338ce55f30309a6a8
Nghiem <nguyennghiem1205@gmail.com>
2023-06-19 13:07:12 +0700

    CustomEntriesServiceTest initial

--- plugins/bc-custom-content/tests/TestCase/Service/CustomEntriesServiceTest.php
+++ plugins/bc-custom-content/tests/TestCase/Service/CustomEntriesServiceTest.php
@@ -12,6 +12,8 @@
 namespace BcCustomContent\Test\TestCase\Service;
 
 use BaserCore\TestSuite\BcTestCase;
+use BcCustomContent\Model\Table\CustomEntriesTable;
+use BcCustomContent\Model\Table\CustomTablesTable;
 use BcCustomContent\Service\CustomEntriesService;
 use BaserCore\Test\Scenario\InitAppScenario;
 use BcCustomContent\Service\CustomEntriesServiceInterface;
@@ -23,6 +25,7 @@ use BaserCore\Service\BcDatabaseServiceInterface;
 /**
  * CustomEntriesServiceTest
  * @property CustomEntriesService $CustomEntriesService
+ * @property BcDatabaseService $BcDatabaseService
  */
 class CustomEntriesServiceTest extends BcTestCase
 {
@@ -53,6 +56,7 @@ class CustomEntriesServiceTest extends BcTestCase
     {
         parent::setUp();
         $this->CustomEntriesService = $this->getService(CustomEntriesServiceInterface::class);
+        $this->BcDatabaseService = $this->getService(BcDatabaseServiceInterface::class);
     }
 
     /**
@@ -68,7 +72,10 @@ class CustomEntriesServiceTest extends BcTestCase
      */
     public function test_construct()
     {
-
+        $this->CustomEntriesService->__construct();
+        $this->assertInstanceOf(CustomEntriesTable::class, $this->CustomEntriesService->CustomEntries);
+        $this->assertInstanceOf(CustomTablesTable::class, $this->CustomEntriesService->CustomTables);
+        $this->assertInstanceOf(BcDatabaseServiceInterface::class, $this->CustomEntriesService->BcDatabaseService);
     }
 
     /**
