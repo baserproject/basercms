73ed828b6555860a0883a98c682ed02482f677c0
Nghiem <nguyennghiem1205@gmail.com>
2023-06-19 12:53:15 +0700

    CustomEntriesServiceTest initial

--- plugins/bc-custom-content/tests/TestCase/Service/CustomEntriesServiceTest.php
+++ plugins/bc-custom-content/tests/TestCase/Service/CustomEntriesServiceTest.php
@@ -12,12 +12,39 @@
 namespace BcCustomContent\Test\TestCase\Service;
 
 use BaserCore\TestSuite\BcTestCase;
+use BcCustomContent\Service\CustomEntriesService;
+use BaserCore\Test\Scenario\InitAppScenario;
+use BcCustomContent\Service\CustomEntriesServiceInterface;
+use Cake\TestSuite\IntegrationTestTrait;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
+use BaserCore\Service\BcDatabaseService;
+use BaserCore\Service\BcDatabaseServiceInterface;
 
 /**
  * CustomEntriesServiceTest
+ * @property CustomEntriesService $CustomEntriesService
  */
 class CustomEntriesServiceTest extends BcTestCase
 {
+
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups'
+    ];
 
     /**
      * Set up
@@ -25,6 +52,7 @@ class CustomEntriesServiceTest extends BcTestCase
     public function setUp(): void
     {
         parent::setUp();
+        $this->CustomEntriesService = $this->getService(CustomEntriesServiceInterface::class);
     }
 
     /**
@@ -33,6 +61,222 @@ class CustomEntriesServiceTest extends BcTestCase
     public function tearDown(): void
     {
         parent::tearDown();
+    }
+
+    /**
+     * test __construct
+     */
+    public function test_construct()
+    {
+
+    }
+
+    /**
+     * test getNew
+     */
+    public function test_getNew()
+    {
+
+    }
+
+    /**
+     * test getFieldControlType
+     */
+    public function test_getFieldControlType()
+    {
+
+    }
+
+    /**
+     * test setup
+     */
+    public function test_setup()
+    {
+
+    }
+
+    /**
+     * test getIndex
+     */
+    public function test_getIndex()
+    {
+
+    }
+
+    /**
+     * test createIndexConditions
+     */
+    public function test_createIndexConditions()
+    {
+
+    }
+
+    /**
+     * test getList
+     */
+    public function test_getList()
+    {
+
+    }
+
+    /**
+     * test createOrder
+     */
+    public function test_createOrder()
+    {
+
+    }
+
+    /**
+     * test get
+     */
+    public function test_get()
+    {
+
+    }
+
+    /**
+     * test createSelect
+     */
+    public function test_createSelect()
+    {
+
+    }
+
+    /**
+     * test create
+     */
+    public function test_create()
+    {
+
+    }
+
+    /**
+     * test update
+     */
+    public function test_update()
+    {
+
+    }
+
+    /**
+     * test delete
+     */
+    public function test_delete()
+    {
+
+    }
+
+    /**
+     * test addField
+     */
+    public function test_addField()
+    {
+
+    }
+
+    /**
+     * test renameField
+     */
+    public function test_renameField()
+    {
+
+    }
+
+    /**
+     * test removeField
+     */
+    public function test_removeField()
+    {
+
+    }
+
+    /**
+     * test createTable
+     */
+    public function test_createTable()
+    {
+
+    }
+
+    /**
+     * test renameTable
+     */
+    public function test_renameTable()
+    {
+
+    }
+
+    /**
+     * test dropTable
+     */
+    public function test_dropTable()
+    {
+
+    }
+
+    /**
+     * test addFields
+     */
+    public function test_addFields()
+    {
+
+    }
+
+    /**
+     * test getControlSource
+     */
+    public function test_getControlSource()
+    {
+
+    }
+
+    /**
+     * test getParentTargetList
+     */
+    public function test_getParentTargetList()
+    {
+
+    }
+
+    /**
+     * test isAllowPublish
+     */
+    public function test_isAllowPublish()
+    {
+
+    }
+
+    /**
+     * test getUrl
+     */
+    public function test_getUrl()
+    {
+
+    }
+
+    /**
+     * test autoConvert
+     */
+    public function test_autoConvert()
+    {
+
+    }
+
+    /**
+     * test moveUp
+     */
+    public function test_moveUp()
+    {
+
+    }
+
+    /**
+     * test moveDown
+     */
+    public function test_moveDown()
+    {
+
     }
 
 }
