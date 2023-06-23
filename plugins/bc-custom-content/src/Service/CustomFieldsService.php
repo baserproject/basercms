4bf89a9592230740b31dec0a1c1f3a7a3bf48496
HungDV <dovanhungk57@gmail.com>
2023-06-23 11:25:24 +0900

    CustomFieldsService::getNew のユニットテストを実装

--- plugins/bc-custom-content/src/Service/CustomFieldsService.php
+++ plugins/bc-custom-content/src/Service/CustomFieldsService.php
@@ -51,6 +51,9 @@ class CustomFieldsService implements CustomFieldsServiceInterface
      * カスタムフィールドの初期値となるエンティティを取得する
      *
      * @return EntityInterface
+     * @checked
+     * @noTodo
+     * @unitTest
      */
     public function getNew()
     {
