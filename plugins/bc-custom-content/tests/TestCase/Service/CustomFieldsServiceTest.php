6a9c29ae65c2a58721fdae8edb087b1fc43ea0d6
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2023-06-23 11:19:23 +0900

    CustomFieldsService::__construct のユニットテストを実装 (#2485)
    Co-authored-by: HungDV <dovanhungk57@gmail.com>

--- plugins/bc-custom-content/tests/TestCase/Service/CustomFieldsServiceTest.php
+++ plugins/bc-custom-content/tests/TestCase/Service/CustomFieldsServiceTest.php
@@ -12,12 +12,38 @@
 namespace BcCustomContent\Test\TestCase\Service;
 
 use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcContainerTrait;
+use BcCustomContent\Service\CustomFieldsServiceInterface;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * CustomFieldsServiceTest
  */
 class CustomFieldsServiceTest extends BcTestCase
 {
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use BcContainerTrait;
+
+    /**
+     * Test subject
+     *
+     * @var $CustomFieldsService
+     */
+    public $CustomFieldsService;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BcCustomContent.Factory/CustomFields',
+        'plugin.BcCustomContent.Factory/CustomLinks',
+        'plugin.BcCustomContent.Factory/CustomTables',
+    ];
 
     /**
      * Set up
@@ -25,6 +51,7 @@ class CustomFieldsServiceTest extends BcTestCase
     public function setUp(): void
     {
         parent::setUp();
+        $this->CustomFieldsService = $this->getService(CustomFieldsServiceInterface::class);
     }
 
     /**
@@ -32,7 +59,88 @@ class CustomFieldsServiceTest extends BcTestCase
      */
     public function tearDown(): void
     {
+        unset($this->CustomFieldsService);
         parent::tearDown();
     }
 
+    /**
+     * test __construct
+     */
+    public function test__construct()
+    {
+        $this->assertTrue(isset($this->CustomFieldsService->CustomFields));
+        $this->assertTrue(isset($this->CustomFieldsService->CustomEntries));
+    }
+
+    /**
+     * test getNew
+     */
+    public function test_getNew()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test get
+     */
+    public function test_get()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test getIndex
+     */
+    public function test_getIndex()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test create
+     */
+    public function test_create()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test update
+     */
+    public function test_update()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test delete
+     */
+    public function test_delete()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test getList
+     */
+    public function test_getList()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test getFieldTypes
+     */
+    public function test_getFieldTypes()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test getControlSource
+     */
+    public function test_getControlSource()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
 }
