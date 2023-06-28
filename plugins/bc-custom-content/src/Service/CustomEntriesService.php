1bca4294444ed1d6dbded2c40c4be8a27b079ad0
nghiem-mb <nghiem@mediabridge.asia>
2023-06-28 10:18:29 +0900

    test-CustomEntriesService-moveUp

--- plugins/bc-custom-content/src/Service/CustomEntriesService.php
+++ plugins/bc-custom-content/src/Service/CustomEntriesService.php
@@ -718,6 +718,9 @@ class CustomEntriesService implements CustomEntriesServiceInterface
      *
      * @param int $id
      * @return mixed
+     * @checked
+     * @noTodo
+     * @unitTest
      */
     public function moveUp(int $id)
     {
--- plugins/bc-custom-content/tests/TestCase/Service/CustomEntriesServiceTest.php
+++ plugins/bc-custom-content/tests/TestCase/Service/CustomEntriesServiceTest.php
@@ -756,6 +756,26 @@ class CustomEntriesServiceTest extends BcTestCase
      */
     public function test_moveUp()
     {
+        //準備
+        $customTable = $this->getService(CustomTablesServiceInterface::class);
+        //カスタムテーブルとカスタムエントリテーブルを生成
+        $customTable->create([
+            'id' => 1,
+            'name' => 'recruit_categories',
+            'title' => '求人情報',
+            'type' => '1',
+            'display_field' => 'name',
+            'has_child' => 0
+        ]);
+        $this->CustomEntriesService->setup(1);
+        //フィクチャーからデーターを生成
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $this->loadFixtureScenario(CustomContentsScenario::class);
+        $this->loadFixtureScenario(CustomEntriesScenario::class);
+
+        //正常系実行
+        $result = $this->CustomEntriesService->moveUp(3);
+        $this->assertTrue($result);
 
     }
 
