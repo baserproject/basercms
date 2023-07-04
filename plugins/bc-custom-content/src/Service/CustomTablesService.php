84a1ce0232a556af39d24ed119c240701883d94d
HungDV <dovanhungk57@gmail.com>
2023-07-04 08:48:13 +0700

    CustomTablesService::get のユニットテストを実装

--- plugins/bc-custom-content/src/Service/CustomTablesService.php
+++ plugins/bc-custom-content/src/Service/CustomTablesService.php
@@ -63,6 +63,10 @@ class CustomTablesService implements CustomTablesServiceInterface
      *
      * @param int $id
      * @return EntityInterface
+     *
+     * @checked
+     * @noTodo
+     * @unitTest
      */
     public function get(int $id, $options = [])
     {
