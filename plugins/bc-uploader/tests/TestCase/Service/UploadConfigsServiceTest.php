55e218b13caa403f55d5f619d37f9f24e940ae4e
Nghiem <nguyennghiem1205@gmail.com>
2023-07-04 15:56:20 +0700

    fix comment

--- plugins/bc-uploader/tests/TestCase/Service/UploadConfigsServiceTest.php
+++ plugins/bc-uploader/tests/TestCase/Service/UploadConfigsServiceTest.php
@@ -16,6 +16,7 @@ use BcUploader\Model\Entity\UploaderConfig;
 use BcUploader\Model\Table\UploaderConfigsTable;
 use BcUploader\Service\UploaderConfigsService;
 use BcUploader\Service\UploaderConfigsServiceInterface;
+use BcUploader\Test\Factory\UploaderConfigFactory;
 
 /**
  * UploadConfigsServiceTest
@@ -56,9 +57,13 @@ class UploadConfigsServiceTest extends BcTestCase
      */
     public function test_get()
     {
+        //準備
+        //データを生成
+        UploaderConfigFactory::make(['name' => 'name_1', 'value' => 'value_1'])->persist();
         //正常系実行
         $result = $this->UploaderConfigsService->get();
         $this->assertInstanceOf(UploaderConfig::class, $result);
+        $this->assertEquals('value_1', $result->name_1);
 
     }
 
