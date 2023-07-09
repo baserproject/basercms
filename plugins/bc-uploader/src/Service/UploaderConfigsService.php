535fee43e0c4f9dd69092cf1123fc48f6d4d0d35
nghiem-mb <127474186+nghiem-mb@users.noreply.github.com>
2023-07-09 09:41:12 +0900

    test-UploaderConfigsService-update (#2525)
    Co-authored-by: Nghiem <nguyennghiem1205@gmail.com>

--- plugins/bc-uploader/src/Service/UploaderConfigsService.php
+++ plugins/bc-uploader/src/Service/UploaderConfigsService.php
@@ -81,6 +81,7 @@ class UploaderConfigsService implements UploaderConfigsServiceInterface
      * @return UploaderConfig|\Cake\Datasource\EntityInterface|false
      * @noTodo
      * @checked
+     * @unitTest
      */
     public function update(array $postData)
     {
