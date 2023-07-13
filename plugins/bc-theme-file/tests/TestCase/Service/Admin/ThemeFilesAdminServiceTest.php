b02cfb801d2aa32cb151c8c70d3b68c9ccfc3397
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2023-07-13 07:25:35 +0700

    ThemeFilesAdminService::construct のユニットテストを実装 (#2578)
    Co-authored-by: HungDV <dovanhungk57@gmail.com>

--- plugins/bc-theme-file/tests/TestCase/Service/Admin/ThemeFilesAdminServiceTest.php
+++ plugins/bc-theme-file/tests/TestCase/Service/Admin/ThemeFilesAdminServiceTest.php
@@ -12,6 +12,9 @@
 namespace BcThemeFile\Test\TestCase\Service\Admin;
 
 use BaserCore\TestSuite\BcTestCase;
+use BcThemeFile\Service\Admin\ThemeFilesAdminService;
+use BcThemeFile\Service\Admin\ThemeFilesAdminServiceInterface;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * ThemeFilesAdminServiceTest
@@ -20,11 +23,37 @@ class ThemeFilesAdminServiceTest extends BcTestCase
 {
 
     /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups',
+    ];
+
+    /**
+     * Test subject
+     *
+     * @var ThemeFilesAdminService
+     */
+    public $ThemeFilesAdminService;
+
+    /**
      * set up
      */
     public function setUp(): void
     {
         parent::setUp();
+        $this->ThemeFilesAdminService = $this->getService(ThemeFilesAdminServiceInterface::class);
     }
 
     /**
@@ -32,7 +61,47 @@ class ThemeFilesAdminServiceTest extends BcTestCase
      */
     public function tearDown(): void
     {
+        unset($this->ThemeFilesAdminService);
         parent::tearDown();
     }
 
+    /**
+     * test construct
+     */
+    public function test__construct()
+    {
+        $this->assertTrue(isset($this->ThemeFilesAdminService->ThemeFoldersService));
+    }
+
+    /**
+     * test getViewVarsForIndex
+     */
+    public function test_getViewVarsForIndex()
+    {
+        $this->markTestIncomplete('テストが未実装です');
+    }
+
+    /**
+     * test getViewVarsForAdd
+     */
+    public function test_getViewVarsForAdd()
+    {
+        $this->markTestIncomplete('テストが未実装です');
+    }
+
+    /**
+     * test getViewVarsForEdit
+     */
+    public function test_getViewVarsForEdit()
+    {
+        $this->markTestIncomplete('テストが未実装です');
+    }
+
+    /**
+     * test getViewVarsForView
+     */
+    public function test_getViewVarsForView()
+    {
+        $this->markTestIncomplete('テストが未実装です');
+    }
 }
