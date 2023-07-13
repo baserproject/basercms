66a41ce71d6dd20abc81465a406cd6b494eb0b68
nghiem-mb <127474186+nghiem-mb@users.noreply.github.com>
2023-07-13 09:22:17 +0900

    test-WidgetAreasService-update (#2574)
    Co-authored-by: Nghiem <nguyennghiem1205@gmail.com>

--- plugins/bc-widget-area/src/Service/WidgetAreasService.php
+++ plugins/bc-widget-area/src/Service/WidgetAreasService.php
@@ -101,6 +101,7 @@ class WidgetAreasService implements WidgetAreasServiceInterface
      * @return EntityInterface
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function update(EntityInterface $entity, array $postData)
     {
