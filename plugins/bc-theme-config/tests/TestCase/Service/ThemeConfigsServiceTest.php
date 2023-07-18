02a8a328a9350162431abd9d1e579f69603ae1d2
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2023-07-18 10:20:55 +0700

    ThemeConfigsService::construct のユニットテストを実装 (#2595)
    Co-authored-by: HungDV <dovanhungk57@gmail.com>

--- plugins/bc-theme-config/tests/TestCase/Service/ThemeConfigsServiceTest.php
+++ plugins/bc-theme-config/tests/TestCase/Service/ThemeConfigsServiceTest.php
@@ -12,12 +12,37 @@
 namespace BcThemeConfig\Test\TestCase\Service;
 
 use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcContainerTrait;
+use BcThemeConfig\Service\ThemeConfigsService;
+use BcThemeConfig\Service\ThemeConfigsServiceInterface;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * ThemeConfigsServiceTest
  */
 class ThemeConfigsServiceTest extends BcTestCase
 {
+    /**
+     * Trait
+     */
+    use ScenarioAwareTrait;
+    use BcContainerTrait;
+
+    /**
+     * Test subject
+     *
+     * @var ThemeConfigsService
+     */
+    public $ThemeConfigsService;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BcThemeConfig.Factory/ThemeConfigs',
+    ];
 
     /**
      * set up
@@ -25,6 +50,7 @@ class ThemeConfigsServiceTest extends BcTestCase
     public function setUp(): void
     {
         parent::setUp();
+        $this->ThemeConfigsService = $this->getService(ThemeConfigsServiceInterface::class);
     }
 
     /**
@@ -32,7 +58,65 @@ class ThemeConfigsServiceTest extends BcTestCase
      */
     public function tearDown(): void
     {
+        unset($this->ThemeConfigsService);
         parent::tearDown();
+    }
+
+    /**
+     * Test __construct
+     */
+    public function test__construct()
+    {
+        // テーブルがセットされている事を確認
+        $this->assertEquals('theme_configs', $this->ThemeConfigsService->ThemeConfigs->getTable());
+    }
+
+    /**
+     * test get
+     */
+    public function test_get()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test clearCache
+     */
+    public function test_clearCache()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test update
+     */
+    public function test_update()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test saveImage
+     */
+    public function test_saveImage()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test deleteImage
+     */
+    public function test_deleteImage()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+    }
+
+    /**
+     * test updateColorConfig
+     */
+    public function test_updateColorConfig()
+    {
+        $this->markTestIncomplete('このテストは、まだ実装されていません。');
     }
 
 }
