2830764e6ea4c2064c5ad89dbf0b98278e4d9d30
HungDV <dovanhungk57@gmail.com>
2023-07-31 08:41:34 +0700

    UploaderFilesAdminService::getViewVarsForIndex ユニットテストを実装

--- plugins/bc-uploader/tests/TestCase/Service/Admin/UploadFilesAdminServiceTest.php
+++ plugins/bc-uploader/tests/TestCase/Service/Admin/UploadFilesAdminServiceTest.php
@@ -12,6 +12,11 @@
 namespace BcUploader\Test\TestCase\Service\Admin;
 
 use BaserCore\TestSuite\BcTestCase;
+use BcUploader\Service\Admin\UploaderFilesAdminService;
+use BcUploader\Service\Admin\UploaderFilesAdminServiceInterface;
+use BcUploader\Test\Factory\UploaderConfigFactory;
+use BcUploader\Test\Factory\UploaderFileFactory;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * UploadFilesAdminServiceTest
@@ -20,11 +25,33 @@ class UploadFilesAdminServiceTest extends BcTestCase
 {
 
     /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+        'plugin.BcUploader.Factory/UploaderConfigs',
+    ];
+
+    /**
+     * Test subject
+     *
+     * @var UploaderFilesAdminService
+     */
+    public $UploaderFilesAdminService;
+
+    /**
      * set up
      */
     public function setUp(): void
     {
         parent::setUp();
+        $this->UploaderFilesAdminService = $this->getService(UploaderFilesAdminServiceInterface::class);
     }
 
     /**
@@ -32,7 +59,45 @@ class UploadFilesAdminServiceTest extends BcTestCase
      */
     public function tearDown(): void
     {
+        unset($this->UploaderFilesAdminService);
         parent::tearDown();
     }
 
+    /**
+     * test getViewVarsForIndex
+     */
+    public function test_getViewVarsForIndex()
+    {
+        UploaderConfigFactory::make(['name' => 'large_width', 'value' => 500])->persist();
+        $rs = $this->UploaderFilesAdminService->getViewVarsForIndex(1);
+        //戻る値を確認
+        $this->assertEquals($rs['listId'], 1);
+        $this->assertTrue($rs['isAjax']);
+        $this->assertArrayHasKey('installMessage', $rs);
+        $this->assertNotNull($rs['uploaderConfigs']);
+    }
+
+    /**
+     * test getViewVarsForAjaxList
+     */
+    public function test_getViewVarsForAjaxList()
+    {
+        $this->markTestIncomplete('テストが未実装です');
+    }
+
+    /**
+     * test checkInstall
+     */
+    public function test_checkInstall()
+    {
+        $this->markTestIncomplete('テストが未実装です');
+    }
+
+    /**
+     * test getViewVarsForAjaxImage
+     */
+    public function test_getViewVarsForAjaxImage()
+    {
+        $this->markTestIncomplete('テストが未実装です');
+    }
 }
