3f4ae70a7a9056b993b4bbf1661daf3227e2023f
nghiem-mb <127474186+nghiem-mb@users.noreply.github.com>
2023-07-31 16:18:05 +0900

    test-ContentsController-get_local_navi (#2616)

--- plugins/baser-core/src/Controller/Api/ContentsController.php
+++ plugins/baser-core/src/Controller/Api/ContentsController.php
@@ -241,6 +241,7 @@ class ContentsController extends BcApiController
      * @return void
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function get_local_navi(ContentsServiceInterface $service, int $id)
     {
