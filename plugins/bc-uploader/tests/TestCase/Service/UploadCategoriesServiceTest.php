b11ff6bf71deb6045b1919739af9434313308151
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2023-08-23 08:46:33 +0700

    UploaderCategoriesService::delete のユニットテスト

--- plugins/bc-uploader/tests/TestCase/Service/UploadCategoriesServiceTest.php
+++ plugins/bc-uploader/tests/TestCase/Service/UploadCategoriesServiceTest.php
@@ -120,7 +120,7 @@ class UploadCategoriesServiceTest extends BcTestCase
         $rs = $this->UploaderCategoriesService->delete(1);
         //戻る値を確認
         $this->assertTrue($rs);
-        //DBにデータが保存しないか確認すうること
+        //DBにデータが保存しないか確認すること
         $this->expectException(RecordNotFoundException::class);
         $this->expectExceptionMessage('Record not found in table "uploader_categories"');
         $this->UploaderCategoriesService->get(1);
