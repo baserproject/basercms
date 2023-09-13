f6053b6d39d9f43f11ef8e01dd6c8be19693f0d4
nghiem-mb <nghiem@mediabridge.asia>
2023-09-14 08:38:58 +0900

    CustomEntriesTableTest initial

--- plugins/bc-custom-content/tests/TestCase/Model/Table/CustomEntriesTableTest.php
+++ plugins/bc-custom-content/tests/TestCase/Model/Table/CustomEntriesTableTest.php
@@ -11,13 +11,23 @@
 
 namespace BcCustomContent\Test\TestCase\Model\Table;
 
+use BaserCore\Test\Scenario\InitAppScenario;
 use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcContainerTrait;
+use BcCustomContent\Model\Table\CustomEntriesTable;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * CustomEntriesTableTest
+ * @property CustomEntriesTable $CustomEntriesTable
  */
 class CustomEntriesTableTest extends BcTestCase
 {
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use BcContainerTrait;
 
     /**
      * Set up
@@ -25,6 +35,8 @@ class CustomEntriesTableTest extends BcTestCase
     public function setUp(): void
     {
         parent::setUp();
+        $this->CustomEntriesTable = $this->getTableLocator()->get('BcCustomContent.CustomEntriesTable');
+        $this->loadFixtureScenario(InitAppScenario::class);
     }
 
     /**
@@ -34,5 +46,342 @@ class CustomEntriesTableTest extends BcTestCase
     {
         parent::tearDown();
     }
+
+    /**
+     * test initialize
+     */
+    public function test_initialize()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test createSearchIndex
+     */
+    public function test_createSearchIndex()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test createSearchDetail
+     */
+    public function test_createSearchDetail()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setUp
+     */
+    public function test_setUp()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setUseTable
+     */
+    public function test_setUseTable()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test getTableName
+     */
+    public function test_getTableName()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setLinks
+     */
+    public function test_setLinks()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setupValidate
+     */
+    public function test_setupValidate()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateMaxFileSize
+     */
+    public function test_setValidateMaxFileSize()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateFileExt
+     */
+    public function test_setValidateFileExt()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateEmailConfirm
+     */
+    public function test_setValidateEmailConfirm()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateRegex
+     */
+    public function test_setValidateRegex()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateEmail
+     */
+    public function test_setValidateEmail()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateNumber
+     */
+    public function test_setValidateNumber()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateHankaku
+     */
+    public function test_setValidateHankaku()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateZenkakuKatakana
+     */
+    public function test_setValidateZenkakuKatakana()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateZenkakuHiragana
+     */
+    public function test_setValidateZenkakuHiragana()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test setValidateDatetime
+     */
+    public function test_setValidateDatetime()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test validationDefault
+     */
+    public function test_validationDefault()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test beforeMarshal
+     */
+    public function test_beforeMarshal()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test autoConvert
+     */
+    public function test_autoConvert()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test findAll
+     */
+    public function test_findAll()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test decodeRow
+     */
+    public function test_decodeRow()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
+    /**
+     * test isJson
+     */
+    public function test_isJson()
+    {
+        //€”õ
+
+        //³íŒnÀs
+
+        //ˆÙíŒnÀs
+
+
+    }
+
 
 }
