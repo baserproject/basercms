8602b32f5defb32ee98c5f7dbde88376d64ed5d2
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2023-09-14 08:58:32 +0700

    CustomContentsController::edit のユニットテスト (#2732)
    Co-authored-by: Đỗ Văn Hùng <dovanhungk57@gmail.com>

--- plugins/bc-custom-content/tests/TestCase/Controller/Admin/CustomContentsControllerTest.php
+++ plugins/bc-custom-content/tests/TestCase/Controller/Admin/CustomContentsControllerTest.php
@@ -11,11 +11,16 @@
 
 namespace BcCustomContent\Test\TestCase\Controller\Admin;
 
+use BaserCore\Service\BcDatabaseServiceInterface;
 use BaserCore\Test\Scenario\InitAppScenario;
 use BaserCore\TestSuite\BcTestCase;
 use BaserCore\Utility\BcContainerTrait;
 use BcCustomContent\Controller\Admin\CustomContentsController;
+use BcCustomContent\Service\CustomTablesServiceInterface;
+use BcCustomContent\Test\Scenario\CustomContentsScenario;
+use Cake\Event\Event;
 use Cake\Http\ServerRequest;
+use Cake\ORM\TableRegistry;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
@@ -77,7 +82,53 @@ class CustomContentsControllerTest extends BcTestCase
      */
     public function test_edit()
     {
-        $this->markTestIncomplete('テストが未実装です');
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        $dataBaseService = $this->getService(BcDatabaseServiceInterface::class);
+        $customTable = $this->getService(CustomTablesServiceInterface::class);
+        //カスタムテーブルとカスタムエントリテーブルを生成
+        $customTable->create([
+            'id' => 1,
+            'name' => 'recruit_categories',
+            'title' => '求人情報',
+            'type' => '1',
+            'display_field' => 'title',
+            'has_child' => 0
+        ]);
+        $this->loadFixtureScenario(CustomContentsScenario::class);
+        $data = [
+            'custom_table_id' => 1,
+            'list_count' => 1,
+            'content' => [
+                'title' => 'custom content change'
+            ]
+        ];
+        //対象URLをコル
+        $this->post('/baser/admin/bc-custom-content/custom_contents/edit/1', $data);
+        //戻る値を確認
+        $this->assertResponseCode(302);
+        $this->assertFlashMessage('カスタムコンテンツ「custom content change」を更新しました。');
+        $this->assertRedirect(['action' => 'edit/1']);
+
+        //エラーを発生するとき
+        $data = [
+            'custom_table_id' => 1,
+            'list_count' => 1,
+            'content' => [
+                'title' => null
+            ]
+        ];
+        $this->post('/baser/admin/bc-custom-content/custom_contents/edit/1', $data);
+        //ステータスを確認
+        $this->assertResponseCode(200);
+        //エラーを確認
+        $vars = $this->_controller->viewBuilder()->getVars();
+        $this->assertEquals(
+            'タイトルを入力してください。',
+            $vars['entity']->getErrors()['content']['title']['_empty']
+        );
+        //不要なテーブルを削除
+        $dataBaseService->dropTable('custom_entry_1_recruit_categories');
     }
 
     /**
@@ -85,7 +136,43 @@ class CustomContentsControllerTest extends BcTestCase
      */
     public function test_beforeEdit()
     {
-        $this->markTestIncomplete('テストが未実装です');
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        $dataBaseService = $this->getService(BcDatabaseServiceInterface::class);
+        $customTable = $this->getService(CustomTablesServiceInterface::class);
+        $customTable->create([
+            'id' => 1,
+            'name' => 'recruit_categories',
+            'title' => '求人情報',
+            'type' => '1',
+            'display_field' => 'title',
+            'has_child' => 0
+        ]);
+        $this->loadFixtureScenario(CustomContentsScenario::class);
+
+        //イベントをコル
+        $this->entryEventToMock(self::EVENT_LAYER_CONTROLLER, 'BcCustomContent.CustomContents.beforeEdit', function (Event $event) {
+            $data = $event->getData('data');
+            $data['description'] = 'beforeEdit';
+            $event->setData('data', $data);
+        });
+        $data = [
+            'custom_table_id' => 1,
+            'list_count' => 1,
+            'description' => 'description edit',
+            'content' => [
+                'title' => 'custom content change'
+            ]
+        ];
+        //対象URLをコル
+        $this->post('/baser/admin/bc-custom-content/custom_contents/edit/1', $data);
+        //イベントに入るかどうか確認
+        $customContents = $this->getTableLocator()->get('BcCustomContent.CustomContents');
+        $query = $customContents->find()->where(['description' => 'beforeEdit']);
+        $this->assertEquals(1, $query->count());
+
+        //不要なテーブルを削除
+        $dataBaseService->dropTable('custom_entry_1_recruit_categories');
     }
 
     /**
@@ -93,7 +180,45 @@ class CustomContentsControllerTest extends BcTestCase
      */
     public function test_afterEdit()
     {
-        $this->markTestIncomplete('テストが未実装です');
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        //データーを生成
+        $dataBaseService = $this->getService(BcDatabaseServiceInterface::class);
+        $customTable = $this->getService(CustomTablesServiceInterface::class);
+        //カスタムテーブルとカスタムエントリテーブルを生成
+        $customTable->create([
+            'id' => 1,
+            'name' => 'recruit_categories',
+            'title' => '求人情報',
+            'type' => '1',
+            'display_field' => 'title',
+            'has_child' => 0
+        ]);
+        $this->loadFixtureScenario(CustomContentsScenario::class);
+        //イベントをコル
+        $this->entryEventToMock(self::EVENT_LAYER_CONTROLLER, 'BcCustomContent.CustomContents.afterEdit', function (Event $event) {
+            $data = $event->getData('data');
+            $customContents = TableRegistry::getTableLocator()->get('BcCustomContent.CustomContents');
+            $data->description = 'afterEdit';
+            $customContents->save($data);
+        });
+        //Postデータを生成
+        $data = [
+            'custom_table_id' => 1,
+            'list_count' => 1,
+            'description' => 'description edit',
+            'content' => [
+                'title' => 'custom content change'
+            ]
+        ];
+        //対象URLをコル
+        $this->post('/baser/admin/bc-custom-content/custom_contents/edit/1', $data);
+        //イベントに入るかどうか確認
+        $customContents = $this->getTableLocator()->get('BcCustomContent.CustomContents');
+        $query = $customContents->find()->where(['description' => 'afterEdit']);
+        $this->assertEquals(1, $query->count());
+        //不要なテーブルを削除
+        $dataBaseService->dropTable('custom_entry_1_recruit_categories');
     }
 
     /**
