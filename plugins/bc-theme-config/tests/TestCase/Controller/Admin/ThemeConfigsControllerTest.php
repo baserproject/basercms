f2a3f4f6a6f790af2a2be8216547929a9b65712a
nghiem-mb <127474186+nghiem-mb@users.noreply.github.com>
2023-09-15 14:55:45 +0700

    Theme configs controller test (#2731)
    Co-authored-by: Nghiem <nguyennghiem1205@gmail.com>

--- plugins/bc-theme-config/tests/TestCase/Controller/Admin/ThemeConfigsControllerTest.php
+++ plugins/bc-theme-config/tests/TestCase/Controller/Admin/ThemeConfigsControllerTest.php
@@ -1,17 +1,20 @@
 <?php
-// TODO ucmitz  : コード確認要
-return;
 /**
  * baserCMS :  Based Website Development Project <https://basercms.net>
- * Copyright (c) baserCMS Users Community <https://basercms.net/community/>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
  *
- * @copyright       Copyright (c) baserCMS Users Community
- * @link            https://basercms.net baserCMS Project
- * @since           baserCMS v 4.0.9
- * @license         https://basercms.net/license/index.html
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
  */
 
-App::uses('ThemeConfigsController', 'Controller');
+namespace BcThemeConfig\Test\TestCase\Controller\Admin;
+use BaserCore\Test\Scenario\InitAppScenario;
+use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcContainerTrait;
+use BcThemeConfig\Controller\Admin\ThemeConfigsController;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * Class ThemeConfigsControllerTest
@@ -20,13 +23,18 @@ App::uses('ThemeConfigsController', 'Controller');
  */
 class ThemeConfigsControllerTest extends BcTestCase
 {
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use BcContainerTrait;
 
     /**
      * set up
      *
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
     }
@@ -36,7 +44,7 @@ class ThemeConfigsControllerTest extends BcTestCase
      *
      * @return void
      */
-    public function tearDown()
+    public function tearDown(): void
     {
         parent::tearDown();
     }
@@ -44,9 +52,27 @@ class ThemeConfigsControllerTest extends BcTestCase
     /**
      * [ADMIN] 設定編集
      */
-    public function testAdmin_form()
+    public function testIndex()
     {
-        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+        //準備
+        $this->enableSecurityToken();
+        $this->enableCsrfToken();
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $data = [
+            'name_add' => 'value_edit'
+        ];
+        $this->post("/baser/admin/bc-theme-config/theme_configs/index", $data);
+        //ステータスを確認
+        $this->assertResponseSuccess();
+        $var = $this->_controller->viewBuilder()->getVars();
+        $this->assertArrayHasKey('themeConfig', $var);
+        $this->assertEquals('value_edit', $var['themeConfig']->name_add);
+        $this->assertRedirect([
+            'plugin' => 'BcThemeConfig',
+            'prefix' => 'Admin',
+            'controller' => 'ThemeConfigsController',
+            'action' => 'index'
+        ]);
     }
 
 }
