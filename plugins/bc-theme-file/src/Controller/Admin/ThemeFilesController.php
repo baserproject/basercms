12b56af0c687ba9a4d7a003f1d008573ea2c915d
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2023-10-12 11:33:34 +0700

    admin_ThemeFilesController::edit ユニットテスt

--- plugins/bc-theme-file/src/Controller/Admin/ThemeFilesController.php
+++ plugins/bc-theme-file/src/Controller/Admin/ThemeFilesController.php
@@ -183,6 +183,7 @@ class ThemeFilesController extends BcAdminAppController
      * @return void
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function edit(ThemeFilesAdminServiceInterface $service)
     {
