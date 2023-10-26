897b887fd3ceaf54426921d953f3245b9dc22e89
ryuring <egashira@catchup.co.jp>
2023-10-26 09:27:03 +0900

    BcRedirectMainSiteMiddlewareが正常に動作していないため、一旦、利用停止

--- plugins/baser-core/src/Plugin.php
+++ plugins/baser-core/src/Plugin.php
@@ -27,7 +27,6 @@ use BaserCore\Event\BcModelEventDispatcher;
 use BaserCore\Event\BcViewEventDispatcher;
 use BaserCore\Middleware\BcAdminMiddleware;
 use BaserCore\Middleware\BcFrontMiddleware;
-use BaserCore\Middleware\BcRedirectMainSiteMiddleware;
 use BaserCore\Middleware\BcRedirectSubSiteMiddleware;
 use BaserCore\Middleware\BcRequestFilterMiddleware;
 use BaserCore\ServiceProvider\BcServiceProvider;
@@ -46,7 +45,6 @@ use Cake\Http\ServerRequestFactory;
 use Cake\I18n\I18n;
 use Cake\Log\Log;
 use Cake\ORM\TableRegistry;
-use Cake\Routing\Middleware\RoutingMiddleware;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Utility\Inflector;
@@ -282,7 +280,6 @@ class Plugin extends BcPlugin implements AuthenticationServiceProviderInterface
         $middlewareQueue
             ->prepend(new BcRequestFilterMiddleware())
             ->insertBefore(CsrfProtectionMiddleware::class, new AuthenticationMiddleware($this))
-            ->insertBefore(RoutingMiddleware::class, new BcRedirectMainSiteMiddleware())
             ->add(new BcAdminMiddleware())
             ->add(new BcFrontMiddleware())
             ->add(new BcRedirectSubSiteMiddleware());
