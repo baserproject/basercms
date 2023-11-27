2bb7d7aa40d212c1d84f6b00591b059807b414f2
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2023-11-27 16:59:37 +0700

    ThemeFileForm::_execute()　のユニットテスト

--- plugins/bc-theme-file/tests/TestCase/Form/ThemeFileFormTest.php
+++ plugins/bc-theme-file/tests/TestCase/Form/ThemeFileFormTest.php
@@ -70,7 +70,57 @@ class ThemeFileFormTest extends BcTestCase
      */
     public function test_execute()
     {
-        $this->markTestIncomplete('このテストは未実装です。');
+        $fullpath = '/var/www/html/plugins/bc-column/templates/layout/';
+        //新規追加テスト
+        $data = [
+            'mode' => 'create',
+            'parent' => $fullpath,
+            'base_name' => 'test_add',
+            'ext' => 'php',
+            'contents' => 'test',
+            'fullpath' => $fullpath
+        ];
+        $rs = $this->execPrivateMethod($this->ThemeFileForm, '_execute', [$data]);
+        //戻る値を確認
+        $this->assertTrue($rs);
+        //実際にファイルが作成されいてるか確認すること
+        $this->assertTrue(file_exists($fullpath . 'test_add.php'));
+        //ファイルの中身を確認
+        $this->assertEquals('test', file_get_contents($fullpath . 'test_add.php'));
+
+        //編集テスト
+        $data = [
+            'mode' => 'update',
+            'parent' => $fullpath,
+            'base_name' => 'test_update',
+            'ext' => 'php',
+            'contents' => 'test update',
+            'fullpath' => $fullpath . 'test_add.php',
+        ];
+        $rs = $this->execPrivateMethod($this->ThemeFileForm, '_execute', [$data]);
+        //戻る値を確認
+        $this->assertTrue($rs);
+        //実際にファイルが変更されいてるか確認すること
+        $this->assertFalse(file_exists($fullpath . 'test_add.php'));
+        $this->assertTrue(file_exists($fullpath . 'test_update.php'));
+        //ファイルの中身を確認
+        $this->assertEquals('test update', file_get_contents($fullpath . 'test_update.php'));
+
+        //作成されたファイルを削除
+        unlink($fullpath . 'test_update.php');
+
+        //modeは['create', 'update']ではない場合、
+        $data = [
+            'mode' => 'new',
+            'parent' => $fullpath,
+            'base_name' => 'test_update',
+            'ext' => 'php',
+            'contents' => 'test update',
+            'fullpath' => $fullpath . 'test_add.php',
+        ];
+        $rs = $this->execPrivateMethod($this->ThemeFileForm, '_execute', [$data]);
+        //戻る値を確認
+        $this->assertFalse($rs);
     }
 
     /**
