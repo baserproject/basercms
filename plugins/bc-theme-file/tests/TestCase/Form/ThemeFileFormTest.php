fcaa262549b8e6e71c0256052c441e8194229945
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2023-11-28 21:19:49 +0700

    ThemeFileForm::validationDefault() のユニットテスト

--- plugins/bc-theme-file/tests/TestCase/Form/ThemeFileFormTest.php
+++ plugins/bc-theme-file/tests/TestCase/Form/ThemeFileFormTest.php
@@ -12,6 +12,7 @@
 namespace BcThemeFile\Test\TestCase\Form;
 
 use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcFile;
 use BcThemeFile\Form\ThemeFileForm;
 use Cake\Form\Schema;
 
@@ -78,7 +79,36 @@ class ThemeFileFormTest extends BcTestCase
      */
     public function test_validationDefault()
     {
-        $this->markTestIncomplete('このテストは未実装です。');
+        $validator = $this->ThemeFileForm->getValidator('default');
+        //base_nameを入力しない場合、
+        $errors = $validator->validate([
+            'base_name' => ''
+        ]);
+        $this->assertEquals('テーマファイル名を入力してください。', current($errors['base_name']));
+        $errors = $validator->validate([
+        ]);
+        $this->assertEquals('テーマファイル名を入力してください。', current($errors['base_name']));
+
+        //無効な文字を入力した場合、
+        $fullpath = BASER_PLUGINS . 'BcThemeSample' . '/templates/layout/';
+        $postData = [
+            'mode' => 'create',
+            'fullpath' => $fullpath,
+            'parent' => $fullpath,
+            'base_name' => 't e s t',
+            'ext' => 'php',
+            'contents' => "<?php echo 'test' ?>"
+        ];
+        $errors = $validator->validate($postData);
+        $this->assertEquals('テーマファイル名は半角英数字とハイフン、アンダースコアのみが利用可能です。', current($errors['base_name']));
+
+        //既にファイルが存在した場合、
+        $file = new BcFile($fullpath . 'test.php');
+        $file->create();
+        $postData['base_name'] = 'test';
+        $errors = $validator->validate($postData);
+        $this->assertEquals('入力されたテーマファイル名は、同一階層に既に存在します。', current($errors['base_name']));
+        unlink($fullpath . 'test.php');
     }
 
     /**
