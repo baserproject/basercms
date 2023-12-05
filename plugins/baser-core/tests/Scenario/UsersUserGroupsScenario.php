b46692fd5cba375dac7bf92d3fd0349f70ff10da
nghiem-mb <nghiem@mediabridge.asia>
2023-12-05 11:48:46 +0700

    PluginsControllerTest

---
+++ plugins/baser-core/tests/Scenario/UsersUserGroupsScenario.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\Scenario;
+
+use BaserCore\Test\Factory\DblogFactory;
+use BaserCore\Test\Factory\PasswordRequestFactory;
+use BaserCore\Test\Factory\SiteFactory;
+use BaserCore\Test\Factory\UserFactory;
+use BaserCore\Test\Factory\UserGroupFactory;
+use BaserCore\Test\Factory\UsersUserGroupFactory;
+use CakephpFixtureFactories\Scenario\FixtureScenarioInterface;
+
+/**
+ * UsersUserGroupsScenario
+ *
+ */
+class UsersUserGroupsScenario implements FixtureScenarioInterface
+{
+
+    /**
+     * load
+     */
+    public function load(...$args)
+    {
+        UsersUserGroupFactory::make(
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'user_group_id' => 1,
+                'created' => '2017-05-03 10:57:07',
+                'modified' => '2017-05-03 10:57:07'
+            ]
+        )->persist();
+        UsersUserGroupFactory::make(
+        [
+            'id' => 2,
+            'user_id' => 2,
+            'user_group_id' => 2,
+            'created' => '2017-05-03 10:57:07',
+            'modified' => '2017-05-03 10:57:07'
+        ])->persist();
+        UsersUserGroupFactory::make(
+        [
+            'id' => 3,
+            'user_id' => 3,
+            'user_group_id' => 3,
+            'created' => '2017-05-03 10:57:07',
+            'modified' => '2017-05-03 10:57:07'
+        ])->persist();
+    }
+
+}
