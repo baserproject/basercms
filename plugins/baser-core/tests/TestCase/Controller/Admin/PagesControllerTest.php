55316d92125e9ba77e17b39881492ceb2aa6956b
nghiem-mb <nghiem@mediabridge.asia>
2023-12-05 09:04:13 +0700

    PagesControllerTest

--- plugins/baser-core/tests/TestCase/Controller/Admin/PagesControllerTest.php
+++ plugins/baser-core/tests/TestCase/Controller/Admin/PagesControllerTest.php
@@ -11,10 +11,18 @@
 
 namespace BaserCore\Test\TestCase\Controller\Admin;
 
+use BaserCore\Test\Scenario\ContentFoldersScenario;
+use BaserCore\Test\Scenario\ContentsScenario;
+use BaserCore\Test\Scenario\InitAppScenario;
+use BaserCore\Test\Scenario\PagesScenario;
+use BaserCore\Test\Scenario\PluginsScenario;
+use BaserCore\Test\Scenario\SiteConfigsScenario;
 use Cake\Event\Event;
 use BaserCore\Service\PagesService;
 use BaserCore\TestSuite\BcTestCase;
 use BaserCore\Controller\Admin\PagesController;
+use Cake\TestSuite\IntegrationTestTrait;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * Class PagesControllerTest
@@ -23,23 +31,10 @@ use BaserCore\Controller\Admin\PagesController;
  */
 class PagesControllerTest extends BcTestCase
 {
-
     /**
-     * Fixtures
-     *
-     * @var array
+     * Trait
      */
-    public $fixtures = [
-        'plugin.BaserCore.Sites',
-        'plugin.BaserCore.SiteConfigs',
-        'plugin.BaserCore.Contents',
-        'plugin.BaserCore.Pages',
-        'plugin.BaserCore.ContentFolders',
-        'plugin.BaserCore.Plugins',
-        'plugin.BaserCore.Users',
-        'plugin.BaserCore.UserGroups',
-        'plugin.BaserCore.UsersUserGroups',
-    ];
+    use ScenarioAwareTrait;
 
     /**
      * set up
@@ -49,6 +44,12 @@ class PagesControllerTest extends BcTestCase
     public function setUp(): void
     {
         parent::setUp();
+        $this->loadFixtureScenario(PluginsScenario::class);
+        $this->loadFixtureScenario(SiteConfigsScenario::class);
+        $this->loadFixtureScenario(ContentFoldersScenario::class);
+        $this->loadFixtureScenario(ContentsScenario::class);
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $this->loadFixtureScenario(PagesScenario::class);
         $this->PagesController = new PagesController($this->getRequest());
         $this->PagesService = new PagesService();
     }
