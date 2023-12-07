a4b07c85505a82deb5eca2cc46d22f3f38c9c8d6
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2023-12-07 19:52:38 +0700

    BcCore/TestSuite FixtureManager を FixtureFactory に変更する #2883

--- plugins/baser-core/tests/Factory/DblogFactory.php
+++ plugins/baser-core/tests/Factory/DblogFactory.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace BaserCore\Test\Factory;
 
+use Cake\I18n\FrozenTime;
 use CakephpFixtureFactories\Factory\BaseFactory as CakephpBaseFactory;
 use Faker\Generator;
 
@@ -31,7 +32,12 @@ class DblogFactory extends CakephpBaseFactory
     protected function setDefaultTemplate(): void
     {
         $this->setDefaultData(function (Generator $faker) {
-            return [];
+            return [
+                'message' => $faker->text(50),
+                'user_id' => $faker->randomNumber(1, 100),
+                'created' => FrozenTime::now(),
+                'modified' => FrozenTime::now()
+            ];
         });
     }
 
