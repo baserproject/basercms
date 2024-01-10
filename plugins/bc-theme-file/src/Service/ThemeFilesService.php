6e8fcaa6fda21de8794f4aa00d91173b4750270f
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2024-01-10 20:26:33 +0700

    ユニットテスト調整

--- plugins/bc-theme-file/src/Service/ThemeFilesService.php
+++ plugins/bc-theme-file/src/Service/ThemeFilesService.php
@@ -207,14 +207,8 @@ class ThemeFilesService extends BcThemeFileService implements ThemeFilesServiceI
         }
         $Folder = new BcFolder($fullpath);
         $Folder->create();
-
-        try {
-            $name = $postData['file']->getClientFilename();
-            $postData['file']->moveTo($fullpath . DS . $name);
-        }catch (\Exception $e){
-            throw new BcException(__d('baser_core', 'ファイルの変更権限がありません。' ));
-        }
-
+        $name = $postData['file']->getClientFilename();
+        $postData['file']->moveTo($fullpath . DS . $name);
     }
 
     /**
