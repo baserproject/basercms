85cc7c716646dd20cc24bcb0695bc7150580ed4b
ryuring <egashira@catchup.co.jp>
2024-01-29 20:21:04 +0900

    TRUST_PROXYを利用している際に、ブラウザでアクセスすると HttpsEnforcerMiddleware で失敗してアクセスできない問題を改善

--- plugins/baser-core/src/Middleware/BcRequestFilterMiddleware.php
+++ plugins/baser-core/src/Middleware/BcRequestFilterMiddleware.php
@@ -69,7 +69,6 @@ class BcRequestFilterMiddleware implements MiddlewareInterface
          * リーバースプロキシを利用している場合、HTTPS ではなく HTTP_X_FORWARDED_SSL が true になるため
          * そちらの値で判定するように調整
          */
-         $a  = filter_var(env('TRUST_PROXY', false));
         if(filter_var(env('TRUST_PROXY', false), FILTER_VALIDATE_BOOLEAN)) {
             $request->trustProxy = true;
             $request->addDetector('https', ['env' => 'HTTP_X_FORWARDED_SSL', 'options' => [1, 'on']]);
