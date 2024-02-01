bf3a5acd92a2fa729810090a3ee5e192be5d257c
ryuring <egashira@catchup.co.jp>
2024-02-01 10:19:15 +0900

    ユニットテストが失敗していたため調整

--- plugins/baser-core/src/Controller/Admin/UsersController.php
+++ plugins/baser-core/src/Controller/Admin/UsersController.php
@@ -18,7 +18,6 @@ use BaserCore\Service\UsersServiceInterface;
 use BaserCore\Utility\BcSiteConfig;
 use BaserCore\Utility\BcUtil;
 use Cake\Core\Configure;
-use Cake\Core\Exception\Exception;
 use Cake\Datasource\Exception\RecordNotFoundException;
 use Cake\Http\Exception\ForbiddenException;
 use Cake\Http\Exception\NotFoundException;
@@ -48,7 +47,7 @@ class UsersController extends BcAdminAppController
     public function initialize(): void
     {
         parent::initialize();
-        if(isset($this->Authentication)) {
+        if($this->components()->has('Authentication')) {
             $this->Authentication->allowUnauthenticated(['login']);
         }
     }
