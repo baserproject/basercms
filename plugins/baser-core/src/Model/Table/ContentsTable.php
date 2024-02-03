ad9750a056095f9f6abd898a46fa1ba4f9d0f9e2
ryuring <egashira@catchup.co.jp>
2024-02-03 20:22:09 +0900

    コードフォーマット

--- plugins/baser-core/src/Model/Table/ContentsTable.php
+++ plugins/baser-core/src/Model/Table/ContentsTable.php
@@ -151,7 +151,7 @@ class ContentsTable extends AppTable
         $validator
             ->integer('parent_id')
             ->requirePresence('parent_id', 'create', __d('baser_core', 'content[parent_id] フィールドが存在しません。'))
-            ->allowEmptyString('parent_id', __d('baser_core', '親フォルダを入力してください。'), function(array $context){
+            ->allowEmptyString('parent_id', __d('baser_core', '親フォルダを入力してください。'), function(array $context) {
                 return (isset($context['data']['id']) && $context['data']['id'] === 1);
             });
 
