6e2b718b6e20301b8313ff44c21e86f9d532ee7a
thangnn <thang.nguyen@mediabrige.vn>
2024-02-06 01:01:22 +0700

    [basercms] unitTest_BlogPostsTable_beforeSave

--- plugins/bc-blog/tests/TestCase/Model/BlogPostsTableTest.php
+++ plugins/bc-blog/tests/TestCase/Model/BlogPostsTableTest.php
@@ -750,4 +750,24 @@ class BlogPostsTableTest extends BcTestCase
         $rs = $this->BlogPostsTable->getPublishByNo(6, 3);
         $this->assertNull($rs);
     }
+    /**
+     * beforeSave
+     * @return void
+     */
+    public function test_beforeSave(){
+        $PluginsService = $this->getService(PluginsServiceInterface::class);
+        $BlogPostsService = $this->getService(BlogPostsServiceInterface::class);
+        $PluginsService->attach('BcSearchIndex');
+
+        $this->loadFixtureScenario(MultiSiteBlogPostScenario::class);
+
+        $blogPost = $BlogPostsService->get(1);
+        $this->BlogPostsTable->beforeSave(new Event("beforeSave"), $blogPost, new ArrayObject());
+        $this->assertFalse($this->BlogPostsTable->isExcluded());
+
+        $blogPost = $BlogPostsService->get(2);
+        $this->BlogPostsTable->beforeSave(new Event("beforeSave"), $blogPost, new ArrayObject());
+        $this->assertFalse($this->BlogPostsTable->isExcluded());
+
+    }
 }
