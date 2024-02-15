db33aeb2c75247e8dbd810dd181c4d9a80062e62
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2024-02-15 09:34:55 +0900

    ラッパーメソッドのためユニットテストはスキップする

--- plugins/bc-blog/src/View/Helper/BlogHelper.php
+++ plugins/bc-blog/src/View/Helper/BlogHelper.php
@@ -193,6 +193,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function currentBlogId()
     {
@@ -218,6 +219,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function blogName()
     {
@@ -243,6 +245,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function title()
     {
@@ -281,6 +284,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function description()
     {
@@ -312,6 +316,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function postTitle(BlogPost $post, bool $link = true, array $options = []): void
     {
@@ -431,6 +436,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function postLink($post, $title, $options = [])
     {
@@ -450,6 +456,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function postContent(
         BlogPost $post,
@@ -518,6 +525,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function postDetail(BlogPost $post, array $options = [])
     {
@@ -560,6 +568,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function category(BlogPost $post, array $options = [])
     {
@@ -613,6 +622,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function tag($post, $separator = ' , ')
     {
@@ -737,6 +747,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function postDate(BlogPost $post, string $format = 'Y/m/d')
     {
@@ -768,6 +779,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function author(BlogPost $post)
     {
@@ -817,6 +829,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function prevLink(BlogPost $post, string $title = '', array $htmlAttributes = [])
     {
@@ -860,6 +873,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function nextLink(BlogPost $post, string $title = '', array $htmlAttributes = [])
     {
@@ -959,6 +973,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function postImg($post, $options = [])
     {
@@ -1227,6 +1242,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function eyeCatch(BlogPost $post, array $options = [])
     {
@@ -1573,6 +1589,7 @@ class BlogHelper extends Helper
      * @return void
      * @checked
      * @noTodo
+     * @unitTest ラッパーメソッドのためユニットテストはスキップする
      */
     public function tagLink($blogContentId, $tag, $options = [])
     {
