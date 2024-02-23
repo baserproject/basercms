3cdf03ac067f4baa349836abced9cfbd8628fa9c
ryuring <egashira@catchup.co.jp>
2024-02-23 11:48:23 +0900

    baserCMS４系よりWidgetAreasのデータを移行した場合に、各ウィジェットにプラグインが設定されていないため、コアのテーマを強制的に設定

--- plugins/bc-admin-third/templates/plugin/BcWidgetArea/Admin/WidgetAreas/edit.php
+++ plugins/bc-admin-third/templates/plugin/BcWidgetArea/Admin/WidgetAreas/edit.php
@@ -169,6 +169,12 @@ $this->BcAdmin->addAdminMainBodyHeaderLinks([
             if ($widget[$key]['status']) {
               $enabled = ' enabled';
             }
+
+            // baserCMS４系よりデータを移行した場合に、プラグインが設定されていないため、コアのテーマを設定
+            if(empty($widget[$key]['plugin'])) {
+              $widget[$key]['plugin'] = \Cake\Utility\Inflector::camelize(Cake\Core\Configure::read('BcApp.coreAdminTheme'), '-');
+            }
+
             $this->setRequest($this->getRequest()->withParsedBody(array_merge(
               $this->getRequest()->getData(),
               [$key => $widget[$key]]
