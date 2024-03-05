49c83f91fe7ea49642d9caa2203f6c38719f2bed
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2024-03-05 09:17:05 +0900

    fix #3172 新規フィールド追加にてテキスト関連設定に数字以外の文字を入力しても保存できてしまう

--- plugins/bc-custom-content/src/Model/Table/CustomFieldsTable.php
+++ plugins/bc-custom-content/src/Model/Table/CustomFieldsTable.php
@@ -78,6 +78,11 @@ class CustomFieldsTable extends AppTable
         $validator
             ->allowEmptyString('size')
             ->integer('size', __d('baser_core', '横幅サイズは整数を入力してください。'));
+
+        $validator
+            ->allowEmptyString('line')
+            ->integer('line', __d('baser_core', '行数は整数を入力してください。'));
+
         $validator
             ->allowEmptyString('max_length')
             ->integer('max_length', __d('baser_core', '最大文字数は整数を入力してください。'));
