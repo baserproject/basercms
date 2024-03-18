4df7343e6feb180fae736c0309ddb806c635f40c
thangnn <thang.nguyen@mediabrige.vn>
2024-03-18 13:05:05 +0700

    modifed by review commnet #issuecomment-2002747800

--- plugins/bc-custom-content/tests/TestCase/View/Helper/CustomContentAdminHelperTest.php
+++ plugins/bc-custom-content/tests/TestCase/View/Helper/CustomContentAdminHelperTest.php
@@ -3,8 +3,7 @@
 namespace BcCustomContent\Test\TestCase\View\Helper;
 
 use BaserCore\TestSuite\BcTestCase;
-use BcCustomContent\Model\Entity\CustomField;
-use BcCustomContent\Model\Entity\CustomLink;
+use BcCustomContent\Test\Factory\CustomLinkFactory;
 use BcCustomContent\View\Helper\CustomContentAdminHelper;
 use Cake\View\View;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
@@ -30,76 +29,67 @@ class CustomContentAdminHelperTest extends BcTestCase
      */
     public function test_label()
     {
-        /**
-         * case customField type BcCcTextarea and customLink parent_id is true
-         * and $options is empty
-         */
-        $customField = new CustomField([
-            'type' => 'BcCcTextarea',
-        ]);
-        //how to create one array customLink
-        $customLink = new CustomLink([
+        $customLink = CustomLinkFactory::make([
             'name' => 'test custom link',
             'status' => 1,
-            'custom_field' => $customField,
+            'custom_field' => [
+                'type' => 'BcCcTextarea',
+            ],
             'parent_id' => 1,
-        ]);
+        ])->getEntity();
         $rs = $this->CustomContentAdminHelper->label($customLink);
+        //check result return
         $this->assertEquals('<label for="test-custom-link">Test Custom Link</label><br>', $rs);
         /**
          * case customField type BcCcTextarea and customLink parent_id is true
          * and $options is not empty
          */
         $options = ['fieldName' => 'is fieldName'];
-        $customField = new CustomField([
-            'name' => 'test custom field',
-            'type' => 'BcCcTextarea',
-        ]);
-        //how to create one array customLink
-        $customLink = new CustomLink([
+        $customLink = CustomLinkFactory::make([
             'name' => 'test custom link',
             'status' => 1,
             'display_admin_list' => 1,
-            'custom_field' => $customField,
+            'custom_field' => [
+                'name' => 'test custom field',
+                'type' => 'BcCcTextarea',
+            ],
             'parent_id' => 1,
-        ]);
+        ])->getEntity();
         $rs = $this->CustomContentAdminHelper->label($customLink, $options);
+        //check result return
         $this->assertEquals('<label for="is-fieldname">Is Field Name</label><br>', $rs);
         /**
          * case customField type BcCcTextarea and customLink parent_id is false
          * and options is empty
          */
-        $customField = new CustomField([
-            'name' => 'test custom field',
-            'type' => 'BcCcTextarea',
-        ]);
-        //how to create one array customLink
-        $customLink = new CustomLink([
+        $customLink = CustomLinkFactory::make([
             'name' => 'test custom link',
             'status' => 1,
             'display_admin_list' => 1,
-            'custom_field' => $customField,
+            'custom_field' => [
+                'name' => 'test custom field',
+                'type' => 'BcCcTextarea',
+            ],
             'parent_id' => 0,
-        ]);
+        ])->getEntity();
         $rs = $this->CustomContentAdminHelper->label($customLink);
+        //check result return
         $this->assertEquals('<label for="test-custom-link">Test Custom Link</label>', $rs);
         /**
          * case customField type BcCcTextarea and customLink parent_id is false
          * and options is not empty
          */
         $options = ['fieldName' => 'is fieldName is not empty'];
-        $customField = new CustomField([
-            'name' => 'test custom field',
-            'type' => 'BcCcTextarea',
-        ]);
-        //how to create one array customLink
-        $customLink = new CustomLink([
+        $customLink = CustomLinkFactory::make([
             'name' => 'test custom link',
             'status' => 1,
             'display_admin_list' => 1,
-            'custom_field' => $customField,
+            'custom_field' => [
+                'name' => 'test custom field',
+                'type' => 'BcCcTextarea',
+            ],
             'parent_id' => 0,
-        ]);
+        ])->getEntity();
         $rs = $this->CustomContentAdminHelper->label($customLink, $options);
         $this->assertEquals('<label for="is-fieldname-is-not-empty">Is Field Name Is Not Empty</label>', $rs);
     }
