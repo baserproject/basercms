1c68e7d2e70ec7be86685acc7ca159465cdf5cf9
thangnn <thang.nguyen@mediabrige.vn>
2024-03-19 13:36:35 +0700

    add unitTest_CustomContentAdminHelper_beforeHead

--- plugins/bc-custom-content/src/View/Helper/CustomContentAdminHelper.php
+++ plugins/bc-custom-content/src/View/Helper/CustomContentAdminHelper.php
@@ -270,6 +270,7 @@ class CustomContentAdminHelper extends CustomContentAppHelper
      * @return string
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function attention(CustomLink $link): string
     {
