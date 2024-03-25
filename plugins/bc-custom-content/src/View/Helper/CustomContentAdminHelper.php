e8a722daa45111da7901a9d9da2daf8b262221f9
thangnnmd <150879641+thangnnmd@users.noreply.github.com>
2024-03-25 15:48:07 +0700

    add CustomContentAdminHelper_afterHead (#3261)
    Co-authored-by: thangnn <thang.nguyen@mediabrige.vn>

--- plugins/bc-custom-content/src/View/Helper/CustomContentAdminHelper.php
+++ plugins/bc-custom-content/src/View/Helper/CustomContentAdminHelper.php
@@ -256,6 +256,7 @@ class CustomContentAdminHelper extends CustomContentAppHelper
      * @return string
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function afterHead(CustomLink $link): string
     {
