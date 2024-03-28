a9e7d0fd52a76bc871cb44764623a33b95b33fae
ryuring <egashira@catchup.co.jp>
2024-03-28 10:38:24 +0900

    不正な日付をフォーマットした場合に Internal Server Error になってしまう問題を改善

--- plugins/baser-core/src/View/Helper/BcTimeHelper.php
+++ plugins/baser-core/src/View/Helper/BcTimeHelper.php
@@ -12,6 +12,7 @@
 namespace BaserCore\View\Helper;
 
 use BaserCore\Event\BcEventDispatcherTrait;
+use Cake\Core\Configure;
 use Cake\View\Helper\TimeHelper;
 use BaserCore\Annotation\Checked;
 use BaserCore\Annotation\NoTodo;
@@ -302,7 +303,17 @@ class BcTimeHelper extends TimeHelper
         if ($date !== "00:00:00" && (!$date || $date == '0000-00-00 00:00:00')) {
             return "";
         }
-        return parent::format($date, $format, $invalid, $timezone);
+        try {
+            return parent::format($date, $format, $invalid, $timezone);
+        } catch (\Throwable $e) {
+            $message = $e->getMessage();
+            $this->getView()->log($message);
+            if(Configure::read('debug')) {
+                return $message;
+            } else {
+                return '';
+            }
+        }
     }
 
     /**
