f91db5b101e92458fb592ac7c05308e1ebeb9b81
ryuring <egashira@catchup.co.jp>
2024-04-17 16:13:42 +0900

    【サイト内検索】カスタムコンテンツをサイト内検索に対応

--- plugins/bc-custom-content/src/Model/Table/CustomContentsTable.php
+++ plugins/bc-custom-content/src/Model/Table/CustomContentsTable.php
@@ -15,6 +15,9 @@ use BaserCore\Model\Table\AppTable;
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
 use BaserCore\Annotation\Checked;
+use Cake\Core\Plugin;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\EventInterface;
 use Cake\Validation\Validator;
 
 /**
@@ -24,6 +27,7 @@ use Cake\Validation\Validator;
  */
 class CustomContentsTable extends AppTable
 {
+
     /**
      * Initialize
      *
@@ -39,8 +43,10 @@ class CustomContentsTable extends AppTable
         $this->addBehavior('Timestamp');
         $this->belongsTo('CustomTables', ['className' => 'BcCustomContent.CustomTables'])
             ->setForeignKey('custom_table_id');
+        if (Plugin::isLoaded('BcSearchIndex')) {
+            $this->addBehavior('BcSearchIndex.BcSearchIndexManager');
+        }
     }
-
 
     /**
      * デフォルトのバリデーションを設定する
@@ -61,6 +67,53 @@ class CustomContentsTable extends AppTable
                 'rule' => 'halfText',
                 'message' => __d('baser_core', '一覧表示件数は半角で入力してください。')]);
         return $validator;
+    }
+
+    /**
+     * beforeSave
+     * @param EventInterface $event
+     * @param EntityInterface $entity
+     * @param \ArrayObject $options
+     * @return boolean
+     * @checked
+     * @noTodo
+     */
+    public function beforeSave(EventInterface $event, EntityInterface $entity, \ArrayObject $options)
+    {
+        if (!Plugin::isLoaded('BcSearchIndex')) {
+            return true;
+        }
+        if (empty($entity->content) || !empty($entity->content->exclude_search)) {
+            $this->setExcluded();
+        }
+        return true;
+    }
+
+    /**
+     * 検索用データを生成する
+     *
+     * @param array $data
+     * @return array|false
+     * @checked
+     * @noTodo
+     */
+    public function createSearchIndex($entity)
+    {
+        if (!$entity || !isset($entity->content)) {
+            return false;
+        }
+        return [
+            'type' => __d('baser_core', 'カスタムコンテンツ'),
+            'model_id' => $entity->id,
+            'content_id' => $entity->content->id,
+            'site_id' => $entity->content->site_id,
+            'title' => $entity->content->title,
+            'detail' => $entity->description,
+            'url' => $entity->content->url,
+            'status' => $entity->content->status,
+            'publish_begin' => $entity->content->publish_begin,
+            'publish_end' => $entity->content->publish_end
+        ];
     }
 
 }
