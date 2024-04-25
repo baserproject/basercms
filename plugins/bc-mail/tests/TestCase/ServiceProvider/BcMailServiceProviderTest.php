5ed3edec929d8efa2da99f7a192e2b8bbdcc8e89
thangnn <thang.nguyen@mediabrige.vn>
2024-04-25 09:38:16 +0700

    refactor code

--- plugins/bc-mail/tests/TestCase/ServiceProvider/BcMailServiceProviderTest.php
+++ plugins/bc-mail/tests/TestCase/ServiceProvider/BcMailServiceProviderTest.php
@@ -3,6 +3,14 @@
 namespace BcMail\Test\TestCase\ServiceProvider;
 
 use BaserCore\TestSuite\BcTestCase;
+use BcMail\Service\Admin\MailContentsAdminServiceInterface;
+use BcMail\Service\Admin\MailFieldsAdminServiceInterface;
+use BcMail\Service\Admin\MailMessagesAdminServiceInterface;
+use BcMail\Service\Front\MailFrontServiceInterface;
+use BcMail\Service\MailConfigsServiceInterface;
+use BcMail\Service\MailContentsServiceInterface;
+use BcMail\Service\MailFieldsServiceInterface;
+use BcMail\Service\MailMessagesServiceInterface;
 use BcMail\ServiceProvider\BcMailServiceProvider;
 use Cake\Core\Container;
 
@@ -11,34 +19,29 @@ class BcMailServiceProviderTest extends BcTestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->Provider = new BcMailServiceProvider();
+        $this->BcMailServiceProvider = new BcMailServiceProvider();
     }
 
     public function tearDown(): void
     {
-        unset($this->Provider);
+        unset($this->BcMailServiceProvider);
         parent::tearDown();
     }
 
+    /**
+     * test services
+     */
     public function testServices()
     {
         $container = new Container();
-        $this->Provider->services($container);
-        $mailConfigsService = $container->get('BcMail\Service\MailConfigsServiceInterface');
-        $this->assertEquals('BcMail\Service\MailConfigsService', get_class($mailConfigsService));
-        $mailContentsService = $container->get('BcMail\Service\MailContentsServiceInterface');
-        $this->assertEquals('BcMail\Service\MailContentsService', get_class($mailContentsService));
-        $mailContentsAdminService = $container->get('BcMail\Service\Admin\MailContentsAdminServiceInterface');
-        $this->assertEquals('BcMail\Service\Admin\MailContentsAdminService', get_class($mailContentsAdminService));
-        $mailFieldsService = $container->get('BcMail\Service\MailFieldsServiceInterface');
-        $this->assertEquals('BcMail\Service\MailFieldsService', get_class($mailFieldsService));
-        $mailFieldsAdminService = $container->get('BcMail\Service\Admin\MailFieldsAdminServiceInterface');
-        $this->assertEquals('BcMail\Service\Admin\MailFieldsAdminService', get_class($mailFieldsAdminService));
-        $mailMessagesService = $container->get('BcMail\Service\MailMessagesServiceInterface');
-        $this->assertEquals('BcMail\Service\MailMessagesService', get_class($mailMessagesService));
-        $mailMessagesAdminService = $container->get('BcMail\Service\Admin\MailMessagesAdminServiceInterface');
-        $this->assertEquals('BcMail\Service\Admin\MailMessagesAdminService', get_class($mailMessagesAdminService));
-        $mailFrontService = $container->get('BcMail\Service\Front\MailFrontServiceInterface');
-        $this->assertEquals('BcMail\Service\Front\MailFrontService', get_class($mailFrontService));
+        $this->BcMailServiceProvider->services($container);
+        $this->assertTrue($container->has(MailConfigsServiceInterface::class));
+        $this->assertTrue($container->has(MailContentsServiceInterface::class));
+        $this->assertTrue($container->has(MailContentsAdminServiceInterface::class));
+        $this->assertTrue($container->has(MailFieldsServiceInterface::class));
+        $this->assertTrue($container->has(MailFieldsAdminServiceInterface::class));
+        $this->assertTrue($container->has(MailMessagesServiceInterface::class));
+        $this->assertTrue($container->has(MailMessagesAdminServiceInterface::class));
+        $this->assertTrue($container->has(MailFrontServiceInterface::class));
     }
 }
