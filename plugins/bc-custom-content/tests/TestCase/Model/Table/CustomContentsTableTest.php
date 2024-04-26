b3a1136b258841f864a5908a31213bd5d50a7a14
thangnn <thang.nguyen@mediabrige.vn>
2024-04-26 13:22:36 +0700

    unitTest_CustomContentTable_createSearchIndex

--- plugins/bc-custom-content/tests/TestCase/Model/Table/CustomContentsTableTest.php
+++ plugins/bc-custom-content/tests/TestCase/Model/Table/CustomContentsTableTest.php
@@ -13,6 +13,10 @@ namespace BcCustomContent\Test\TestCase\Model\Table;
 
 use BaserCore\TestSuite\BcTestCase;
 use BcCustomContent\Model\Table\CustomContentsTable;
+use BcCustomContent\Service\CustomContentsService;
+use BcCustomContent\Test\Factory\CustomContentFactory;
+use BcCustomContent\Test\Scenario\CustomContentsScenario;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * CustomContentsTableTest
@@ -20,6 +24,11 @@ use BcCustomContent\Model\Table\CustomContentsTable;
  */
 class CustomContentsTableTest extends BcTestCase
 {
+
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
 
     /**
      * Set up
@@ -82,5 +91,38 @@ class CustomContentsTableTest extends BcTestCase
         $this->assertEquals([
             '_empty' => '一覧表示件数は必須項目です。',
         ], $errors['list_count']);
+    }
+
+    /**
+     * test createSearchIndex
+     */
+    public function test_createSearchIndex()
+    {
+        //content empty
+        $entity = CustomContentFactory::make([
+            'id' => 1,
+        ])->getEntity();
+        $this->assertFalse($this->CustomContentsTable->createSearchIndex($entity));
+
+        //The system operates normally
+        $this->loadFixtureScenario(CustomContentsScenario::class);
+        $customContentService = new CustomContentsService();
+        $customContent = $customContentService->get(1);
+
+        //set content publish_begin and publish_end
+        $customContent->content->publish_begin = '2021-01-01 00:00:00';
+        $customContent->content->publish_end = '2021-12-31 23:59:59';
+
+        $rs = $this->CustomContentsTable->createSearchIndex($customContent);
+        $this->assertEquals($rs['type'], 'カスタムコンテンツ');
+        $this->assertEquals($rs['model_id'], 1);
+        $this->assertEquals($rs['content_id'], 100);
+        $this->assertEquals($rs['site_id'], 1);
+        $this->assertEquals($rs['title'], 'サービスタイトル');
+        $this->assertEquals($rs['detail'], 'サービステスト');
+        $this->assertEquals($rs['url'], '/test/');
+        $this->assertTrue($rs['status']);
+        $this->assertEquals($rs['publish_begin'], '2021-01-01 00:00:00');
+        $this->assertEquals($rs['publish_end'], '2021-12-31 23:59:59');
     }
 }
