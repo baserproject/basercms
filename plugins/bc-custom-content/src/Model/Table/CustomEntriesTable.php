d6ccac2654e2972821a4537feadc86f44951d3e1
thangnnmd <150879641+thangnnmd@users.noreply.github.com>
2024-05-08 08:35:59 +0700

    add CustomEntriesTable_setValidateFileExt (#3308)
    Co-authored-by: thangnn <thang.nguyen@mediabrige.vn>

--- plugins/bc-custom-content/src/Model/Table/CustomEntriesTable.php
+++ plugins/bc-custom-content/src/Model/Table/CustomEntriesTable.php
@@ -293,6 +293,7 @@ class CustomEntriesTable extends AppTable
      * @return Validator
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function setValidateFileExt(Validator $validator, CustomLink $link)
     {
