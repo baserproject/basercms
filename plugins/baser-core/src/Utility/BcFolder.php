34d6094e48ffa6531cdd674ecac2ba5402a1112b
ryuring <egashira@catchup.co.jp>
2024-07-08 17:29:06 +0900

    BcFolderに後方互換用の機能を実装

--- plugins/baser-core/src/Utility/BcFolder.php
+++ plugins/baser-core/src/Utility/BcFolder.php
@@ -14,6 +14,7 @@ namespace BaserCore\Utility;
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
 use BaserCore\Annotation\Checked;
+use Cake\Log\LogTrait;
 use Exception;
 use FilesystemIterator;
 use RecursiveDirectoryIterator;
@@ -26,12 +27,22 @@ class BcFolder
 {
 
     /**
+     * Trait
+     */
+    use LogTrait;
+
+    /**
      * パス
      * @var string
      */
     private string $path;
 
+    /**
+     * 権限
+     * @var int
+     */
     public int $mode = 0755;
+
     /**
      * Holds messages from last method.
      *
@@ -53,8 +64,12 @@ class BcFolder
      * @noTodo
      * @unitTest
      */
-    public function __construct(string $path)
+    public function __construct(string $path = '')
     {
+        if(!$path) {
+            // @deprecated 6.0.0 since 5.1.0 後方互換用
+            $this->log(__d('baser_core', 'BcFile では、コンストラクタでパスの指定をしないのは非推奨です。パスの指定行うようにしてください。この要件はバージョン 6.0.0 で必須となります。'));
+        }
         $this->path = $path;
     }
 
@@ -78,8 +93,13 @@ class BcFolder
      * @noTodo
      * @unitTest
      */
-    public function create(int $mask = 0777)
+    public function create(int|string $mask = 0777)
     {
+        if(is_string($mask)) {
+            // @deprecated 6.0.0 since 5.1.0 後方互換用
+            $this->log(__d('baser_core', 'BcFile::create() では、第一引数にパスを指定するのは非推奨です。パスの指定はコンストラクタで行ってください。この要件はバージョン 6.0.0 で必須となります。'));
+            $this->path = $mask;
+        }
         $path = $this->path;
         $parent = dirname($path);
         if (!is_dir($parent)) {
@@ -368,6 +388,22 @@ class BcFolder
         $files = $this->getFiles();
 
         return array_values(preg_grep('/^' . $regexpPattern . '$/i', $files));
+    }
+
+    /**
+     * フォルダ内のファイルとフォルダを読み込む
+     *
+     * @param mixed $sort 利用不可
+     * @param bool $exceptions 利用不可
+     * @param bool $fullPath
+     * @return array
+     * @deprecated 6.0.0 since 5.1.0 後方互換用
+     */
+    public function read($sort = true, $exceptions = false, $fullPath = false)
+    {
+        $files[0] = $this->getFolders(['full' => $fullPath]);
+        $files[1] = $this->getFiles(['full' => $fullPath]);
+        return $files;
     }
 
 }
