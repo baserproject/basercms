d00bfe37aa477d37f317056e9c4278a05a2178ed
thangnn <thang.nguyen@mediabrige.vn>
2024-07-09 21:42:15 +0700

    Add unitTest_SearchIndexesSearchForm_buildSchema

---
+++ plugins/bc-search-index/tests/TestCase/Form/SearchIndexesSearchFormTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BcSearchIndex\Test\TestCase\Form;
+
+use BaserCore\TestSuite\BcTestCase;
+use BcSearchIndex\Form\SearchIndexesSearchForm;
+use Cake\Form\Schema;
+
+class SearchIndexesSearchFormTest extends BcTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function test_buildSchema(): void
+    {
+        $schema = $this->execPrivateMethod(new SearchIndexesSearchForm(), '_buildSchema', [new Schema()]);
+        $this->assertEquals('string', $schema->fieldType('type'));
+        $this->assertEquals('string', $schema->fieldType('site_id'));
+        $this->assertEquals('string', $schema->fieldType('folder_id'));
+        $this->assertEquals('string', $schema->fieldType('keyword'));
+        $this->assertEquals('string', $schema->fieldType('status'));
+        $this->assertEquals('string', $schema->fieldType('priority'));
+    }
+}
