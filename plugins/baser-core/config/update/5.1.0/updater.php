24912fdda6a34a11e9f9694198afff2ac9ed4aa3
ryuring <egashira@catchup.co.jp>
2024-07-15 20:35:52 +0900

    5.1.x のアップデータを調整

--- plugins/baser-core/config/update/5.1.0/updater.php
+++ plugins/baser-core/config/update/5.1.0/updater.php
@@ -19,12 +19,27 @@ use BaserCore\Utility\BcUpdateLog;
 
 $updateDir = __DIR__;
 
+/**
+ * AjaxView.php を上書き
+ */
 if (is_writable(ROOT . DS . 'src' . DS . 'View' . DS . 'AjaxView.php')) {
     copy($updateDir . DS . 'src' . DS . 'View' . DS . 'AjaxView.php', ROOT . DS . 'src' . DS . 'View' . DS . 'AjaxView.php');
 } else {
     BcUpdateLog::set(__d('baser_core', ROOT . DS . 'src' . DS . 'View' . DS . 'AjaxView.php に書き込み権限がありません。' . $updateDir . DS . 'src' . DS . 'View' . DS . 'AjaxView.php をコピーして手動で上書きしてください。'));
 }
 
+/**
+ * サイト設定の「簡易パスワード設定」を有効にする
+ */
 /** @var \BaserCore\Service\SiteConfigsService $siteConfigsService */
 $siteConfigsService = BcContainer::get()->get(SiteConfigsServiceInterface::class);
 $siteConfigsService->setValue('allow_simple_password', true);
+
+/**
+ * サイトURLがSSLと違う場合、SITE_URL を SSL_URL に変更する
+ */
+$siteUrl = env('SITE_URL');
+$sslUrl = env('SSL_URL');
+if($siteUrl !== $sslUrl) {
+    $siteConfigsService->putEnv('SITE_URL', $sslUrl);
+}
