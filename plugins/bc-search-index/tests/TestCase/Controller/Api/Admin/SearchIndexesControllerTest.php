0c9f591fffef65ee3dafe057d0ede187b4127854
HungDV2022 <110375578+HungDV2022@users.noreply.github.com>
2024-08-04 15:00:15 +0900

    SearchIndexesController::index()のユニットテスト (#3625)
    Co-authored-by: Đỗ Văn Hùng <dovanhungk57@gmail.com>

--- plugins/bc-search-index/tests/TestCase/Controller/Api/Admin/SearchIndexesControllerTest.php
+++ plugins/bc-search-index/tests/TestCase/Controller/Api/Admin/SearchIndexesControllerTest.php
@@ -121,6 +121,23 @@ class SearchIndexesControllerTest extends BcTestCase
     }
 
     /**
+     * test index
+     */
+    public function testIndex()
+    {
+        //データ生成
+        SearchIndexFactory::make(['site_id' => 4, 'title' => '会社案内', 'detail' => 'baserCMS inc.',])->persist();
+
+        //APIを呼ぶ
+        $this->get('/baser/api/admin/bc-search-index/search_indexes.json?keyword=inc&s=4&token=' . $this->accessToken);
+
+        //戻り値を確認
+        $this->assertResponseOk();
+        $result = json_decode((string)$this->_response->getBody());
+        $this->assertEquals('会社案内', $result->searchIndexes[0]->title);
+    }
+
+    /**
      * test delete
      * @return void
      */
