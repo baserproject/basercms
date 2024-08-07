0b5c64a266ff4df4ea53e4cf8d2e0f1c3cac6fc0
thangnn <nguyenthang.mediabridge@gmail.com>
2024-08-07 09:48:26 +0700

    modified

--- plugins/baser-core/tests/TestCase/Controller/Api/Admin/ContentsControllerTest.php
+++ plugins/baser-core/tests/TestCase/Controller/Api/Admin/ContentsControllerTest.php
@@ -430,5 +430,15 @@ class ContentsControllerTest extends \BaserCore\TestSuite\BcTestCase
 
         $result = json_decode((string)$this->_response->getBody());
         $this->assertCount(20, $result->contents);
+
+        // treeテスト
+        $this->get('/baser/api/admin/baser-core/contents/index.json?site_id=1&list_type=tree&token=' . $this->accessToken);
+        $this->assertResponseOk();
+
+        // tableテスト
+        $this->get('/baser/api/admin/baser-core/contents/index.json?site_id=1&folder_id=6&name=サービス&type=Page&token=' . $this->accessToken);
+        $this->assertResponseOk();
+        $result = json_decode((string)$this->_response->getBody());
+        $this->assertCount(3, $result->contents);
     }
 }
