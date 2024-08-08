409d72b09877255e1829a934c00fa2b5b3cfab67
thangnnmd <150879641+thangnnmd@users.noreply.github.com>
2024-08-09 05:12:09 +0700

    BaserCorePlugin::getSkipCsrfUrl (#3684)
    Co-authored-by: thangnn <nguyenthang.mediabridge@gmail.com>

--- plugins/baser-core/src/BaserCorePlugin.php
+++ plugins/baser-core/src/BaserCorePlugin.php
@@ -335,6 +335,7 @@ class BaserCorePlugin extends BcPlugin implements AuthenticationServiceProviderI
      * @return array
      * @noTodo
      * @checked
+     * @unitTest
      */
     protected function getSkipCsrfUrl(): array
     {
