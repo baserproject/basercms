f546f9cd102f8c5bbcb6ab6841e4ed38b57ab216
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2024-08-13 10:27:46 +0900

    UploaderFile::filesExists() ユニットテスト調整

--- plugins/bc-uploader/tests/TestCase/Model/Entity/UploaderFileTest.php
+++ plugins/bc-uploader/tests/TestCase/Model/Entity/UploaderFileTest.php
@@ -3,7 +3,9 @@
 namespace BcUploader\Test\TestCase\Model\Entity;
 
 use BaserCore\TestSuite\BcTestCase;
+use BaserCore\Utility\BcFile;
 use BcUploader\Model\Entity\UploaderFile;
+use BcUploader\Test\Factory\UploaderFileFactory;
 use BcUploader\Test\Scenario\UploaderFilesScenario;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
@@ -30,9 +32,31 @@ class UploaderFileTest extends BcTestCase
         parent::tearDown();
     }
 
+    /**
+     * test filesExists
+     */
     public function test_filesExists()
     {
-        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+        //データを生成
+        UploaderFileFactory::make(['name' => 'social_new.jpg', 'publish_begin' => '2017-07-09 03:38:07'])->persist();
+
+        //テストファイルを生成
+        $file = new BcFile('/var/www/html/webroot/files/uploads/limited/social_new__small.jpg');
+        $file->create();
+        $file = new BcFile('/var/www/html/webroot/files/uploads/limited/social_new__midium.jpg');
+        $file->create();
+        $file = new BcFile('/var/www/html/webroot/files/uploads/limited/social_new__large.jpg');
+        $file->create();
+
+        //テスト対象メソッドを呼ぶ
+        $entity = $this->UploaderFile->find()->where(['UploaderFiles.name' => "social_new.jpg"])->first();
+        $rs = $entity->filesExists();
+        $this->assertEquals(['small' => true, 'midium' => true, 'large' => true], $rs);
+
+        //テストファイルを削除
+        unlink('/var/www/html/webroot/files/uploads/limited/social_new__small.jpg');
+        unlink('/var/www/html/webroot/files/uploads/limited/social_new__midium.jpg');
+        unlink('/var/www/html/webroot/files/uploads/limited/social_new__large.jpg');
     }
 
     public function test_getSmall()
