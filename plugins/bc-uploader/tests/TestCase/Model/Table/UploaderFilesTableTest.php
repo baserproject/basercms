86b48e2db2380693f95afc8ebd1a94bb45614cd5
Đỗ Văn Hùng <dovanhungk57@gmail.com>
2024-08-15 12:08:54 +0900

    UploaderFilesTable::validationDefault() ユニットテスト

--- plugins/bc-uploader/tests/TestCase/Model/Table/UploaderFilesTableTest.php
+++ plugins/bc-uploader/tests/TestCase/Model/Table/UploaderFilesTableTest.php
@@ -54,6 +54,21 @@ class UploaderFilesTableTest extends BcTestCase
         $this->assertTrue($this->UploaderFilesTable->hasBehavior('BcUpload'));
         $this->assertTrue($this->UploaderFilesTable->hasAssociation('UploaderCategories'));
     }
+
+    /**
+     * test validationDefault
+     * @return void
+     */
+    public function testValidationDefault()
+    {
+        $blogCategory = $this->UploaderFilesTable->newEntity(["name" => 'test', "publish_begin" => "2021-01-27 12:00:00", "publish_end" => "2021-01-01 00:00:00"]);
+
+        $errors = $blogCategory->getErrors();
+        $this->assertEquals('公開期間が不正です。', current($errors['publish_begin']));
+        $this->assertEquals('公開期間が不正です。', current($errors['publish_end']));
+        $this->assertEquals('許可されていないファイルです。', current($errors['name']));
+    }
+
     /**
      * 公開期間をチェックする
      * @dataProvider periodDataProvider
