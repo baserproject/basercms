4fd68f25b15eb7eecc469dde487cc2827175c2af
thangnn <nguyenthang.mediabridge@gmail.com>
2024-08-22 09:20:49 +0700

    modified

--- plugins/bc-installer/tests/TestCase/Service/InstallationsServiceTest.php
+++ plugins/bc-installer/tests/TestCase/Service/InstallationsServiceTest.php
@@ -68,9 +68,6 @@ class InstallationsServiceTest extends BcTestCase
                 'BcRequire' => [
                     'phpVersion' => "8.0.0",
                     'phpMemory' => "128",
-                    'MySQLVersion' => "5.0.0",
-                    'winSQLiteVersion' => "3.7.16",
-                    'PostgreSQLVersion' => "8.4.0"
                 ]
             ]
         );
