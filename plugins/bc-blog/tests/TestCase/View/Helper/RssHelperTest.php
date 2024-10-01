4501ac8af66a1edce50c12528ce8e2042f690121
thangnnmd <150879641+thangnnmd@users.noreply.github.com>
2024-10-02 06:40:34 +0700

    RssHelper::implementedEvents (#3839)
    Co-authored-by: thangnn <nguyenthang.mediabridge@gmail.com>

--- plugins/bc-blog/tests/TestCase/View/Helper/RssHelperTest.php
+++ plugins/bc-blog/tests/TestCase/View/Helper/RssHelperTest.php
@@ -201,4 +201,13 @@ class RssHelperTest extends BcTestCase
         $this->assertStringContainsString('<description>Channel Description', $result);
         $this->assertStringContainsString('<customElement>Custom Value', $result);
     }
+
+    /**
+     * test implementedEvents
+     */
+    public function testImplementedEvents()
+    {
+        $result = $this->RssHelper->implementedEvents();
+        $this->assertEquals([], $result);
+    }
 }
