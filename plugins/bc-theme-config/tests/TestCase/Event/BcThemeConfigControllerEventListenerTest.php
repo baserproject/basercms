ef57c52707064cf3495251d929544f7dac9cbb78
thangnn <nguyenthang.mediabridge@gmail.com>
2024-10-25 13:59:11 +0700

    Add unitTest_BcThemeConfigControllerEventListener_baserCoreThemesAfterApply

---
+++ plugins/bc-theme-config/tests/TestCase/Event/BcThemeConfigControllerEventListenerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BcThemeConfig\Test\TestCase\Event;
+
+use BaserCore\TestSuite\BcTestCase;
+use BcThemeConfig\Event\BcThemeConfigControllerEventListener;
+use Cake\Event\Event;
+
+class BcThemeConfigControllerEventListenerTest extends BcTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->BcThemeConfigControllerEventListener = new BcThemeConfigControllerEventListener();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test baserCoreThemesAfterApply
+     */
+    public function test_baserCoreThemesAfterApply()
+    {
+        $path = WWW_ROOT . 'files' . DS . 'theme_configs' . DS . 'config.css';
+
+        if (!is_dir(dirname($path))) {
+            mkdir(dirname($path), 0777, true);
+        }
+        file_put_contents($path, 'test content');
+        $this->assertFileExists($path);
+
+        $event = new Event('BaserCore.Themes.afterApply');
+        $this->BcThemeConfigControllerEventListener->baserCoreThemesAfterApply($event);
+
+        $this->assertFileDoesNotExist($path);
+
+        // cleanup
+        if (file_exists($path)) {
+            unlink($path);
+        }
+        if (is_dir(dirname($path))) {
+            rmdir(dirname($path));
+        }
+    }
+}
