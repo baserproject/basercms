062215d78fba0429d858b262289cc1425baf5444
HungDV2022 <dovanhungk57@gmail.com>
2024-11-22 10:28:11 +0900

    BcShortCodeEventListener::afterRender() ユニットテスト調整

--- plugins/baser-core/tests/TestCase/Event/BcShortCodeEventListenerTest.php
+++ plugins/baser-core/tests/TestCase/Event/BcShortCodeEventListenerTest.php
@@ -4,9 +4,16 @@ namespace BaserCore\Test\TestCase\Event;
 
 use BaserCore\Event\BcShortCodeEventListener;
 use BaserCore\TestSuite\BcTestCase;
+use BaserCore\View\BcFrontAppView;
+use Cake\Event\Event;
 
 class BcShortCodeEventListenerTest extends BcTestCase
 {
+    /**
+     * @var BcShortCodeEventListener
+     */
+    protected $BcShortCodeEventListener;
+
     public function setUp(): void
     {
         parent::setUp();
@@ -26,4 +33,34 @@ class BcShortCodeEventListenerTest extends BcTestCase
         $this->assertTrue(is_array($this->BcShortCodeEventListener->implementedEvents()));
     }
 
+    /**
+     * test afterRender
+     */
+    public function testAfterRender()
+    {
+        //準備
+        $view = new BcFrontAppView($this->getRequest('/'));
+        $view->loadHelper('BcBaser', ['className' => 'BaserCore.BcBaser']);
+        $view->assign('content', '[BcBaser.getSitemap]');
+        $event = new Event('afterRender', $view);
+        //テストを実行
+        $this->BcShortCodeEventListener->afterRender($event);
+        //戻り値を確認
+        $this->assertTextContains('<ul class="menu ul-level-1">', $view->fetch('content'));
+    }
+
+    /**
+     * test _execShortCode
+     */
+    public function test_execShortCode()
+    {
+        //準備
+        $view = new BcFrontAppView($this->getRequest('/'));
+        $view->loadHelper('BcBaser', ['className' => 'BaserCore.BcBaser']);
+        $view->assign('content', '[BcBaser.getSitemap]');
+        //テストを実行
+        $this->execPrivateMethod($this->BcShortCodeEventListener, '_execShortCode', [$view]);
+        //戻り値を確認
+        $this->assertTextContains('<ul class="menu ul-level-1">', $view->fetch('content'));
+    }
 }
