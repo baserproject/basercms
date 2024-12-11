3663ab2c6caaa8a82b0ba8ccbc272c2a47ce4ea3
thangnn <nguyenthang.mediabridge@gmail.com>
2024-12-11 15:31:35 +0700

    Add unitTest_BcInstallerPlugin_routes

--- plugins/bc-installer/tests/TestCase/BcInstallerPluginTest.php
+++ plugins/bc-installer/tests/TestCase/BcInstallerPluginTest.php
@@ -9,7 +9,9 @@ use BcInstaller\Command\InstallCommand;
 use BcInstaller\Service\Admin\InstallationsAdminServiceInterface;
 use BcInstaller\Service\InstallationsServiceInterface;
 use Cake\Console\CommandCollection;
+use Cake\Core\Configure;
 use Cake\Core\Container;
+use Cake\Routing\Router;
 
 class BcInstallerPluginTest extends BcTestCase
 {
@@ -40,7 +42,12 @@ class BcInstallerPluginTest extends BcTestCase
      */
     public function test_routes()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        Configure::write('BcEnv.isInstalled', false);
+        $routes = Router::createRouteBuilder('/install');
+        $this->BcInstallerPlugin->routes($routes);
+        $result = Router::parseRequest($this->getRequest('/install'));
+        $this->assertEquals('Installations', $result['controller']);
+        Configure::write('BcEnv.isInstalled', true);
     }
 
     /**
