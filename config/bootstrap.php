3901f613abc55588f4593336aaf777fb99fea3fd
Ryuichi <iwasaki@catchup.co.jp>
2025-02-25 15:56:28 +0900

    X-Powered-Byヘッダーを送信しない機能が欲しい #4125

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -232,3 +232,4 @@ ServerRequest::addDetector('tablet', function ($request) {
 // and https://unicode-org.github.io/icu/userguide/format_parse/datetime/#datetime-format-syntax
 //\Cake\I18n\Date::setToStringFormat('dd.MM.yyyy');
 //\Cake\I18n\Time::setToStringFormat('dd.MM.yyyy HH:mm');
+header_remove('X-Powered-By');
