ecf8277d37cbab86f4c2015e3a23e1c94aa81c8f
ryuring <egashira@catchup.co.jp>
2025-09-26 08:11:08 +0900

    ssl オプションを削除、full オプションのテストを追加

--- plugins/baser-core/src/View/Helper/BcBaserHelper.php
+++ plugins/baser-core/src/View/Helper/BcBaserHelper.php
@@ -408,7 +408,7 @@ class BcBaserHelper extends Helper
      *    - `escapeTitle` : タイトルをエスケープするかどうか（初期値 : true）
      *    - `prefix` : URLにプレフィックスをつけるかどうか（初期値 : false）
      *    - `forceTitle` : 許可されていないURLの際にタイトルを強制的に出力するかどうか（初期値 : false）
-     *    - `ssl` : SSL用のURLをして出力するかどうか（初期値 : false）
+     *    - `full` : URLをフルパスで出力するかどうか（初期値 : false）
      *     ※ その他のパラメータについては、HtmlHelper::link() を参照。
      * @param string $confirmMessage 確認メッセージ（初期値 : false）
      *    リンクをクリックした際に確認メッセージが表示され、はいをクリックした場合のみ遷移する
@@ -433,7 +433,6 @@ class BcBaserHelper extends Helper
      *    - `escapeTitle` : タイトルをエスケープするかどうか（初期値 : true）
      *    - `prefix` : URLにプレフィックスをつけるかどうか（初期値 : false）
      *    - `forceTitle` : 許可されていないURLの際にタイトルを強制的に出力するかどうか（初期値 : false）
-     *    - `ssl` : SSL用のURLをして出力するかどうか（初期値 : false）
      *    - `enabled` : リンクが有効かどうか（初期値 : true）
      *    - `full` : URLをフルパスで出力するかどうか（初期値 : false）
      *     ※ その他のパラメータについては、HtmlHelper::image() を参照。
@@ -453,7 +452,6 @@ class BcBaserHelper extends Helper
             'escape' => true,
             'prefix' => false,
             'forceTitle' => false,
-            'ssl' => $this->isSSL(),
             'enabled' => true,
             'full' => false
         ], $options);
@@ -472,9 +470,9 @@ class BcBaserHelper extends Helper
         $request = $this->getView()->getRequest();
         $prefix = $options['prefix'];
         $forceTitle = $options['forceTitle'];
-        $ssl = $options['ssl'];
         $enabled = $options['enabled'];
-        unset($options['prefix'], $options['forceTitle'], $options['ssl'], $options['enabled']);
+        $full = $options['full'];
+        unset($options['prefix'], $options['forceTitle'], $options['enabled'], $options['full']);
 
         if ($prefix && is_array($url) && !empty($request->getParam('prefix'))) {
             $url[$request->getParam('prefix')] = true;
@@ -491,18 +489,14 @@ class BcBaserHelper extends Helper
             }
         }
 
-        // $options['full'] がtrueの場合、常にフルパスとする
-        // 現在SSLの場合、特定の条件でフルパスとする
-        // - //(スラッシュスラッシュ)から始まるURL
-        // - http / https 以外のプロトコル
-        // - ハッシュタグから始まるURL
-        $full = $options['full'];
         if (BcUtil::isInstalled()
-            && ($full && $ssl)
+            && ($full)
             && !(preg_match('/^(javascript|https?|ftp|tel|mailto):/', $srcUrl))
             && !(strpos($srcUrl, '//') === 0)
             && !preg_match('/^#/', $srcUrl)) {
             $full = true;
+        } else {
+            $full = false;
         }
 
         if (preg_match('{^' . BcUtil::getPrefix(true) . '\/}', $srcUrl) || !isset($this->BcContents)) {
