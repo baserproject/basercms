062c194b8d95b442b10d5b2cf500974772067a34
ryuring <egashira@catchup.co.jp>
2025-11-21 13:21:07 +0900

    ドキュメントルートの位置をプロジェクトルートに変更

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * The Front Controller for handling every request
+ *
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
@@ -9,8 +11,34 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @since         0.2.9
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
  */
 
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+define('WWW_ROOT', __DIR__ . '/');
+
+// For built-in server
+if (PHP_SAPI === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+if(file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+} else {
+    require __DIR__ . '/composer_installer.php';
+    return;
+}
+
+use App\Application;
+use Cake\Http\Server;
+
+// Bind your application to the server.
+$server = new Server(new Application(__DIR__ . '/config'));
+
+// Run the request/response through the application and emit the response.
+$server->emit($server->run());
