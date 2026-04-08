50ed6009e9cb62a383c72512862d34baf1d76ff8
kaburk <sakaguchi@catchup.co.jp>
2026-04-08 18:05:53 +0900

    fix: #4345 ブログ草稿欄のバリデーションエラーメッセージが表示されない問題を修正 (#4348)

--- plugins/bc-blog/tests/TestCase/View/BlogAdminAppViewTest.php
+++ plugins/bc-blog/tests/TestCase/View/BlogAdminAppViewTest.php
@@ -11,6 +11,8 @@
 
 namespace BcBlog\Test\TestCase\View;
 
+use BcBlog\Test\Factory\BlogContentFactory;
+use BcBlog\Test\Factory\BlogPostFactory;
 use BcBlog\View\BlogAdminAppView;
 use BaserCore\TestSuite\BcTestCase;
 
@@ -53,6 +55,57 @@ class BlogAdminAppViewTest extends BcTestCase
     {
         $this->BlogAdminAppView->initialize();
         $this->assertNotEmpty($this->BlogAdminAppView->Blog);
+    }
+
+    /**
+     * ブログ記事フォームで草稿欄のエラーを表示する
+     */
+    public function testRenderBlogPostFormDisplaysDraftError(): void
+    {
+        $request = $this->getRequest('/baser/admin/bc-blog/blog_posts/edit/1/1')
+            ->withParam('plugin', 'BcBlog')
+            ->withParam('prefix', 'Admin')
+            ->withParam('controller', 'BlogPosts')
+            ->withAttribute('formTokenData', ['dummy']);
+        $this->BlogAdminAppView->setRequest($request);
+        $this->BlogAdminAppView->setTheme('BcAdminThird');
+
+        $post = BlogPostFactory::make([
+            'blog_content_id' => 1,
+            'title' => 'test',
+            'name' => 'test',
+            'detail' => '',
+            'detail_draft' => '<?php echo $test; ?>',
+            'status' => false,
+            'posted' => null,
+            'publish_begin' => null,
+            'publish_end' => null,
+        ])->getEntity();
+        $post->setError('detail_draft', ['containsScript' => '草稿欄でスクリプトの入力は許可されていません。']);
+
+        $blogContent = BlogContentFactory::make([
+            'id' => 1,
+            'use_content' => true,
+            'tag_use' => false,
+        ])->getEntity();
+
+        $this->BlogAdminAppView->set(compact('post', 'blogContent'));
+        $this->BlogAdminAppView->set([
+            'editor' => 'BaserCore.BcCkeditor',
+            'editorOptions' => [],
+            'editorEnterBr' => false,
+            'users' => [],
+            'categories' => [],
+            'hasNewCategoryAddablePermission' => false,
+            'hasNewTagAddablePermission' => false,
+            'fullUrl' => 'https://localhost'
+        ]);
+        $this->BlogAdminAppView->BcAdminForm->BcUpload->setTable('BcBlog.BlogPosts');
+        $this->BlogAdminAppView->BcAdminForm->create($post);
+
+        $result = $this->BlogAdminAppView->element('BlogPosts/form');
+
+        $this->assertStringContainsString('草稿欄でスクリプトの入力は許可されていません。', $result);
     }
 
 }
