31cc5f56b11d981175c51a2fc5d9a08b5cef7d39
cu-iwasaki <iwasaki@catchup.co.jp>
2026-07-24 06:39:33 +0900

    ファイルの公開期間外に設定するとフロントで500エラーになる不具合の修正 (#4475)

--- plugins/baser-core/src/Controller/BcErrorController.php
+++ plugins/baser-core/src/Controller/BcErrorController.php
@@ -24,6 +24,7 @@ use Cake\Event\EventManagerInterface;
 use Cake\Http\Response;
 use Cake\Http\ServerRequest;
 use Cake\ORM\TableRegistry;
+use Throwable;
 
 /**
  * BcErrorController
@@ -108,6 +109,18 @@ class BcErrorController extends BcFrontAppController
         $this->viewBuilder()->setTemplatePath('Error');
         // プラグイン等のイベントが残っているとエラー画面が正常に表示されない場合があるため、イベントを削除する
         EventManager::instance()->off('View.beforeRender');
+    }
+
+    /**
+     * NotFoundException のエラーページを描画する
+     *
+     * @param Throwable|null $error
+     * @return void
+     * @noTodo
+     */
+    public function notFound(?Throwable $error = null): void
+    {
+        $this->viewBuilder()->setTemplate('error400');
     }
 
 }
