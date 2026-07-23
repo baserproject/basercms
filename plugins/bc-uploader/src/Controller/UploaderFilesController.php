c953bd16a1932a80256550640f8034de4c5cc3fa
Ryuichi <iwasaki@catchup.co.jp>
2026-07-23 13:24:39 +0900

    打ち消しコミット

--- plugins/bc-uploader/src/Controller/UploaderFilesController.php
+++ plugins/bc-uploader/src/Controller/UploaderFilesController.php
@@ -54,10 +54,6 @@ class UploaderFilesController extends BcFrontAppController
             }
         }
 
-        if ($display && !file_exists(WWW_ROOT . 'files' . DS . 'uploads' . DS . 'limited' . DS . $filename)) {
-            $display = false;
-        }
-
         if ($display) {
             $info = pathinfo($filename);
             $ext = $info['extension'];
